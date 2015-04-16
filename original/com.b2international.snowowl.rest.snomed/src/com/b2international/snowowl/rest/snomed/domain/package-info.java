87ad58bffc603aa683dccdc738ae5e602593296a
Kai Kewley <kaikewley@gmail.com>
2015-04-16 11:49:46 +0100

    Removing old example code.

--- original/com.b2international.snowowl.rest.snomed/src/com/b2international/snowowl/rest/snomed/domain/package-info.java
+++
@@ -1,9 +0,0 @@
-/*******************************************************************************
- * Copyright (c) 2014 B2i Healthcare. All rights reserved.
- *******************************************************************************/
-
-/**
- * TODO add javadoc
- * @author Andras Peteri
- */
-package com.b2international.snowowl.rest.snomed.domain;
