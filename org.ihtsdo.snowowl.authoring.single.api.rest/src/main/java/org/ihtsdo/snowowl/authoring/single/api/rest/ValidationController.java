f9dc89b185e1a1acacd999d6e3066b6e9f320638
Kai Kewley <kaikewley@gmail.com>
2015-08-06 21:50:31 +0100

    Orchestration validation communication via JMS queues.

--- org.ihtsdo.snowowl.authoring.single.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/single/api/rest/ValidationController.java
+++ org.ihtsdo.snowowl.authoring.single.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/single/api/rest/ValidationController.java
@@ -1,14 +1,10 @@
 package org.ihtsdo.snowowl.authoring.single.api.rest;
 
-import java.io.IOException;
-
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
-
 import net.rcarz.jiraclient.JiraException;
-
 import org.ihtsdo.otf.rest.exception.BusinessServiceException;
 import org.ihtsdo.snowowl.api.rest.common.AbstractRestService;
 import org.ihtsdo.snowowl.api.rest.common.AbstractSnomedRestService;
@@ -16,9 +12,13 @@ import org.ihtsdo.snowowl.authoring.single.api.pojo.Validation;
 import org.ihtsdo.snowowl.authoring.single.api.service.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import us.monoid.json.JSONException;
+
+import java.io.IOException;
 
 @Api("Authoring Projects")
 @RestController
@@ -33,7 +33,7 @@ public class ValidationController extends AbstractSnomedRestService {
 	@RequestMapping(value = "/projects/{projectKey}/tasks/{taskKey}/validation", method = RequestMethod.POST)
 	public Validation startValidation(@PathVariable final String projectKey, @PathVariable final String taskKey) throws JiraException,
 			JSONException, IOException, BusinessServiceException {
-		return validationService.startValidation(projectKey, taskKey);
+		return validationService.startValidation(projectKey, taskKey, ControllerHelper.getUsername());
 	}
 
 	@ApiOperation(value = "Recover the most recent validation on a task")
@@ -50,7 +50,7 @@ public class ValidationController extends AbstractSnomedRestService {
 	@RequestMapping(value = "/projects/{projectKey}/validation", method = RequestMethod.POST)
 	public Validation startValidation(@PathVariable final String projectKey) throws JiraException, JSONException, IOException,
 			BusinessServiceException {
-		return validationService.startValidation(projectKey);
+		return validationService.startValidation(projectKey, ControllerHelper.getUsername());
 	}
 
 	@ApiOperation(value = "Recover the most recent validation on project")
--- org.ihtsdo.snowowl.authoring.single.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/single/api/service/ValidationService.java
+++ org.ihtsdo.snowowl.authoring.single.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/single/api/service/ValidationService.java
@@ -1,37 +1,82 @@
 package org.ihtsdo.snowowl.authoring.single.api.service;
 
-import java.io.IOException;
-
-import org.ihtsdo.otf.im.utility.SecurityService;
-import org.ihtsdo.otf.rest.client.OrchestrationRestClient;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.ihtsdo.otf.jms.MessagingHelper;
 import org.ihtsdo.otf.rest.exception.BusinessServiceException;
+import org.ihtsdo.snowowl.authoring.single.api.pojo.EntityType;
+import org.ihtsdo.snowowl.authoring.single.api.pojo.Notification;
 import org.ihtsdo.snowowl.authoring.single.api.pojo.Validation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
 
-import us.monoid.json.JSONException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
 
+@Component
 public class ValidationService {
-	
-	@Autowired
-	SecurityService ims;
-	
-	@Autowired
-	OrchestrationRestClient orchestrationClient;
-	
 
-	public Validation startValidation(String projectKey, String taskKey) throws JSONException, IOException, BusinessServiceException {
-		orchestrationClient.validate("MAIN/" + projectKey + "/" + taskKey);
-		return new Validation (Validation.STATUS_SCHEDULED, "");
+	private static final String VALIDATION_REQUEST_QUEUE = "orchestration.termserver-release-validation";
+	private static final String VALIDATION_RESPONSE_QUEUE = "sca.termserver-release-validation.response";
+	public static final String PATH = "path";
+	public static final String USERNAME = "username";
+	public static final String PROJECT = "project";
+	public static final String TASK = "task";
+	public static final String STATUS = "status";
+
+	@Autowired
+	private MessagingHelper messagingHelper;
+
+	@Autowired
+	private NotificationService notificationService;
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	public Validation startValidation(String projectKey, String taskKey, String username) throws BusinessServiceException {
+		return doStartValidation("MAIN/" + projectKey + "/" + taskKey, username, projectKey, taskKey);
 	}
 
+	public Validation startValidation(String projectKey, String username) throws BusinessServiceException {
+		return doStartValidation("MAIN/" + projectKey, username, projectKey, null);
+	}
+
+	private Validation doStartValidation(String path, String username, String projectKey, String taskKey) throws BusinessServiceException {
+		try {
+			Map<String, String> properties = new HashMap<>();
+			properties.put(PATH, path);
+			properties.put(USERNAME, username);
+			properties.put(PROJECT, projectKey);
+			if (taskKey != null) {
+				properties.put(TASK, taskKey);
+			}
+			messagingHelper.send(VALIDATION_REQUEST_QUEUE, properties, VALIDATION_RESPONSE_QUEUE);
+			return new Validation(Validation.STATUS_SCHEDULED, "");
+		} catch (JsonProcessingException | JMSException e) {
+			throw new BusinessServiceException("Failed to send validation request, please contact support.", e);
+		}
+	}
+
+	@JmsListener(destination = VALIDATION_RESPONSE_QUEUE)
+	public void receiveValidationEvent(TextMessage message) {
+		try {
+			notificationService.queueNotification(
+					message.getStringProperty(MessagingHelper.REQUEST_PROPERTY_NAME_PREFIX + USERNAME),
+					new Notification(
+							message.getStringProperty(MessagingHelper.REQUEST_PROPERTY_NAME_PREFIX + PROJECT),
+							message.getStringProperty(MessagingHelper.REQUEST_PROPERTY_NAME_PREFIX + TASK),
+							EntityType.Validation,
+							message.getStringProperty(STATUS)));
+		} catch (JMSException e) {
+			logger.error("Failed to handle validation event message.", e);
+		}
+	}
 
 	public Validation getValidation(String projectKey, String taskKey) {
 		return new Validation ("NOT YET IMPLEMENTED", "");
-	}
-
-	public Validation startValidation(String projectKey) throws JSONException, IOException, BusinessServiceException {
-		orchestrationClient.validate("MAIN/" + projectKey);
-		return new Validation(Validation.STATUS_SCHEDULED, "");
 	}
 
 	public Validation getValidation(String projectKey) {
