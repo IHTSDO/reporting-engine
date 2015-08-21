f02a9a128fe442e796c1a7e7724965d5d1f78207
Kai Kewley <kaikewley@gmail.com>
2015-08-21 15:16:50 +0100

    WRP-788 message/concept read endpoint.

--- org.ihtsdo.snowowl.authoring.single.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/single/api/review/repository/ReviewMessageRepository.java
+++ org.ihtsdo.snowowl.authoring.single.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/single/api/review/repository/ReviewMessageRepository.java
@@ -2,6 +2,7 @@ package org.ihtsdo.snowowl.authoring.single.api.review.repository;
 
 import org.ihtsdo.snowowl.authoring.single.api.review.domain.Branch;
 import org.ihtsdo.snowowl.authoring.single.api.review.domain.ReviewMessage;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,5 +10,10 @@ import java.util.List;
 public interface ReviewMessageRepository extends CrudRepository<ReviewMessage, Long> {
 
 	List<ReviewMessage> findByBranch(Branch branch);
+
+	@Query("select m from ReviewMessage as m " +
+			"where m.branch = ?1 " +
+			"and ?2 member of m.subjectConceptIds ")
+	List<ReviewMessage> findByBranchAndConcept(Branch branch, String conceptId);
 
 }
--- org.ihtsdo.snowowl.authoring.single.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/single/api/review/service/ReviewService.java
+++ org.ihtsdo.snowowl.authoring.single.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/single/api/review/service/ReviewService.java
@@ -2,17 +2,22 @@ package org.ihtsdo.snowowl.authoring.single.api.review.service;
 
 import org.ihtsdo.snowowl.authoring.single.api.review.domain.Branch;
 import org.ihtsdo.snowowl.authoring.single.api.review.domain.ReviewMessage;
+import org.ihtsdo.snowowl.authoring.single.api.review.domain.ReviewMessageRead;
 import org.ihtsdo.snowowl.authoring.single.api.review.pojo.AuthoringTaskReview;
 import org.ihtsdo.snowowl.authoring.single.api.review.pojo.ReviewConcept;
 import org.ihtsdo.snowowl.authoring.single.api.review.pojo.ReviewMessageCreateRequest;
 import org.ihtsdo.snowowl.authoring.single.api.review.repository.BranchRepository;
+import org.ihtsdo.snowowl.authoring.single.api.review.repository.ReviewMessageReadRepository;
 import org.ihtsdo.snowowl.authoring.single.api.review.repository.ReviewMessageRepository;
 import org.ihtsdo.snowowl.authoring.single.api.service.BranchService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 import java.util.concurrent.ExecutionException;
+import javax.transaction.Transactional;
 
 @Service
 public class ReviewService {
@@ -21,31 +26,72 @@ public class ReviewService {
 	private ReviewMessageRepository messageRepository;
 
 	@Autowired
+	private ReviewMessageReadRepository messageReadRepository;
+
+	@Autowired
 	private BranchRepository branchRepository;
 
 	@Autowired
 	private BranchService branchService;
 
-	public AuthoringTaskReview retrieveTaskReview(String projectKey, String taskKey, List<Locale> locales) throws ExecutionException, InterruptedException {
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Transactional
+	public AuthoringTaskReview retrieveTaskReview(String projectKey, String taskKey, List<Locale> locales, String username) throws ExecutionException, InterruptedException {
 		final AuthoringTaskReview authoringTaskReview = branchService.diffTaskBranch(projectKey, taskKey, locales);
 		final List<ReviewConcept> reviewConcepts = authoringTaskReview.getConcepts();
 		final Branch branch = branchRepository.findOneByProjectAndTask(projectKey, taskKey);
 		if (branch != null) {
-			final List<ReviewMessage> reviewMessages = messageRepository.findByBranch(branch);
-			Map<String, List<ReviewMessage>> conceptMessagesMap = new HashMap<>();
-			for (ReviewMessage reviewMessage : reviewMessages) {
-				for (String conceptId : reviewMessage.getSubjectConceptIds()) {
-					if (!conceptMessagesMap.containsKey(conceptId)) {
-						conceptMessagesMap.put(conceptId, new ArrayList<ReviewMessage>());
-					}
-					conceptMessagesMap.get(conceptId).add(reviewMessage);
-				}
-			}
+			Map<String, List<ReviewMessage>> conceptMessagesMap = getConceptMessagesMap(branch);
+			Map<String, List<ReviewMessage>> conceptMessagesReadMap = getMessagesReadMap(branch, username);
 			for (ReviewConcept reviewConcept : reviewConcepts) {
-				reviewConcept.setMessages(conceptMessagesMap.get(reviewConcept.getId()));
+				final String conceptId = reviewConcept.getId();
+				final List<ReviewMessage> conceptMessages = conceptMessagesMap.get(conceptId);
+				reviewConcept.setMessages(conceptMessages);
+				reviewConcept.setRead(conceptMessages == null || conceptMessages.isEmpty() ||
+						(conceptMessagesReadMap.containsKey(conceptId) && conceptMessagesReadMap.get(conceptId).containsAll(conceptMessages)));
 			}
 		}
 		return authoringTaskReview;
+	}
+
+	/**
+	 * Builds a map of conceptIds and messages with that concept in the subject.
+	 * @param branch
+	 * @return
+	 */
+	private Map<String, List<ReviewMessage>> getConceptMessagesMap(Branch branch) {
+		final List<ReviewMessage> reviewMessages = messageRepository.findByBranch(branch);
+		Map<String, List<ReviewMessage>> conceptMessagesMap = new HashMap<>();
+		for (ReviewMessage reviewMessage : reviewMessages) {
+			for (String conceptId : reviewMessage.getSubjectConceptIds()) {
+				if (!conceptMessagesMap.containsKey(conceptId)) {
+					conceptMessagesMap.put(conceptId, new ArrayList<ReviewMessage>());
+				}
+				conceptMessagesMap.get(conceptId).add(reviewMessage);
+			}
+		}
+		return conceptMessagesMap;
+	}
+
+	/**
+	 * Builds map where key is conceptId and value is a list of read messages for this user.
+	 * @param branch
+	 * @param username
+	 * @return
+	 */
+	private Map<String, List<ReviewMessage>> getMessagesReadMap(Branch branch, String username) {
+		Map<String, List<ReviewMessage>> conceptMessagesReadMap = new HashMap<>();
+		final List<ReviewMessageRead> messageReadStatuses = messageReadRepository.findByReviewMessageBranchAndUser(branch, username);
+		logger.info("messageReadStatuses {}", messageReadStatuses);
+		for (ReviewMessageRead messageReadStatus : messageReadStatuses) {
+			final String conceptId = messageReadStatus.getConceptId();
+			if (!conceptMessagesReadMap.containsKey(conceptId)) {
+				conceptMessagesReadMap.put(conceptId, new ArrayList<ReviewMessage>());
+			}
+			conceptMessagesReadMap.get(conceptId).add(messageReadStatus.getMessage());
+		}
+		return conceptMessagesReadMap;
 	}
 
 	public ReviewMessage postReviewMessage(String projectKey, String taskKey, ReviewMessageCreateRequest createRequest, String fromUsername) {
@@ -55,6 +101,16 @@ public class ReviewService {
 						createRequest.getSubjectConceptIds(), createRequest.isFeedbackRequested(), fromUsername));
 	}
 
+	public void markAsRead(String projectKey, String taskKey, String conceptId, String username) {
+		final Branch branch = branchRepository.findOneByProjectAndTask(projectKey, taskKey);
+		final List<ReviewMessage> messages = messageRepository.findByBranchAndConcept(branch, conceptId);
+		for (ReviewMessage message : messages) {
+			if (messageReadRepository.findOneByMessageAndConceptIdAndUsername(message, conceptId, username) == null) {
+				messageReadRepository.save(new ReviewMessageRead(message, conceptId, username));
+			}
+		}
+	}
+
 	private Branch getCreateBranch(String projectKey, String taskKey) {
 		Branch branch = branchRepository.findOneByProjectAndTask(projectKey, taskKey);
 		if (branch == null) {
@@ -62,5 +118,4 @@ public class ReviewService {
 		}
 		return branch;
 	}
-
 }
