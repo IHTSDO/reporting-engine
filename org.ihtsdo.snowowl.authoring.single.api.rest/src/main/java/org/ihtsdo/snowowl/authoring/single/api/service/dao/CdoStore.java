9ecaab09f8bf2d3763a4eb3aed749ab500857db9
Kai Kewley <kaikewley@gmail.com>
2015-10-15 22:49:00 +0100

    WRP-1256 Cleaner DB connection pooling and autoclose.

--- org.ihtsdo.snowowl.authoring.single.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/single/api/service/dao/CdoStore.java
+++ org.ihtsdo.snowowl.authoring.single.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/single/api/service/dao/CdoStore.java
@@ -1,36 +1,39 @@
 package org.ihtsdo.snowowl.authoring.single.api.service.dao;
 
 import com.b2international.snowowl.api.domain.IComponent;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.dbcp2.Utils;
+import org.apache.commons.lang.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.sql.*;
 import java.util.Date;
 import java.util.*;
 
-import org.apache.commons.dbcp2.Utils;
-import org.apache.commons.lang.time.StopWatch;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class CdoStore {
 
-	public static final String MAIN = "MAIN";
-	
-	PoolConnectionFactory pool;
+	@Autowired
+	private BasicDataSource dataSource;
+
 	private Logger logger = LoggerFactory.getLogger(getClass());
-	
+
+	private static final String MAIN = "MAIN";
+
 	private static final String GET_BRANCH_ID = "select childBranch.id "
 		+ " from cdo_branches childBranch "
 		+ " where childBranch.base_id =  "
 		+ " ( select max(parent.id) from cdo_branches parent where name = ? ) "
 		+ " and childBranch.name = ? ";
-	
+
 	private static final String GET_PROJECT_BRANCH_ID = " select max(childBranch.id) "
 			+ " from cdo_branches childBranch "
 			+ " where childBranch.base_id = 0 "
 			+ " and childBranch.name = ? "; 
 
 	//Union updates from concept, description and relationship tables to get last update times
-	private static final String GET_CONCEPTS_CHANGED_SINCE = 
+	private static final String GET_CONCEPTS_CHANGED_SINCE =
 		" select concept_sctid, max(cdo_created) from ( "
 		+ " select id as concept_sctid, cdo_created "
 		+ " from snomed_concept "
@@ -97,38 +100,33 @@ public class CdoStore {
 	private static final int QUERY_TIMEOUT = 30;
 	
 	public void init() throws SQLException {
-	
-		pool = new PoolConnectionFactory();
-		pool.init();
-
 		logger.info("Ensuring presence of temptable: temp_component");
-		Connection conn = pool.getConn();
-		Statement createTempTableStatement = conn.createStatement();
-		createTempTableStatement.execute(CREATE_TEMP_TABLE);
-		Utils.closeQuietly(conn);
+		try (Connection conn = dataSource.getConnection()) {
+			Statement createTempTableStatement = conn.createStatement();
+			createTempTableStatement.execute(CREATE_TEMP_TABLE);
+		}
 	}
 
 	public Integer getBranchId(String parentBranchName, String childBranchName) throws SQLException {
-		
-		PreparedStatement stmt;
-		Connection conn = pool.getConn();  //Pooled connection will also pool prepared statement if it's been used before
-		if (childBranchName.equals(MAIN)) {
-			//MAIN is always branch 0
-			return new Integer(0);
-		} else if (parentBranchName.equals(MAIN)) {
-			//If we're working with a direct child of MAIN, we need a slightly different SQL
-			stmt = conn.prepareStatement(GET_PROJECT_BRANCH_ID);
-			stmt.setString(1, childBranchName);
-		} else {
-			stmt = conn.prepareStatement(GET_BRANCH_ID);
-			stmt.setString(1, parentBranchName);
-			stmt.setString(2, childBranchName);
+		try (Connection conn = dataSource.getConnection()) {
+			PreparedStatement stmt;
+			if (childBranchName.equals(MAIN)) {
+				//MAIN is always branch 0
+				return 0;
+			} else if (parentBranchName.equals(MAIN)) {
+				//If we're working with a direct child of MAIN, we need a slightly different SQL
+				stmt = conn.prepareStatement(GET_PROJECT_BRANCH_ID);
+				stmt.setString(1, childBranchName);
+			} else {
+				stmt = conn.prepareStatement(GET_BRANCH_ID);
+				stmt.setString(1, parentBranchName);
+				stmt.setString(2, childBranchName);
+			}
+			ResultSet rs = stmt.executeQuery();
+			Integer result = rs.next() ? new Integer(rs.getInt(1)) : null;
+			logger.info("Determined branch id for {}/{}: {}", parentBranchName, childBranchName, result);
+			return result;
 		}
-		ResultSet rs = stmt.executeQuery();
-		Integer result = rs.next() ? new Integer (rs.getInt(1)) : null; 
-		Utils.closeQuietly(conn);
-		logger.info("Determined branch id for {}/{}: {}",parentBranchName, childBranchName,result);
-		return result;
 	}
 	
 	/**
@@ -141,21 +139,21 @@ public class CdoStore {
 	 */
 	public Set<String> getConceptChanges(Integer branchId, Date fromDate) throws SQLException {
 		logger.info("Starting concepts changed query for branch " + branchId );
-		
-		Connection conn = pool.getConn();  //Pooled connection will also pool prepared statement if it's been used before
-		PreparedStatement stmt = conn.prepareStatement(GET_CONCEPTS_CHANGED_SINCE);
-		stmt.setInt(1, branchId);
-		stmt.setInt(2, branchId);
-		stmt.setInt(3, branchId);
-		stmt.setLong(4, fromDate.getTime());
-		ResultSet rs = stmt.executeQuery();
-		Set<String> conceptsChanged = new HashSet<String>();
-		while (rs.next()) {
-			conceptsChanged.add(rs.getString(1));
-		}
-		logger.info("Completed concepts changed for branch " + branchId + " with " + conceptsChanged.size() + " concepts changed.");
+		try (Connection conn = dataSource.getConnection()) {
+			PreparedStatement stmt = conn.prepareStatement(GET_CONCEPTS_CHANGED_SINCE);
+			stmt.setInt(1, branchId);
+			stmt.setInt(2, branchId);
+			stmt.setInt(3, branchId);
+			stmt.setLong(4, fromDate.getTime());
+			ResultSet rs = stmt.executeQuery();
+			Set<String> conceptsChanged = new HashSet<String>();
+			while (rs.next()) {
+				conceptsChanged.add(rs.getString(1));
+			}
+			logger.info("Completed concepts changed for branch " + branchId + " with " + conceptsChanged.size() + " concepts changed.");
 
-		return conceptsChanged; 
+			return conceptsChanged;
+		}
 	}
 	
 	/**
@@ -170,39 +168,39 @@ public class CdoStore {
 		stopWatch.start();
 		logger.info("Starting last updated query for branch " + branchId );
 
-		Connection conn = pool.getConn();
-		if (populateTempTable) {
-			populateTempTable(concepts, transactionId, conn);
-			logger.info("Populated temp table for branchId {}", branchId);
-		}
-		
-		PreparedStatement stmt = conn.prepareStatement(GET_CONCEPTS_LAST_MODIFIED);
-		stmt.setInt(1, branchId);
-		stmt.setInt(2, transactionId);
-		stmt.setInt(3, branchId);
-		stmt.setInt(4, transactionId);
-		stmt.setInt(5, branchId);
-		stmt.setInt(6, transactionId);
-		
-		Map<String, Date> lastUpdatedMap = new HashMap<String, Date>();
-		stmt.setQueryTimeout(QUERY_TIMEOUT);
-		ResultSet rs = stmt.executeQuery();
-		int rowCount = 0;
-		while (rs.next()) {
-			lastUpdatedMap.put(rs.getString(1), new Date(rs.getLong(2)));
-			rowCount++;
-		}
-		logger.info("Recovered " + rowCount + " last updated times for branch {}", branchId );
-		
-		//If we didn't populate the temp table, then it's our turn to clean it up
-		if (!populateTempTable) {
-			cleanTempTable(transactionId, conn);
-		}
-		Utils.closeQuietly(conn);
-		stopWatch.stop();
-		logger.info("Completed last updated for branch " + branchId + " with " + concepts.size() + " conflicts in " + stopWatch);
+		try (Connection conn = dataSource.getConnection()) {
+			if (populateTempTable) {
+				populateTempTable(concepts, transactionId, conn);
+				logger.info("Populated temp table for branchId {}", branchId);
+			}
 
-		return lastUpdatedMap; 
+			PreparedStatement stmt = conn.prepareStatement(GET_CONCEPTS_LAST_MODIFIED);
+			stmt.setInt(1, branchId);
+			stmt.setInt(2, transactionId);
+			stmt.setInt(3, branchId);
+			stmt.setInt(4, transactionId);
+			stmt.setInt(5, branchId);
+			stmt.setInt(6, transactionId);
+
+			Map<String, Date> lastUpdatedMap = new HashMap<String, Date>();
+			stmt.setQueryTimeout(QUERY_TIMEOUT);
+			ResultSet rs = stmt.executeQuery();
+			int rowCount = 0;
+			while (rs.next()) {
+				lastUpdatedMap.put(rs.getString(1), new Date(rs.getLong(2)));
+				rowCount++;
+			}
+			logger.info("Recovered " + rowCount + " last updated times for branch {}", branchId);
+
+			//If we didn't populate the temp table, then it's our turn to clean it up
+			if (!populateTempTable) {
+				cleanTempTable(transactionId, conn);
+			}
+			stopWatch.stop();
+			logger.info("Completed last updated for branch " + branchId + " with " + concepts.size() + " conflicts in " + stopWatch);
+
+			return lastUpdatedMap;
+		}
 	}
 
 	private void populateTempTable(List<IComponent> components, int transactionId, Connection conn) throws SQLException {
