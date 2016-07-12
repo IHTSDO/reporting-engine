6d83892c4e84c88b784a2476a2c0719a000dff79
András Péteri <apeteri@b2international.com>
2016-07-12 17:55:07 +0200

    WRP-1900: Retrieve username directly from authentication

--- org.ihtsdo.snowowl.api.rest.common/src/main/java/org/ihtsdo/snowowl/api/rest/common/ControllerHelper.java
+++ org.ihtsdo.snowowl.api.rest.common/src/main/java/org/ihtsdo/snowowl/api/rest/common/ControllerHelper.java
@@ -1,15 +1,10 @@
 package org.ihtsdo.snowowl.api.rest.common;
 
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public class ControllerHelper {
 
-	public static UserDetails getUserDetails() {
-		return (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-	}
-
 	public static String getUsername() {
-		return getUserDetails().getUsername();
+		return SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
 	}
 }
