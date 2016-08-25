61c9ac5fef248798940fd6b66076d3789a403719
Peter Groves Williams <petergwilliams@hotmail.com>
2016-08-25 11:41:21 +0200

    remove semantic tag filter.  Add project name into report output

--- termserver-scripting/src/main/java/org/ihtsdo/termserver/scripting/fixes/LostRelationships.java
+++ termserver-scripting/src/main/java/org/ihtsdo/termserver/scripting/fixes/LostRelationships.java
@@ -54,10 +54,10 @@ public class LostRelationships extends TermServerFix{
 				String msg = "Concept " + thisConcept.getConceptId() + " has no FSN";
 				criticalErrors.add(msg);
 				println(msg);
-			} else if (!thisConcept.getFsn().contains("(product)")) {
+			} /*else if (!thisConcept.getFsn().contains("(product)")) {
 				debug ("Skipping " + thisConcept);
 				continue;
-			}
+			}*/
 			for(Relationship thisRel : thisConcept.getRelationships(CHARACTERISTIC_TYPE.INFERRED_RELATIONSHIP, ACTIVE_STATE.INACTIVE)) {
 				List<Relationship> replacements = thisConcept.getRelationships(CHARACTERISTIC_TYPE.INFERRED_RELATIONSHIP, thisRel.getType(), ACTIVE_STATE.ACTIVE);
 				if (replacements.size() == 0) {
@@ -84,7 +84,7 @@ public class LostRelationships extends TermServerFix{
 	protected void init(String[] args) throws IOException, TermServerFixException {
 		super.init(args);
 		SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd_HHmmss");
-		String reportFilename = "lost_relationships_" + df.format(new Date()) + "_" + env  + ".csv";
+		String reportFilename = "lost_relationships_" + project.toLowerCase() + "_" + df.format(new Date()) + "_" + env  + ".csv";
 		reportFile = new File(outputDir, reportFilename);
 		reportFile.createNewFile();
 		println ("Outputting Report to " + reportFile.getAbsolutePath());
