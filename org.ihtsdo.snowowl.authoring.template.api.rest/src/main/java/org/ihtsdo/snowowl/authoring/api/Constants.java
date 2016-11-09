15ed1cea97bc3dc9640619e58c70390944637595
Kai Kewley <kaikewley@gmail.com>
2016-11-09 16:25:29 +0000

    Remove old code - first attempt at template authoring module.

--- org.ihtsdo.snowowl.authoring.template.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/api/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.ihtsdo.snowowl.authoring.single.api;
-
-public interface Constants {
-
-	String MAIN = "MAIN";
-
-}
--- org.ihtsdo.snowowl.authoring.template.api.rest/src/main/java/org/ihtsdo/snowowl/authoring/api/model/Model.java
+++
@@ -1,7 +0,0 @@
-package org.ihtsdo.snowowl.authoring.single.api.model;
-
-public interface Model {
-
-	String getName();
-
-}
