2a029743260b4c803a07109c8408b5fae6977a37
Peter Groves Williams <petergwilliams@hotmail.com>
2017-09-25 11:11:44 +0100

    Add support for modifying extisting terms

---
+++ termserver-scripting/src/main/java/org/ihtsdo/termserver/scripting/fixes/ReplaceExistingTerms.java
@@ -0,0 +1,141 @@
+package org.ihtsdo.termserver.scripting.fixes;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.apache.commons.lang.NotImplementedException;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.ihtsdo.termserver.scripting.GraphLoader;
+import org.ihtsdo.termserver.scripting.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.client.SnowOwlClientException;
+import org.ihtsdo.termserver.scripting.domain.Batch;
+import org.ihtsdo.termserver.scripting.domain.Component;
+import org.ihtsdo.termserver.scripting.domain.Concept;
+import org.ihtsdo.termserver.scripting.domain.Description;
+import org.ihtsdo.termserver.scripting.domain.RF2Constants;
+import org.ihtsdo.termserver.scripting.domain.Task;
+import org.ihtsdo.termserver.scripting.util.SnomedUtils;
+
+import us.monoid.json.JSONObject;
+
+/*
+ * Removes a substring from all active Terms, where matched in context for a given subHierarchy
+ */
+public class ReplaceExistingTerms extends BatchFix implements RF2Constants{
+	
+	String subHierarchyStr = "373873005"; // |Pharmaceutical / biologic product (product)|
+	static Map<String, String> replacementMap = new HashMap<String, String>();
+	static final String match = "Product containing";
+	static final String replace =  "Product containing only";
+	
+	protected ReplaceExistingTerms(BatchFix clone) {
+		super(clone);
+	}
+
+	public static void main(String[] args) throws TermServerScriptException, IOException, SnowOwlClientException, InterruptedException {
+		ReplaceExistingTerms fix = new ReplaceExistingTerms(null);
+		try {
+			fix.selfDetermining = true;
+			fix.populateEditPanel = false;
+			fix.init(args);
+			//Recover the current project state from TS (or local cached archive) to allow quick searching of all concepts
+			fix.loadProjectSnapshot(false); //Load all descriptions
+			//We won't incude the project export in our timings
+			fix.startTimer();
+			Batch batch = fix.formIntoBatch();
+			fix.batchProcess(batch);
+			println ("Processing complete.  See results: " + fix.reportFile.getAbsolutePath());
+		} finally {
+			fix.finish();
+		}
+	}
+	
+	protected void init(String[] args) throws TermServerScriptException, IOException {
+		super.init(args);
+	}
+
+	@Override
+	public int doFix(Task task, Concept concept, String info) throws TermServerScriptException {
+		Concept loadedConcept = loadConcept(concept, task.getBranchPath());
+		int changesMade = replaceTerms(task, loadedConcept);
+		if (changesMade > 0) {
+			try {
+				String conceptSerialised = gson.toJson(loadedConcept);
+				if (!dryRun) {
+					debug ("Updating state of " + loadedConcept + info);
+					tsClient.updateConcept(new JSONObject(conceptSerialised), task.getBranchPath());
+				}
+			} catch (Exception e) {
+				report(task, concept, Severity.CRITICAL, ReportActionType.API_ERROR, "Failed to save changed concept to TS: " + ExceptionUtils.getStackTrace(e));
+			}
+		}
+		return changesMade;
+	}
+
+	private int replaceTerms(Task task, Concept concept) throws TermServerScriptException {
+		int changesMade = 0;
+		for (Description d : concept.getDescriptions(ActiveState.ACTIVE)) {
+			if (d.getTerm().startsWith(match)) {
+				String newTerm = d.getTerm().replace(match, replace);
+				if (termAlreadyExists(concept, newTerm)) {
+					report(task, concept, Severity.CRITICAL, ReportActionType.VALIDATION_CHECK, "Replacement term already exists: " + newTerm);
+				} else {
+					Description replacement = d.clone(null);
+					replacement.setTerm(newTerm);
+					d.setActive(false);
+					d.setInactivationIndicator(InactivationIndicator.ERRONEOUS);
+					concept.addDescription(replacement);
+					changesMade++;
+					String msg = "Inactivated term " + d.getDescriptionId() + " replaced with " + newTerm;
+					report(task, concept, Severity.MEDIUM, ReportActionType.DESCRIPTION_CHANGE_MADE, msg);
+				}
+			}
+		}
+		return changesMade;
+	}
+
+	private boolean termAlreadyExists(Concept concept, String newTerm) {
+		boolean termAlreadyExists = false;
+		for (Description description : concept.getDescriptions()) {
+			if (description.getTerm().equals(newTerm)) {
+				termAlreadyExists = true;
+			}
+		}
+		return termAlreadyExists;
+	}
+
+	protected List<Component> identifyComponentsToProcess() throws TermServerScriptException {
+		Set<Concept> allPotential = GraphLoader.getGraphLoader().getConcept(subHierarchyStr).getDescendents(NOT_SET);
+		Set<Concept> allAffected = new TreeSet<Concept>();  //We want to process in the same order each time, in case we restart and skip some.
+		println("Identifying concepts to process");
+		for (Concept c : allPotential) {
+			String semTag = SnomedUtils.deconstructFSN(c.getFsn())[1];
+			if (semTag.equals("(clinical drug)")) {
+				for (Description d : c.getDescriptions(ActiveState.ACTIVE)) {
+					if (d.getTerm().contains(match)) {
+						allAffected.add(c);
+						break;
+					}
+				}
+			}
+		}
+		println ("Identified " + allAffected.size() + " concepts to process");
+		return new ArrayList<Component>(allAffected);
+	}
+
+	@Override
+	protected Concept loadLine(String[] lineItems) throws TermServerScriptException {
+		return null; // We will identify descriptions to edit from the snapshot
+	}
+
+	@Override
+	protected Batch formIntoBatch(String fileName, List<Concept> allConcepts,
+			String branchPath) throws TermServerScriptException {
+		throw new NotImplementedException();
+	}
+}
