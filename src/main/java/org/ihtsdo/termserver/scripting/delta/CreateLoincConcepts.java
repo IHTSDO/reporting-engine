80f91ebb0c721e62cee2be670d6fab22eaaf3f35
Peter Groves Williams <petergwilliams@hotmail.com>
2017-09-27 10:33:55 +0100

    Create LOINC concept in Delta archive from TSV

---
+++ src/main/java/org/ihtsdo/termserver/scripting/delta/CreateLoincConcepts.java
@@ -0,0 +1,220 @@
+package org.ihtsdo.termserver.scripting.delta;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.commons.lang.StringUtils;
+import org.ihtsdo.termserver.scripting.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.client.SnowOwlClientException;
+import org.ihtsdo.termserver.scripting.domain.Concept;
+import org.ihtsdo.termserver.scripting.domain.Description;
+import org.ihtsdo.termserver.scripting.domain.LangRefsetEntry;
+import org.ihtsdo.termserver.scripting.domain.Relationship;
+import org.ihtsdo.termserver.scripting.util.SnomedUtils;
+
+public class CreateLoincConcepts extends DeltaGenerator {
+	
+	String moduleId="900000000000207008";
+	String[] langRefsetIds = new String[] { "900000000000508004",  //GB
+											"900000000000509007" }; //US
+
+	String nameSpace="0";
+	DefinitionStatus defStatus = DefinitionStatus.FULLY_DEFINED;
+	Map<LoincElement, Concept> loincAttributes;
+	
+	enum LoincElement { Component(0),
+						PropertyType(1),
+						TimeAspect(2),
+						DirectSite(3),
+						InheresIn(4),
+						ScaleType(5),
+						LOINC_FSN(6),
+						LOINC_Unique_ID (7),
+						Correlation_ID(8);
+		private int idx;
+		LoincElement(int idx) {
+			this.idx = idx;
+		}
+
+		public int geIdx() {
+			return idx;
+		}
+	}
+	
+	public static void main(String[] args) throws TermServerScriptException, IOException, SnowOwlClientException, InterruptedException {
+		CreateLoincConcepts delta = new CreateLoincConcepts();
+		try {
+			delta.inputFileHasHeaderRow = true;
+			delta.edition="INT";
+			delta.languageCode="en";
+			delta.inputFileDelimiter = TSV_FIELD_DELIMITER;
+			delta.init(args);
+			//Recover the current project state from TS (or local cached archive) to allow quick searching of all concepts
+			delta.loadProjectSnapshot(false);  //Not just FSN, load all terms with lang refset also
+			delta.initMap();
+			delta.startTimer();
+			delta.processFile();
+			delta.closeFiles(); //Need to flush files before zipping
+			SnomedUtils.createArchive(new File(delta.outputDirName));
+		} finally {
+			delta.finish();
+		}
+	}
+	
+	protected void init (String[] args) throws IOException, TermServerScriptException, SnowOwlClientException, SnowOwlClientException {
+		super.init(args);
+		descIdGenerator.setNamespace(nameSpace);
+		descIdGenerator.isExtension(isExtension);
+		
+		for (int x=0; x<args.length; x++) {
+			if (args[x].equals("-m")) {
+				moduleId = args[++x];
+			}
+		}
+		
+		print ("Targetting which namespace? [" + nameSpace + "]: ");
+		String response = STDIN.nextLine().trim();
+		if (!response.isEmpty()) {
+			nameSpace = response;
+		}
+		
+		print ("Targetting which moduleId? [" + moduleId + "]: ");
+		response = STDIN.nextLine().trim();
+		if (!response.isEmpty()) {
+			moduleId = response;
+		}
+		
+		print ("Targetting which language code? [" + languageCode + "]: ");
+		response = STDIN.nextLine().trim();
+		if (!response.isEmpty()) {
+			languageCode = response;
+		}
+		
+		String langRefsetIdStr = StringUtils.join(langRefsetIds, ",");  
+		print ("Targetting which language refset(s)? [" + langRefsetIdStr + "]: ");
+		response = STDIN.nextLine().trim();
+		if (!response.isEmpty()) {
+			langRefsetIds = response.split(COMMA);
+		}
+		
+		print ("What's the Edition? [" + edition + "]: ");
+		response = STDIN.nextLine().trim();
+		if (!response.isEmpty()) {
+			edition = response;
+		}
+		
+		if (moduleId.isEmpty() || langRefsetIds  == null) {
+			String msg = "Require both moduleId and langRefset Id to be specified (-m -l parameters)";
+			throw new TermServerScriptException(msg);
+		}
+	}
+	
+	private void initMap() throws TermServerScriptException {
+		loincAttributes = new HashMap<>();
+		loincAttributes.put(LoincElement.Component, gl.getConcept(246093002L));
+		loincAttributes.put(LoincElement.PropertyType, gl.getConcept(704318007L));
+		loincAttributes.put(LoincElement.TimeAspect, gl.getConcept(370134009L));
+		loincAttributes.put(LoincElement.DirectSite, gl.getConcept(704327008L));
+		loincAttributes.put(LoincElement.InheresIn, gl.getConcept(704319004L));
+		loincAttributes.put(LoincElement.ScaleType, gl.getConcept(370132008L));
+	}
+
+	protected List<Concept> processFile() throws TermServerScriptException {
+		List<Concept> newConcepts = super.processFile();
+		print ("Outputting RF2 files");
+		for (Concept thisConcept : newConcepts) {
+			outputRF2(thisConcept);
+		}
+		println("\nOutputting complete");
+		return null;
+	}
+
+	private void addLangRefsetEntry(Description d, String sctIdAcceptability) {
+		for (String langRefsetId : langRefsetIds) {
+			LangRefsetEntry l = new LangRefsetEntry();
+			l.setId(UUID.randomUUID().toString());
+			l.setRefsetId(langRefsetId);
+			l.setActive(true);
+			l.setEffectiveTime(null);
+			l.setModuleId(moduleId);
+			l.setAcceptabilityId(sctIdAcceptability);
+			l.setReferencedComponentId(d.getDescriptionId());
+			l.setDirty();
+			d.getLangRefsetEntries().add(l);
+		}
+	}
+
+	@Override
+	//Component	PropertyType	TimeAspect	DirectSite	InheresIn	ScaleType	LOINC_FSN	LOINC_Unique_ID	Correlation_ID
+	protected Concept loadLine(String[] lineItems)
+			throws TermServerScriptException {
+		if (lineItems.length > 3) {
+			Concept concept = new Concept(conIdGenerator.getSCTID());
+			concept.setModuleId(moduleId);
+			concept.setDirty();
+			addFsnAndPT(concept, lineItems[LoincElement.LOINC_FSN.geIdx()]);
+			addSyn(concept, "Correlation ID:", lineItems[LoincElement.Correlation_ID.geIdx()]);
+			addSyn(concept, "LOINC Unique ID:", lineItems[LoincElement.LOINC_Unique_ID.geIdx()]);
+			addAttributes(concept, lineItems);
+			concept.setDefinitionStatus(defStatus);
+			return concept;
+		}
+		return null;
+	}
+
+	private void addFsnAndPT(Concept concept, String term) throws TermServerScriptException {
+		Description pt = new Description(descIdGenerator.getSCTID());
+		pt.setTerm(term);
+		pt.setActive(true);
+		pt.setLang(languageCode);
+		pt.setConceptId(concept.getConceptId());
+		pt.setType(DescriptionType.SYNONYM);
+		pt.setCaseSignificance(SCTID_ENTIRE_TERM_CASE_SENSITIVE);
+		pt.setModuleId(moduleId);
+		pt.setDirty();
+		addLangRefsetEntry(pt, SCTID_PREFERRED_TERM);
+		concept.addDescription(pt);
+		Description fsn = pt.clone(descIdGenerator.getSCTID());
+		fsn.setType(DescriptionType.FSN);
+		fsn.setTerm(term + " (observable entity)");
+		concept.setFsn(fsn.getTerm());
+		concept.addDescription(fsn);
+	}
+	
+	private void addSyn(Concept concept, String prefix, String data) throws TermServerScriptException {
+		Description syn = new Description(descIdGenerator.getSCTID());
+		syn.setTerm(prefix + data);
+		syn.setActive(true);
+		syn.setModuleId(moduleId);
+		syn.setLang(languageCode);
+		syn.setType(DescriptionType.SYNONYM);
+		syn.setConceptId(concept.getConceptId());
+		syn.setCaseSignificance(SCTID_ENTIRE_TERM_CASE_SENSITIVE);
+		addLangRefsetEntry(syn, SCTID_ACCEPTABLE_TERM);
+		concept.addDescription(syn);
+	}
+	
+
+	private void addAttributes(Concept concept, String[] lineItems) throws TermServerScriptException {
+		//Work through the attribute map and create stated relationships of the appropriate type
+		for (Map.Entry<LoincElement, Concept> entry : loincAttributes.entrySet()) {
+			Concept source = concept;
+			Concept type = entry.getValue();
+			String targetSctid = lineItems[entry.getKey().geIdx()];
+			Concept target = gl.getConcept(targetSctid);
+			Relationship rel = new Relationship (source, type, target, UNGROUPED);
+			rel.setRelationshipId(relIdGenerator.getSCTID());
+			rel.setModuleId(moduleId);
+			rel.setActive(true);
+			rel.setCharacteristicType(CharacteristicType.STATED_RELATIONSHIP);
+			rel.setModifier(Modifier.EXISTENTIAL);
+			rel.setDirty();
+			concept.addRelationship(rel);
+		}
+	}
+
+}
