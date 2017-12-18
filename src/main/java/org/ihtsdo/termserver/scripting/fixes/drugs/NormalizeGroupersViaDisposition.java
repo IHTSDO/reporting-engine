7cb868af5804b86b295d201e956724e4d4e937f8
Peter Groves Williams <petergwilliams@hotmail.com>
2017-12-18 16:20:06 +0000

    DRUGS-398 Set ProxPrimParent, report additional synonyms, improve case sensitivity handling

--- src/main/java/org/ihtsdo/termserver/scripting/fixes/drugs/NormalizeGroupersViaDisposition.java
+++ src/main/java/org/ihtsdo/termserver/scripting/fixes/drugs/NormalizeGroupersViaDisposition.java
@@ -27,6 +27,8 @@ Normalize the terms (using the disposition) and set the semantic tag to (product
 public class NormalizeGroupersViaDisposition extends DrugBatchFix implements RF2Constants{
 	
 	Map<String, String> substancesMap = new HashMap<>();
+	Relationship newParentRel;
+	String newParent = "763158003"; // |Medicinal product (product)| 
 	
 	protected NormalizeGroupersViaDisposition(BatchFix clone) {
 		super(clone);
@@ -39,7 +41,7 @@ public class NormalizeGroupersViaDisposition extends DrugBatchFix implements RF2
 			fix.reportNoChange = true;
 			fix.populateEditPanel = true;
 			fix.populateTaskDescription = true;
-			fix.additionalReportColumns = "ACTION_DETAIL, ATTRIBUTE_COUNT";
+			fix.additionalReportColumns = "ACTION_DETAIL, ATTRIBUTE_COUNT, SYNONYM_COUNT";
 			fix.init(args);
 			//Recover the current project state from TS (or local cached archive) to allow quick searching of all concepts
 			fix.loadProjectSnapshot(false); //Need full descriptions so we can get PT of target (not loaded from TS)
@@ -53,6 +55,8 @@ public class NormalizeGroupersViaDisposition extends DrugBatchFix implements RF2
 	}
 
 	private void postLoadInit() throws TermServerScriptException {
+		Concept parentConcept =  gl.getConcept(newParent);
+		newParentRel = new Relationship(null, IS_A, parentConcept, 0);
 	}
 
 	@Override
@@ -66,7 +70,7 @@ public class NormalizeGroupersViaDisposition extends DrugBatchFix implements RF2
 			if (countAttributes(loadedConcept) > 0) {
 				loadedConcept.setDefinitionStatus(DefinitionStatus.FULLY_DEFINED);
 				changes++;
-				report (task, loadedConcept, Severity.MEDIUM, ReportActionType.CONCEPT_CHANGE_MADE, "Concept marked as fully defined");
+				report (task, loadedConcept, Severity.LOW, ReportActionType.CONCEPT_CHANGE_MADE, "Concept marked as fully defined");
 			} else {
 				report (task, loadedConcept, Severity.HIGH, ReportActionType.VALIDATION_CHECK, "Unable to mark fully defined - no attributes!");
 			}
@@ -86,8 +90,40 @@ public class NormalizeGroupersViaDisposition extends DrugBatchFix implements RF2
 
 	private int modelGroupers(Task task, Concept loadedConcept) throws TermServerScriptException {
 		
-		String attributeCount = Integer.toString(countAttributes(loadedConcept));
+		int changeCount = setProximalPrimitiveParent(task, loadedConcept);
+		changeCount += setActiveIngredient(task, loadedConcept);
+		return changeCount;
+	}
+
+	private int setProximalPrimitiveParent(Task task, Concept loadedConcept) throws TermServerScriptException {
 		
+		int changeCount = 0;
+		List<Relationship> parentRels = new ArrayList<Relationship> (loadedConcept.getRelationships(CharacteristicType.STATED_RELATIONSHIP, 
+																	IS_A,
+																	ActiveState.ACTIVE));
+		
+		boolean replacementRequired = true;
+		for (Relationship parentRel : parentRels) {
+			//Do we already have the new parent?
+			if (parentRel.getTarget().equals(newParent)) {
+				replacementRequired = false;
+				report(task, loadedConcept, Severity.LOW, ReportActionType.INFO, "Proximal primitive parent already present");
+			} else {
+				remove (task, parentRel, loadedConcept, newParentRel.getTarget().toString(), true);
+				changeCount++;
+			}
+		}
+		
+		if (replacementRequired) {
+			Relationship thisNewParentRel = newParentRel.clone(null);
+			thisNewParentRel.setSource(loadedConcept);
+			loadedConcept.addRelationship(thisNewParentRel);
+		}
+		return changeCount;
+	}
+	
+	private int setActiveIngredient(Task task, Concept loadedConcept) throws TermServerScriptException {
+		String attributeCount = Integer.toString(countAttributes(loadedConcept));
 		//What is our new active ingredient?   Do we have that relationship already?  If not, add ungrouped.
 		Concept target = getTarget(loadedConcept);
 		Relationship targetRel = new Relationship (loadedConcept, HAS_ACTIVE_INGRED, target, 0);
@@ -115,6 +151,19 @@ public class NormalizeGroupersViaDisposition extends DrugBatchFix implements RF2
 		return 1;
 	}
 
+	private void remove(Task t, Relationship rel, Concept c, String retained, boolean isParent) throws TermServerScriptException {
+		//Are we inactivating or deleting this relationship?
+		if (rel.getEffectiveTime() == null || rel.getEffectiveTime().isEmpty()) {
+			c.removeRelationship(rel);
+			String msg = "Deleted parent relationship: " + rel.getTarget() + " in favour of " + retained;
+			report (t, c, Severity.LOW, ReportActionType.RELATIONSHIP_DELETED, msg);
+		} else {
+			rel.setEffectiveTime(null);
+			rel.setActive(false);
+			String msg = "Inactivated parent relationship: " + rel.getTarget() + " in favour of " + retained;
+			report (t, c, Severity.LOW, ReportActionType.RELATIONSHIP_INACTIVATED, msg);
+		}
+	}
 
 	private Concept getTarget(Concept loadedConcept) throws TermServerScriptException {
 		String targetStr = substancesMap.get(loadedConcept.getConceptId());
@@ -131,7 +180,8 @@ public class NormalizeGroupersViaDisposition extends DrugBatchFix implements RF2
 		//A synonym will match the FSN (without semantic tag)
 		
 		//So work out what X is - the PT of the target substance
-		String X = getTarget(loadedConcept).getPreferredSynonym(US_ENG_LANG_REFSET).getTerm();
+		Description xDesc = getTarget(loadedConcept).getPreferredSynonym(US_ENG_LANG_REFSET);
+		String X = xDesc.getTerm();
 		
 		//Do we have a gb_X ?
 		String gbX = null;
@@ -140,7 +190,10 @@ public class NormalizeGroupersViaDisposition extends DrugBatchFix implements RF2
 			gbX = gbDescX.getTerm();
 		}
 		
-		String fsnPartner = "Product containing " + SnomedUtils.deCapitalize(X);
+		CaseSignificance csOfX = xDesc.getCaseSignificance();
+		
+		String xInSentence = csOfX.equals(CaseSignificance.ENTIRE_TERM_CASE_SENSITIVE) ? X : SnomedUtils.deCapitalize(X);
+		String fsnPartner = "Product containing " + xInSentence;
 		String fsn = fsnPartner + " (product)";
 		String pt = X + " product";
 		String gbPT = null;
@@ -148,30 +201,31 @@ public class NormalizeGroupersViaDisposition extends DrugBatchFix implements RF2
 			gbPT = gbX + " product";
 		}
 		
-		replaceFSN(task, loadedConcept, fsn);
-		replacePT(task, loadedConcept, pt, gbPT);
+		replaceFSN(task, loadedConcept, fsn, csOfX);
+		replacePT(task, loadedConcept, pt, gbPT, csOfX);
 		
 		//And add the fsnPartner if it doesn't already exist
 		if (!termAlreadyExists(loadedConcept, fsnPartner)) {
 			Description d = Description.withDefaults(fsnPartner, DescriptionType.SYNONYM);
 			d.setAcceptabilityMap(createAcceptabilityMap(AcceptabilityMode.ACCEPTABLE_BOTH));
+			d.setCaseSignificance(csOfX);
 			loadedConcept.addDescription(d);
 			report (task, loadedConcept, Severity.LOW, ReportActionType.DESCRIPTION_ADDED, fsnPartner);
 		}
 		return 1;
  	}
 
-	private void replaceFSN(Task t, Concept c, String fsn) {
+	private void replaceFSN(Task t, Concept c, String fsn, CaseSignificance cs) throws TermServerScriptException {
 		//Get the FSN and - check we're making a change - replace it.
 		Description fsnDesc = c.getFSNDescription();
 		if (!fsnDesc.getTerm().equals(fsn)) {
-			replaceTerm (t, c, fsnDesc, fsn, AcceptabilityMode.PREFERRED_BOTH);
+			replaceTerm (t, c, fsnDesc, fsn, AcceptabilityMode.PREFERRED_BOTH, cs);
 		} else {
 			report (t, c, Severity.LOW, ReportActionType.NO_CHANGE, "No change required to FSN");
 		}
 	}
 
-	private void replacePT(Task t, Concept c, String pt, String gbPT) throws TermServerScriptException {
+	private void replacePT(Task t, Concept c, String pt, String gbPT, CaseSignificance cs) throws TermServerScriptException {
 		AcceptabilityMode mode = AcceptabilityMode.PREFERRED_BOTH;
 		if (gbPT != null) {
 			mode = AcceptabilityMode.PREFERRED_US;
@@ -179,23 +233,24 @@ public class NormalizeGroupersViaDisposition extends DrugBatchFix implements RF2
 		List<Description> existingPTs = c.getDescriptions(Acceptability.PREFERRED, DescriptionType.SYNONYM, ActiveState.ACTIVE);
 		if (existingPTs.size() == 1) {
 			//We're replacing one PT with two
-			replaceTerm(t, c, existingPTs.get(0), pt, mode);
+			replaceTerm(t, c, existingPTs.get(0), pt, mode, cs);
 			if (gbPT != null) {
 				Description gbDesc = existingPTs.get(0).clone(null);
 				gbDesc.setTerm(gbPT);
 				gbDesc.setActive(true);
 				gbDesc.setAcceptabilityMap(createAcceptabilityMap(AcceptabilityMode.PREFERRED_GB));
+				gbDesc.setCaseSignificance(cs);
 			}
 		} else {
 			Description usPTDesc = c.getPreferredSynonym(US_ENG_LANG_REFSET);
 			Description gbPTDesc = c.getPreferredSynonym(GB_ENG_LANG_REFSET);
 			if (gbPT != null) {
 				//If we have a replacement gbPt, we're replacing each PT with its appropriate partner
-				replaceTerm(t, c, usPTDesc, pt, AcceptabilityMode.PREFERRED_US);
-				replaceTerm(t, c, gbPTDesc, gbPT, AcceptabilityMode.PREFERRED_GB);
+				replaceTerm(t, c, usPTDesc, pt, AcceptabilityMode.PREFERRED_US, cs);
+				replaceTerm(t, c, gbPTDesc, gbPT, AcceptabilityMode.PREFERRED_GB, cs);
 			} else {
 				//Otherwise we're replacing the US preferred term, and inactivating the other.  Warn about this!
-				replaceTerm(t, c, usPTDesc, pt, AcceptabilityMode.PREFERRED_BOTH);
+				replaceTerm(t, c, usPTDesc, pt, AcceptabilityMode.PREFERRED_BOTH, cs);
 				gbPTDesc.setActive(false);
 				gbPTDesc.setInactivationIndicator(InactivationIndicator.NONCONFORMANCE_TO_EDITORIAL_POLICY);
 				report (t, c, Severity.HIGH, ReportActionType.VALIDATION_CHECK, "Replaced US/GB variants with single.  Please check");
@@ -203,15 +258,19 @@ public class NormalizeGroupersViaDisposition extends DrugBatchFix implements RF2
 		}
 	}
 
-	private void replaceTerm(Task t, Concept c, Description d, String newTerm, AcceptabilityMode mode) {
+	private void replaceTerm(Task t, Concept c, Description d, String newTerm, AcceptabilityMode mode, CaseSignificance cs) throws TermServerScriptException {
 		
 		if (termAlreadyExists(c, newTerm)) {
 			report (t, c, Severity.LOW, ReportActionType.NO_CHANGE, "Term already exists: " + newTerm);
 			return;
-		} 
+		}
+
+		//Report count of active acceptable synonyms
+		String activeAcceptableCount = Integer.toString(c.getDescriptions(Acceptability.ACCEPTABLE, DescriptionType.SYNONYM, ActiveState.ACTIVE).size());
 		
 		Description newDesc = d.clone(null);
 		newDesc.setTerm(newTerm);
+		newDesc.setCaseSignificance(cs);
 		
 		//Are we deleting or inactivating the old description?
 		String action = "";
@@ -226,7 +285,7 @@ public class NormalizeGroupersViaDisposition extends DrugBatchFix implements RF2
 		c.addDescription(newDesc);
 		newDesc.setAcceptabilityMap(createAcceptabilityMap(mode));
 		String msg = action + d + " in favour of '" + newTerm + "' (" + mode.toString().toLowerCase() + ")";
-		report (t, c, Severity.LOW, ReportActionType.DESCRIPTION_CHANGE_MADE, msg);
+		report (t, c, Severity.LOW, ReportActionType.DESCRIPTION_CHANGE_MADE, msg, "", activeAcceptableCount);
 	}
 
 	private Integer countAttributes(Concept c) {
