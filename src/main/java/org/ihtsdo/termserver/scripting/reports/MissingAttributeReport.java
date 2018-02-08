3f2797ed26ced5fb3447eea168648d1ccde818f4
Peter Groves Williams <petergwilliams@hotmail.com>
2018-02-08 18:46:40 +0000

    DRUGS-457 Report for specified concepts which are missing a particular attribute

---
+++ src/main/java/org/ihtsdo/termserver/scripting/reports/MissingAttributeReport.java
@@ -0,0 +1,56 @@
+package org.ihtsdo.termserver.scripting.reports;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.List;
+
+import org.ihtsdo.termserver.scripting.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.client.SnowOwlClientException;
+import org.ihtsdo.termserver.scripting.domain.Component;
+import org.ihtsdo.termserver.scripting.domain.Concept;
+
+/**
+ * DRUGS-457
+ * Reports concepts (from a list supplied) that DO NOT have the specified 
+ * attribute of interest
+ */
+public class MissingAttributeReport extends TermServerReport {
+	
+	List<Component> concepts;
+	Concept attributeType;
+	
+	public static void main(String[] args) throws TermServerScriptException, IOException, SnowOwlClientException {
+		MissingAttributeReport report = new MissingAttributeReport();
+		try {
+			report.init(args);
+			report.loadProjectSnapshot(false);  //Load all descriptions
+			report.postLoadInit();
+			report.runMissingAttributeReport();
+		} catch (Exception e) {
+			println("Failed to produce MissingAttributeReport due to " + e.getMessage());
+			e.printStackTrace(new PrintStream(System.out));
+		} finally {
+			report.finish();
+		}
+	}
+
+	private void postLoadInit() throws TermServerScriptException {
+		attributeType = gl.getConcept("411116001"); // |Has manufactured dose form (attribute)|
+	}
+
+	private void runMissingAttributeReport() throws TermServerScriptException {
+		List<Concept> concepts = asConcepts(processFile(inputFile));
+		for (Concept c : concepts) {
+			//If we don't have the FSN then project doesn't yet know about this concept
+			if (c.getFsn() == null || c.getFsn().isEmpty()) {
+				report (c, "Not yet available in project - status unknown");
+				incrementSummaryInformation("Concepts unavailable in project");
+			} else if (c.getRelationships(CharacteristicType.STATED_RELATIONSHIP, attributeType, ActiveState.ACTIVE).size()==0) {
+				report (c, "Missing " + attributeType);
+				incrementSummaryInformation("Issues encountered");
+			}
+		}
+		addSummaryInformation("Concepts checked", concepts.size());
+	}
+	
+}
