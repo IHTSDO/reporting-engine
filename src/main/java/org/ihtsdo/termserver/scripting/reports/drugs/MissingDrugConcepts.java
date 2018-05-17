d2bb8608b6c491190050cc5cf19c5c36321e1c74
Peter Groves Williams <petergwilliams@hotmail.com>
2018-05-17 10:57:43 +0100

    DRUGS-534, DRUGS-535 Queries to identify missing MP and MPF 'only' concepts

--- src/main/java/org/ihtsdo/termserver/scripting/reports/drugs/MissingDrugConcepts.java
+++ src/main/java/org/ihtsdo/termserver/scripting/reports/drugs/MissingDrugConcepts.java
@@ -9,13 +9,21 @@ import org.ihtsdo.termserver.scripting.domain.Concept;
 import org.ihtsdo.termserver.scripting.reports.TermServerReport;
 
 /**
- * DRUGS-289 Report to list any ingredients that use different units across products
+ * Reports to identify missing drug concepts.
+ * 
+ * DRUGS-534 : Query to identify CD concepts that are sufficiently defined 
+ * with stated parent |Medicinal product| and that do have an inferred MPF-only parent
+ * 
+ * DRUGS-535 : Query to identify MP concepts that are sufficiently defined 
+ * with stated parent |Medicinal product| and that do have an inferred MP-only parent
+ * 
  */
 public class MissingDrugConcepts extends TermServerReport {
 	
 	private static final String CD = "(clinical drug)";
 	private static final String MPF = "(medicinal product form)";
 	private static final String MP = "(medicinal product)";
+	private static final String ONLY = " only ";
 	
 	Concept subHierarchy;
 	
@@ -27,7 +35,9 @@ public class MissingDrugConcepts extends TermServerReport {
 			report.loadProjectSnapshot(false);  //Load all descriptions
 			report.postInit();
 			//report.runIdentifyMissingMPConceptsReport();
-			report.runIdentifyMissingMPFConceptsReport();
+			//report.runIdentifyMissingMPFConceptsReport();
+			report.runIdentifyMissingMPFOnlyConceptsReport(); //DRUGS-534
+			//report.runIdentifyMissingMPOnlyConceptsReport(); //DRUGS-535
 		} catch (Exception e) {
 			info("Failed to produce MissingDrugConcepts Report due to " + e.getMessage());
 			e.printStackTrace(new PrintStream(System.out));
@@ -58,7 +68,7 @@ public class MissingDrugConcepts extends TermServerReport {
 			}
 			incrementSummaryInformation("Concepts checked");
 		}
-	} */
+	} 
 	
 	private void runIdentifyMissingMPFConceptsReport() throws TermServerScriptException {
 		for (Concept c : subHierarchy.getDescendents(NOT_SET)) {
@@ -85,6 +95,46 @@ public class MissingDrugConcepts extends TermServerReport {
 			incrementSummaryInformation("Concepts checked");
 		}
 	}
+	
+	private void runIdentifyMissingMPOnlyConceptsReport() throws TermServerScriptException {
+		for (Concept c : subHierarchy.getDescendents(NOT_SET)) {
+			// MP concepts that are sufficiently defined
+			if (c.isActive() && c.getFsn().contains(MP) && c.getDefinitionStatus().equals(DefinitionStatus.FULLY_DEFINED)) {
+				//...with stated parent |Medicinal product| 
+				if (hasStatedParent (c, MEDICINAL_PRODUCT)) {
+					//...and that have an inferred parent which is MP "Only"
+					for (Concept parent : c.getParents(CharacteristicType.INFERRED_RELATIONSHIP)) {
+						if (parent.getFsn().contains(MP) && parent.getFsn().contains(ONLY)) {
+							report (c, parent.toString());
+							incrementSummaryInformation("Concepts reported");
+						}
+					}
+				}
+				incrementSummaryInformation("FD MPs with |Medicinal Product| stated parent checked");
+			}
+			incrementSummaryInformation("Concepts checked");
+		}
+	} */
+	
+	private void runIdentifyMissingMPFOnlyConceptsReport() throws TermServerScriptException {
+		for (Concept c : subHierarchy.getDescendents(NOT_SET)) {
+			// CD concepts that are sufficiently defined
+			if (c.isActive() && c.getFsn().contains(CD) && c.getDefinitionStatus().equals(DefinitionStatus.FULLY_DEFINED)) {
+				//...with stated parent |Medicinal product| 
+				if (hasStatedParent (c, MEDICINAL_PRODUCT)) {
+					//...and that have an inferred parent which is MPF "Only"
+					for (Concept parent : c.getParents(CharacteristicType.INFERRED_RELATIONSHIP)) {
+						if (parent.getFsn().contains(MPF) && parent.getFsn().contains(ONLY)) {
+							report (c, parent.toString());
+							incrementSummaryInformation("Concepts reported");
+						}
+					}
+				}
+				incrementSummaryInformation("FD MPs with |Medicinal Product| stated parent checked");
+			}
+			incrementSummaryInformation("Concepts checked");
+		}
+	} 
 
 	private boolean hasStatedParent(Concept c, Concept targetParent) {
 		for (Concept parent : c.getParents(CharacteristicType.STATED_RELATIONSHIP)) {
