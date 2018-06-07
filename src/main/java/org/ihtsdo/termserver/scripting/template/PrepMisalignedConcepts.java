2a2d4d26ac2e5b29107bb4ffbbcabbe193e75a1d
Peter Groves Williams <petergwilliams@hotmail.com>
2018-06-07 11:13:16 +0100

    QI-5 Add birth trauma template

--- src/main/java/org/ihtsdo/termserver/scripting/template/PrepMisalignedConcepts.java
+++ src/main/java/org/ihtsdo/termserver/scripting/template/PrepMisalignedConcepts.java
@@ -58,20 +58,21 @@ public class PrepMisalignedConcepts extends TemplateFix {
 		runStandAlone = true; 
 		additionalReportColumns = "CharacteristicType, MatchedTemplate, Template Diagnostic";
 		
-		/*subHierarchyStr = "125605004";  // QI-5 |Fracture of bone (disorder)|
+		subHierarchyStr = "125605004";  // QI-5 |Fracture of bone (disorder)|
 		templateNames = new String[] {	"Fracture of Bone Structure.json",
 										"Fracture Dislocation of Bone Structure.json",
 										"Pathologic fracture of bone due to Disease.json",
 										"Pathologic fracture morphology of bone structure co-occurrent and due to Neoplasm of bone.json",
-										"Traumatic abnormality of spinal cord structure co-occurrent and due to fracture morphology of vertebral bone structure.json" };
+										"Traumatic abnormality of spinal cord structure co-occurrent and due to fracture morphology of vertebral bone structure.json",
+										"Injury of finding site due to birth trauma.json"};
 		/*
 		//subHierarchyStr =  "128294001";  // QI-9 |Chronic inflammatory disorder (disorder)
-		//templateNames = new String[] {"Chronic Inflammatory Disorder.json"}; */
+		//templateNames = new String[] {"Chronic Inflammatory Disorder.json"}; 
 		
 		subHierarchyStr =  "126537000";  //QI-14 |Neoplasm of bone (disorder)|
 		templateNames = new String[] {	"Neoplasm of Bone.json",
 										"Pathologic fracture morphology of bone structure co-occurrent and due to Neoplasm of bone.json"};
-		
+		*/
 		/*subHierarchyStr =  "34014006"; //QI-15 |Viral disease (disorder)|
 		templateNames = new String[] {	"Infection caused by Virus.json",
 										"Infection of bodysite caused by virus.json"};
