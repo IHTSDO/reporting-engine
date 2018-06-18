4f859d64f56788737cf76039be1d6895cd697f9d
Peter Groves Williams <petergwilliams@hotmail.com>
2018-06-18 17:38:39 +0100

    QI Add report to get metrics based on probable template types

---
+++ src/main/java/org/ihtsdo/termserver/scripting/reports/qi/GenerateWorkDoneStatsWithTempateTypes.java
@@ -0,0 +1,147 @@
+package org.ihtsdo.termserver.scripting.reports.qi;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.*;
+
+import org.ihtsdo.termserver.scripting.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.client.SnowOwlClientException;
+import org.ihtsdo.termserver.scripting.domain.*;
+import org.ihtsdo.termserver.scripting.reports.TermServerReport;
+import org.ihtsdo.termserver.scripting.util.SnomedUtils;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+
+/**
+ * QI
+ * Report changes made in this authoring cycle for specific sub-hierarchies.
+ */
+public class GenerateWorkDoneStatsWithTempateTypes extends TermServerReport {
+	
+	List<Concept> subHierarchies;
+	GenerateInitialAnalysis ipReport = new GenerateInitialAnalysis();
+	int modifiedSince = 20180131;
+	
+	String [] co_occurrantWords = new String[] { " and ", " with ", " in " };
+	Concept[] co_occurrantTypeAttrb;
+	Concept[] complexTypeAttrbs;
+	
+	enum TemplateType {SIMPLE, PURE_CO, COMPLEX, NONE};
+	
+	public static void main(String[] args) throws TermServerScriptException, IOException, SnowOwlClientException {
+		GenerateWorkDoneStatsWithTempateTypes report = new GenerateWorkDoneStatsWithTempateTypes();
+		try {
+			report.additionalReportColumns = "FSN, Simple, Pure, Complex, Total";
+			report.init(args);
+			report.loadProjectSnapshot(false);  //Load all descriptions
+			report.postLoadInit();
+			report.generateWorkDoneStats();
+		} catch (Exception e) {
+			info("Failed to produce MissingAttributeReport due to " + e.getMessage());
+			e.printStackTrace(new PrintStream(System.out));
+		} finally {
+			report.finish();
+		}
+	}
+
+	private void postLoadInit() throws TermServerScriptException, IOException {
+		subHierarchies = new ArrayList<>();
+		info ("Loading " + inputFile);
+		if (!inputFile.canRead()) {
+			throw new TermServerScriptException ("Cannot read: " + inputFile);
+		}
+		List<String> lines = Files.readLines(inputFile, Charsets.UTF_8);
+		for (String line : lines) {
+			if (!line.trim().isEmpty()) {
+				subHierarchies.add(gl.getConcept(line));
+			}
+		}
+		
+		co_occurrantTypeAttrb =  new Concept[] {
+				gl.getConcept("47429007") //|Associated with (attribute)|
+		};
+		
+		complexTypeAttrbs = new Concept[] {
+			gl.getConcept("42752001"), //|Due to (attribute)|
+			gl.getConcept("726633004"), //|Temporally related to (attribute)|
+			gl.getConcept("255234002"), //|After (attribute)|
+			gl.getConcept("288556008"), //|Before (attribute)|
+			gl.getConcept("371881003"), //|During (attribute)|
+			gl.getConcept("363713009"), //|Has interpretation (attribute)|
+			gl.getConcept("363714003") //|Interprets (attribute)|
+		};
+	}
+
+	private void generateWorkDoneStats() throws TermServerScriptException {
+		ipReport.setQuiet(true);
+		for (Concept subHierarchyStart : subHierarchies) {
+			int[] templateTypeTotal = new int[4];
+			int[] templateTypeModified = new int[4];
+			debug ("Analysing subHierarchy: " + subHierarchyStart);
+			Set<Concept> subHierarchy = descendantsCache.getDescendentsOrSelf(subHierarchyStart);
+			int total = subHierarchy.size();
+			for (Concept c : subHierarchy) {
+				TemplateType type = getTemplateType(c);
+				templateTypeTotal[type.ordinal()]++;
+				//Has this concept been modified in the stated form since X?
+				if (isModified(c, CharacteristicType.STATED_RELATIONSHIP)) {
+					templateTypeModified[type.ordinal()]++;
+				}
+			}
+			
+			report (subHierarchyStart, templateTypeTotal[0] + "/" + templateTypeModified[0],
+					templateTypeTotal[1] + "/" + templateTypeModified[1],
+					templateTypeTotal[2] + "/" + templateTypeModified[2],
+					templateTypeTotal[3] + "/" + templateTypeModified[3],
+					total);
+		}
+	}
+
+	private TemplateType getTemplateType(Concept c) throws TermServerScriptException {
+		//Zero case, do we in fact have no attributes?
+		if (countAttributes(c, CharacteristicType.INFERRED_RELATIONSHIP) == 0) {
+			return TemplateType.NONE;
+		}
+		
+		//Firstly, if we have any of the complex attribute types 
+		if (SnomedUtils.getTargets(c, complexTypeAttrbs, CharacteristicType.INFERRED_RELATIONSHIP).size() > 0) {
+			return TemplateType.COMPLEX;
+		}
+		
+		//Do we have a pure co-occurrent type
+		if (SnomedUtils.getTargets(c, co_occurrantTypeAttrb, CharacteristicType.INFERRED_RELATIONSHIP).size() > 0) {
+			return TemplateType.PURE_CO;
+		}
+		
+		//Do we have a simple co-occurrent word?
+		for (String word : co_occurrantWords) {
+			if (c.getFsn().contains(word)) {
+				return TemplateType.PURE_CO;
+			}
+		}
+		
+		//Otherwise we'll assume it's simple
+		return TemplateType.SIMPLE;
+	}
+
+	private boolean isModified(Concept c, CharacteristicType charType) throws TermServerScriptException {
+		for (Relationship r : c.getRelationships(charType, ActiveState.BOTH)) {
+			if (r.getEffectiveTime() == null || Integer.parseInt(r.getEffectiveTime()) > modifiedSince) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private Integer countAttributes(Concept c, CharacteristicType charType) {
+		int attributeCount = 0;
+		for (Relationship r : c.getRelationships(charType, ActiveState.ACTIVE)) {
+			if (!r.getType().equals(IS_A)) {
+				attributeCount++;
+			}
+		}
+		return attributeCount;
+	}
+	
+}
