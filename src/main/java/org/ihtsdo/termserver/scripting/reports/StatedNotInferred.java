31bd69c2ebbff4d78dcecb5fddc113d6b666acef
Peter Groves Williams <petergwilliams@hotmail.com>
2018-06-29 14:56:54 +0100

    SUBST-267 Report to identify redudant dispositions

---
+++ src/main/java/org/ihtsdo/termserver/scripting/reports/StatedNotInferred.java
@@ -0,0 +1,52 @@
+package org.ihtsdo.termserver.scripting.reports;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.*;
+import org.ihtsdo.termserver.scripting.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.client.SnowOwlClientException;
+import org.ihtsdo.termserver.scripting.domain.*;
+
+/**
+ * SUBST-267 Find any attributes that have been stated, which do not exactly
+ * exist in the inferred form - ie the classifier has removed them as redundant.
+ */
+public class StatedNotInferred extends TermServerReport {
+	
+	Concept subHierarchy = SUBSTANCE;
+	Concept attributeType = HAS_DISPOSITION;
+	
+	public static void main(String[] args) throws TermServerScriptException, IOException, SnowOwlClientException {
+		StatedNotInferred report = new StatedNotInferred();
+		try {
+			report.additionalReportColumns = "FSN, Redundant Disposition";
+			report.init(args);
+			report.loadProjectSnapshot(false);  //Load all descriptions
+			report.runStatedNotInferredReport();
+		} catch (Exception e) {
+			info("Failed to produce StatedNotInferred Report due to " + e.getMessage());
+			e.printStackTrace(new PrintStream(System.out));
+		} finally {
+			report.finish();
+		}
+	}
+
+	private void runStatedNotInferredReport() throws TermServerScriptException {
+		Collection<Concept> subHierarchy = gl.getConcept(this.subHierarchy.getConceptId()).getDescendents(NOT_SET);
+		info ("Finding redundant relationships");
+		for (Concept c : subHierarchy) {
+			for (Relationship r : c.getRelationships(CharacteristicType.STATED_RELATIONSHIP, attributeType, ActiveState.ACTIVE)) {
+				//How many of these do we have?
+				int match = c.getRelationships(CharacteristicType.INFERRED_RELATIONSHIP, r.getType(), r.getTarget(), ActiveState.ACTIVE).size();
+				if (match == 0) {
+					incrementSummaryInformation("Issues Encountered");
+					report (c, r.getTarget());
+				}
+			}
+		}
+		addSummaryInformation("Concepts checked", subHierarchy.size());
+	}
+
+	
+	
+}
