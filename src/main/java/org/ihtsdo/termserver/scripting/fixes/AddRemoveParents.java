b46fbf6caaf69ecb5215aed5d5fcf6f165db2c87
Peter Groves Williams <petergwilliams@hotmail.com>
2018-07-12 15:40:10 +0100

    Anatomy run

--- src/main/java/org/ihtsdo/termserver/scripting/fixes/AddRemoveParents.java
+++ src/main/java/org/ihtsdo/termserver/scripting/fixes/AddRemoveParents.java
@@ -115,8 +115,8 @@ public class AddRemoveParents extends BatchFix implements RF2Constants{
 			report (null, c, Severity.CRITICAL, ReportActionType.VALIDATION_CHECK, "Cannot modify concept - is inactive");
 			return null;
 		}
-		if (lineItems[2].equals(ACTIVE_FLAG)) {
-		//if (!lineItems[2].equals(ACTIVE_FLAG)) {
+		//if (lineItems[2].equals(ACTIVE_FLAG)) {
+		if (!lineItems[2].equals(ACTIVE_FLAG)) {
 			RelationshipGroup g = changeMap.get(c);
 			if (g == null) {
 				g = new RelationshipGroup(UNGROUPED);
