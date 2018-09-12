7f026baa715e1c52c462c16d5252ec2e70b58756
Peter Groves Williams <petergwilliams@hotmail.com>
2018-09-12 11:01:16 +0100

    ISRS-391 ISRS-392 Correct module id on inactive components

---
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/fixes/CorrectModuleId.java
@@ -0,0 +1,104 @@
+package org.ihtsdo.termserver.scripting.fixes;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.commons.lang.NotImplementedException;
+import org.ihtsdo.termserver.scripting.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.ValidationFailure;
+import org.ihtsdo.termserver.scripting.client.SnowOwlClientException;
+import org.ihtsdo.termserver.scripting.dao.ReportSheetManager;
+import org.ihtsdo.termserver.scripting.domain.*;
+import org.ihtsdo.termserver.scripting.fixes.BatchFix;
+
+/*
+ * ISRS-391, ISRS-392
+ * Where a description or relationship belongs to a module that differs
+ * from that of the host concept, correct that.
+ * Applies to both active and inactive concepts.
+*/
+public class CorrectModuleId extends BatchFix implements RF2Constants{
+	
+	protected CorrectModuleId(BatchFix clone) {
+		super(clone);
+	}
+
+	public static void main(String[] args) throws TermServerScriptException, IOException, SnowOwlClientException, InterruptedException {
+		CorrectModuleId fix = new CorrectModuleId(null);
+		try {
+			ReportSheetManager.targetFolderId = "15WXT1kov-SLVi4cvm2TbYJp_vBMr4HZJ";  //Release QA
+			fix.populateEditPanel = false;
+			fix.selfDetermining = true;
+			fix.init(args);
+			fix.loadProjectSnapshot(false); //Load all descriptions
+			fix.processFile();
+		} finally {
+			fix.finish();
+		}
+	}
+
+	@Override
+	public int doFix(Task task, Concept concept, String info) throws TermServerScriptException, ValidationFailure {
+		//We will not load the concept because the Browser endpoint does not populate the full array of inactivation indicators
+		int changesMade = 0;
+		try {
+			Concept loadedConcept = loadConcept(concept, task.getBranchPath());
+			changesMade = fixModuleId(task, loadedConcept);
+			if (changesMade > 0) {
+				updateConcept(task, loadedConcept, info);
+			}
+		} catch (TermServerScriptException e) {
+			throw new TermServerScriptException ("Failed to remove duplicate inactivation indicator on " + concept, e);
+		}
+		return changesMade;
+	}
+
+	private int fixModuleId(Task t, Concept c) throws TermServerScriptException {
+		int changesMade = 0;
+		for (Description d : c.getDescriptions()) {
+			if (!d.getModuleId().equals(c.getModuleId())) {
+				report (t, c, Severity.LOW, ReportActionType.MODULE_CHANGE_MADE, d, d.getModuleId() + " -> " + c.getModuleId());
+				d.setModuleId(c.getModuleId());
+				changesMade++;
+			}
+		}
+		
+		for (Relationship r : c.getRelationships(CharacteristicType.STATED_RELATIONSHIP, ActiveState.BOTH)) {
+			if (!r.getModuleId().equals(c.getModuleId())) {
+				report (t, c, Severity.LOW, ReportActionType.MODULE_CHANGE_MADE, r, r.getModuleId() + " -> " + c.getModuleId());
+				r.setModuleId(c.getModuleId());
+				changesMade++;
+			}
+		}
+		return changesMade;
+	}
+
+	@Override
+	protected List<Component> identifyComponentsToProcess() throws TermServerScriptException {
+		//Work through all inactive concepts and check the inactivation indicator on all
+		//active descriptions
+		info ("Identifying concepts to process");
+		List<Concept> processMe = new ArrayList<Concept>();
+		setQuiet(true);
+		for (Concept c : ROOT_CONCEPT.getDescendents(NOT_SET)) {
+			if (!c.getConceptId().equals("277040004")) {
+			//	continue;
+			}
+			if (fixModuleId(null, c.cloneWithIds()) > 0) {
+				processMe.add(c);
+			}
+		}
+		setQuiet(false);
+		info ("Identified " + processMe.size() + " concepts to process");
+		processMe.sort(Comparator.comparing(Concept::getFsn));
+		return new ArrayList<Component>(processMe);
+	}
+
+	
+	@Override
+	protected List<Component> loadLine(String[] lineItems)
+			throws TermServerScriptException {
+		throw new NotImplementedException("This class self determines concepts to process");
+	}
+
+}
