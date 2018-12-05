aa217061eabb92191a58b1bb9ce3dfa7d9cee43f
Peter Groves Williams <petergwilliams@hotmail.com>
2018-12-05 12:33:30 +0000

    QI-153 Add template for general infection (not previously covered)

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/template/PrepMisalignedConcepts.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/template/PrepMisalignedConcepts.java
@@ -122,11 +122,16 @@ public class PrepMisalignedConcepts extends TemplateFix {
 		exclusionWords = new ArrayList<String>();
 		exclusionWords.add("gangrene");
 		exclusionWords.add("obstruction");
-		*/
 		
 		subHierarchyStr = "432119003 |Aneurysm (disorder)|"; //QI-143 
 		templateNames = new String[] {	"templates/Aneurysm of Cardiovascular system.json" };
-				
+		*/
+		subHierarchyStr = "40733004|Infectious disease|"; //QI-153
+		templateNames = new String[] {	"templates/Infection NOS.json" };
+		setExclusions(new String[] {"87628006 |Bacterial infectious disease (disorder)|","34014006 |Viral disease (disorder)|",
+				"3218000 |Mycosis (disorder)|","8098009 |Sexually transmitted infectious disease (disorder)|", 
+				"17322007 |Disease caused by parasite (disorder)|", "91302008 |Sepsis (disorder)|"});
+	
 		super.init(args);
 	}
 	
