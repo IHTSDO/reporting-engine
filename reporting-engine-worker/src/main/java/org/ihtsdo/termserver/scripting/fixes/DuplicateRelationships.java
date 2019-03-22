f7aeacb7242cf50eca7ea7da43e34353704f5073
Peter Groves Williams <petergwilliams@hotmail.com>
2019-03-22 19:17:25 +0000

    INFRA-3449 Add class to selectively remove duplicate relationships

---
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/fixes/DuplicateRelationships.java
@@ -0,0 +1,137 @@
+package org.ihtsdo.termserver.scripting.fixes;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.ihtsdo.termserver.scripting.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.ValidationFailure;
+import org.ihtsdo.termserver.scripting.client.TermServerClientException;
+import org.ihtsdo.termserver.scripting.dao.ReportSheetManager;
+import org.ihtsdo.termserver.scripting.domain.*;
+import org.ihtsdo.termserver.scripting.fixes.BatchFix;
+
+
+/* INFRA-3449 Fix issue when the same relationship exists as both active and inactive
+ */
+public class DuplicateRelationships extends BatchFix implements RF2Constants{
+	
+	protected DuplicateRelationships(BatchFix clone) {
+		super(clone);
+	}
+
+	public static void main(String[] args) throws TermServerScriptException, IOException, TermServerClientException, InterruptedException {
+		DuplicateRelationships fix = new DuplicateRelationships(null);
+		try {
+			ReportSheetManager.targetFolderId = "1fIHGIgbsdSfh5euzO3YKOSeHw4QHCM-m";  //Ad-Hoc Batch Updates
+			fix.runStandAlone = false;  //Was causing issues with historical associations not being set
+			fix.selfDetermining = true;
+			fix.init(args);
+			//Recover the current project state from TS (or local cached archive) to allow quick searching of all concepts
+			fix.loadProjectSnapshot(true); //Just 
+			fix.postInit();
+			fix.processFile();
+		} finally {
+			fix.finish();
+		}
+	}
+
+	@Override
+	public int doFix(Task task, Concept concept, String info) throws TermServerScriptException, ValidationFailure {
+		Concept tsConcept = loadConcept(concept, task.getBranchPath());
+		int changesMade = removeRedundantRelationships(task, tsConcept);
+		if (changesMade > 0) {
+			updateConcept(task, tsConcept, info);
+		}
+		return changesMade;
+	}
+
+	private int removeRedundantRelationships(Task t, Concept c) throws TermServerScriptException {
+		int changesMade = 0;
+		for (Relationship r : c.getRelationships(CharacteristicType.STATED_RELATIONSHIP, ActiveState.ACTIVE)) {
+			List<Relationship> duplicates = c.getRelationships(r, ActiveState.BOTH);
+			if (duplicates.size() > 1) {
+				//Has one been released?
+				List<Relationship> released = duplicates.stream().filter(l -> l.isReleased()).collect(Collectors.toList());
+				Relationship champion = null;
+				if (released.size() == 1) {
+					champion = released.get(0);
+				} else if (released.size() > 1) {
+					Severity severity = duplicates.size() == released.size() ? Severity.LOW : Severity.HIGH;
+					report(t, c, severity, ReportActionType.VALIDATION_CHECK, "Concept has > 1 release duplicate relationships", r);
+					t.remove(c);
+					return NO_CHANGES_MADE;
+				}
+				duplicates.removeAll(released);
+				for (Relationship duplicate : duplicates) {
+					if (champion == null) {
+						//If we haven't found one to keep yet, keep the first one.
+						champion = duplicate;
+					} else {
+						changesMade += removeRelationship(t, c, duplicate);
+					}
+				}
+				report (t, c, Severity.LOW, ReportActionType.INFO, "Retained: " + champion);
+			}
+		}
+		return changesMade;
+	}
+
+	@Override
+	protected List<Component> identifyComponentsToProcess() throws TermServerScriptException {
+		//Find concepts with two relationships the same triple + group, but different Ids
+		info ("Identifying concepts to process");
+		List<Concept> processMe = new ArrayList<>();
+		
+		//Code to protect us from SCTIDs changing between released and TS files is 
+		//Also preventing us from seeing duplicates, so I'll have to hard code the list
+		
+		processMe.add(gl.getConcept("14311001"));
+		processMe.add(gl.getConcept("18665000"));
+		processMe.add(gl.getConcept("18665000"));
+		processMe.add(gl.getConcept("19429009"));
+		processMe.add(gl.getConcept("39023007"));
+		processMe.add(gl.getConcept("39023007"));
+		processMe.add(gl.getConcept("74046003"));
+		processMe.add(gl.getConcept("74046003"));
+		processMe.add(gl.getConcept("78324009"));
+		processMe.add(gl.getConcept("78712000"));
+		processMe.add(gl.getConcept("111204001"));
+		processMe.add(gl.getConcept("111204001"));
+		processMe.add(gl.getConcept("111204001"));
+		processMe.add(gl.getConcept("111204001"));
+		processMe.add(gl.getConcept("128600008"));
+		processMe.add(gl.getConcept("195657008"));
+		processMe.add(gl.getConcept("195657008"));
+		processMe.add(gl.getConcept("195668008"));
+		processMe.add(gl.getConcept("195668008"));
+		processMe.add(gl.getConcept("195681001"));
+		processMe.add(gl.getConcept("195681001"));
+		processMe.add(gl.getConcept("427921009"));
+		processMe.add(gl.getConcept("429354005"));
+		processMe.add(gl.getConcept("429354005"));
+		processMe.add(gl.getConcept("430935001"));
+		processMe.add(gl.getConcept("442159003"));
+		processMe.add(gl.getConcept("442159003"));
+		processMe.add(gl.getConcept("444546002"));
+		processMe.add(gl.getConcept("699211004"));
+		
+		/*
+		//for (Concept c : gl.getAllConcepts()) {
+		for (Concept c : Collections.singletonList(gl.getConcept("14311001"))) {
+			if (!c.isActive()) {
+				continue;
+			}
+			for (Relationship r : c.getRelationships(CharacteristicType.STATED_RELATIONSHIP, ActiveState.ACTIVE)) {
+				if (c.getRelationships(r, ActiveState.BOTH).size() > 1) {
+					processMe.add(c);
+				}
+			}
+		}
+		*/
+		info ("Identified " + processMe.size() + " concepts to process");
+		processMe.sort(Comparator.comparing(Concept::getFsn));
+		return new ArrayList<Component>(processMe);
+	}
+
+}
