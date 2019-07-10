11bc678743d7a7e7c335c2ffbe9f05a82b898956
Peter Groves Williams <petergwilliams@hotmail.com>
2019-07-10 17:04:58 +0100

    INFRA-3583 Specify shell to use in server initialisation scripts

--- reporting-engine-worker/start_reporting_worker_dev.sh
+++ reporting-engine-worker/start_reporting_worker_dev.sh
@@ -1,3 +1,4 @@
-export RPW_Environment=uat-int
+#!/bin/bash
+export RPW_Environment=dev-int
 export RPW_Version=latest
 nohup ./reporting_worker.sh 2>&1 | tee /var/log/supervisor/reporting_worker_startup.log
