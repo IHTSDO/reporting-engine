295f6dbc26f844db9c69c411390f6837448c63d1
Peter Groves Williams <petergwilliams@hotmail.com>
2019-09-11 11:21:03 +0100

    DRUGS-784 Add a couple of extra MPF acceptable dose forms

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/drugs/ValidateDrugModeling.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/drugs/ValidateDrugModeling.java
@@ -199,6 +199,8 @@ public class ValidateDrugModeling extends TermServerReport implements ReportClas
 		Map<Concept, Boolean> acceptableDoseForms;
 		if (isMPF(c)) {
 			acceptableDoseForms = acceptableMpfDoseForms;
+			acceptableDoseForms.put(gl.getConcept("785898006 |Conventional release solution for irrigation (dose form)|"), Boolean.TRUE);
+			acceptableDoseForms.put(gl.getConcept("785910004 |Prolonged-release intralesional implant (dose form)|"), Boolean.TRUE);
 		} else {
 			acceptableDoseForms = acceptableCdDoseForms;
 		}
