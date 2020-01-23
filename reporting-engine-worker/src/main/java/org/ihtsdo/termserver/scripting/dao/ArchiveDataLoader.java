06bdb2faa455800fd469f45f5f9ec5eb8bbcc826
Peter Groves Williams <petergwilliams@hotmail.com>
2020-01-23 15:10:31 +0000

    RP-303 Do not attempt to connect to S3 when useCloud=false

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/dao/ArchiveDataLoader.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/dao/ArchiveDataLoader.java
@@ -6,12 +6,15 @@ import org.apache.commons.io.IOUtils;
 import org.ihtsdo.otf.resourcemanager.ResourceManager;
 import org.ihtsdo.otf.exception.TermServerScriptException;
 import org.ihtsdo.termserver.scripting.TermServerScript;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.aws.core.io.s3.SimpleStorageResourceLoader;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.amazonaws.auth.*;
 import com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper;
@@ -20,6 +23,8 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 @Service
 public class ArchiveDataLoader {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ResourceDataLoader.class);
 	
 	@Autowired
 	private ArchiveLoaderConfig archiveLoaderConfig;
@@ -32,6 +37,9 @@ public class ArchiveDataLoader {
 	
 	@Value("${aws.secretKey}")
 	private String awsSecretKey;
+	
+	@Value("${archives.useCloud}")
+	private String useCloudStr;
 	
 	@EventListener(ApplicationReadyEvent.class)
 	public void init() {
@@ -46,36 +54,47 @@ public class ArchiveDataLoader {
 	}
 	
 	public void download (File archive) throws TermServerScriptException {
-		try {
-			if (archiveLoaderConfig == null) {
-				archiveLoaderConfig = new ArchiveLoaderConfig();
-				archiveLoaderConfig.init("archives");
+		boolean useCloud = false;
+		if (!StringUtils.isEmpty(useCloudStr)) {
+			useCloud = useCloudStr.toLowerCase().equals("true");
+		} else {
+			throw new TermServerScriptException("Check application.properties - archives.useCloud is not specified");
+		}
+		
+		if (useCloud) {
+			try {
+				if (archiveLoaderConfig == null) {
+					archiveLoaderConfig = new ArchiveLoaderConfig();
+					archiveLoaderConfig.init("archives");
+				}
+				AWSCredentialsProvider awsCredProv;
+				if (awsKey == null || awsKey.isEmpty()) {
+					awsCredProv = new EC2ContainerCredentialsProviderWrapper();
+					TermServerScript.info("Connecting to S3 with EC2 environment configured credentials");
+				} else {
+					AWSCredentials awsCreds = new BasicAWSCredentials(awsKey, awsSecretKey);
+					awsCredProv = new AWSStaticCredentialsProvider(awsCreds);
+					TermServerScript.info("Connecting to S3 with locally specified account: " + awsKey);
+				}
+	
+				AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+										.withCredentials(awsCredProv)
+										.withRegion(region)
+										.build();
+				ResourceManager resourceManager = new ResourceManager(archiveLoaderConfig, new SimpleStorageResourceLoader(s3Client));
+				try (InputStream input = resourceManager.readResourceStream(archive.getName());
+					OutputStream out = new FileOutputStream(archive);) {
+					TermServerScript.info("Downloading " + archive.getName() + " from S3");
+					IOUtils.copy(input, out);
+					TermServerScript.info("Download complete");
+				}
+			} catch (Throwable  t) {
+				final String msg = "Error when trying to download " + archive.getName() + " from S3 via :" +  archiveLoaderConfig;
+				throw new TermServerScriptException(msg, t);
 			}
-			AWSCredentialsProvider awsCredProv;
-			if (awsKey == null || awsKey.isEmpty()) {
-				awsCredProv = new EC2ContainerCredentialsProviderWrapper();
-				TermServerScript.info("Connecting to S3 with EC2 environment configured credentials");
-			} else {
-				AWSCredentials awsCreds = new BasicAWSCredentials(awsKey, awsSecretKey);
-				awsCredProv = new AWSStaticCredentialsProvider(awsCreds);
-				TermServerScript.info("Connecting to S3 with locally specified account: " + awsKey);
-			}
-
-			AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-									.withCredentials(awsCredProv)
-									.withRegion(region)
-									.build();
-			ResourceManager resourceManager = new ResourceManager(archiveLoaderConfig, new SimpleStorageResourceLoader(s3Client));
-			try (InputStream input = resourceManager.readResourceStream(archive.getName());
-				OutputStream out = new FileOutputStream(archive);) {
-				TermServerScript.info("Downloading " + archive.getName() + " from S3");
-				IOUtils.copy(input, out);
-				TermServerScript.info("Download complete");
-			}
-		} catch (Throwable  t) {
-			final String msg = "Error when trying to download " + archive.getName() + " from S3 via :" +  archiveLoaderConfig;
-			throw new TermServerScriptException(msg, t);
-		} 
+		} else {
+			LOGGER.info("ArchiveDataLoader set to local source. Will expect " + archive + " to be available.");
+		}
 	}
 
 	public static ArchiveDataLoader create() throws TermServerScriptException {
@@ -84,6 +103,7 @@ public class ArchiveDataLoader {
 		loader.region = properties.getProperty("cloud.aws.region.static");
 		loader.awsKey = properties.getProperty("aws.key");
 		loader.awsSecretKey = properties.getProperty("aws.secretKey");
+		loader.useCloudStr = properties.getProperty("archives.useCloud");
 		return loader;
 	}
 	
--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/dao/ResourceDataLoader.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/dao/ResourceDataLoader.java
@@ -15,11 +15,11 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.aws.core.io.s3.SimpleStorageResourceLoader;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.AnonymousAWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper;
 import com.amazonaws.services.s3.AmazonS3;
@@ -43,43 +43,56 @@ public class ResourceDataLoader {
 	@Value("${aws.secretKey}")
 	private String awsSecretKey;
 	
+	@Value("${resources.useCloud}")
+	private String useCloudStr;
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(ResourceDataLoader.class);
 
 	@EventListener(ApplicationReadyEvent.class)
-	private void init() throws BusinessServiceException {
-		try {
-			
-			AWSCredentialsProvider awsCredProv;
-			if (awsKey == null || awsKey.isEmpty()) {
-				awsCredProv = new EC2ContainerCredentialsProviderWrapper();
-				TermServerScript.info("Connecting to S3 with EC2 environment configured credentials");
-			} else {
-				AWSCredentials awsCreds = new BasicAWSCredentials(awsKey, awsSecretKey);
-				awsCredProv = new AWSStaticCredentialsProvider(awsCreds);
-				TermServerScript.info("Connecting to S3 with locally specified account: " + awsKey);
-			}
-
-			AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-									.withCredentials(awsCredProv)
-									.withRegion(region)
-									.build();
-			ResourceManager resourceManager = new ResourceManager(resourceConfig, new SimpleStorageResourceLoader(s3Client));
-			for (String fileName : fileNames) {
-				File localFile = new File ("resources/" + fileName);
-				TermServerScript.info ("Downloading " + fileName + " from S3");
-				try (InputStream input = resourceManager.readResourceStreamOrNullIfNotExists(fileName);
-						OutputStream out = new FileOutputStream(localFile);) {
-					if (input != null) {
-						IOUtils.copy(input, out);
-					}
-				} catch (Exception e) {
-					throw new TermServerScriptException ("Unable to load " + fileName + " from S3", e);
+	private void init() throws TermServerScriptException {
+		boolean useCloud = false;
+		if (!StringUtils.isEmpty(useCloudStr)) {
+			useCloud = useCloudStr.toLowerCase().equals("true");
+		} else {
+			throw new TermServerScriptException("Check application.properties - resources.useCloud is not specified");
+		}
+		
+		if (useCloud) {
+			try {
+				AWSCredentialsProvider awsCredProv;
+				if (awsKey == null || awsKey.isEmpty()) {
+					awsCredProv = new EC2ContainerCredentialsProviderWrapper();
+					TermServerScript.info("Connecting to S3 with EC2 environment configured credentials");
+				} else {
+					AWSCredentials awsCreds = new BasicAWSCredentials(awsKey, awsSecretKey);
+					awsCredProv = new AWSStaticCredentialsProvider(awsCreds);
+					TermServerScript.info("Connecting to S3 with locally specified account: " + awsKey);
 				}
+	
+				AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+										.withCredentials(awsCredProv)
+										.withRegion(region)
+										.build();
+				ResourceManager resourceManager = new ResourceManager(resourceConfig, new SimpleStorageResourceLoader(s3Client));
+				for (String fileName : fileNames) {
+					File localFile = new File ("resources/" + fileName);
+					TermServerScript.info ("Downloading " + fileName + " from S3");
+					try (InputStream input = resourceManager.readResourceStreamOrNullIfNotExists(fileName);
+							OutputStream out = new FileOutputStream(localFile);) {
+						if (input != null) {
+							IOUtils.copy(input, out);
+						}
+					} catch (Exception e) {
+						throw new TermServerScriptException ("Unable to load " + fileName + " from S3", e);
+					}
+				}
+			} catch (Throwable  t) {
+				final String errorMsg = "Error when trying to download the us-to-gb-terms-map.txt file from S3 via :" +  resourceConfig;
+				LOGGER.error(errorMsg, t);
 			}
-		} catch (Throwable  t) {
-			final String errorMsg = "Error when trying to download the us-to-gb-terms-map.txt file from S3 via :" +  resourceConfig;
-			LOGGER.error(errorMsg, t);
-		} 
+		} else {
+			LOGGER.info("AWS S3 marked as local due to 'resources.useCloud=false' setting.");
+		}
 	}
 	
 	public static ResourceDataLoader create() throws TermServerScriptException {
@@ -88,6 +101,7 @@ public class ResourceDataLoader {
 		loader.region = properties.getProperty("cloud.aws.region.static");
 		loader.awsKey = properties.getProperty("aws.key");
 		loader.awsSecretKey = properties.getProperty("aws.secretKey");
+		loader.useCloudStr = properties.getProperty("resources.useCloud");
 		return loader;
 	}
 }
