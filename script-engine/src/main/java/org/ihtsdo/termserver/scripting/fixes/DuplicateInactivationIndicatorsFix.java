8b5a91ec979813ba6139f5f108655a1c27b00233
pgwilliams <petergwilliams@hotmail.com>
2020-04-29 08:38:40 +0100

    Auto-refactor for final and bracket spacing

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/DuplicateInactivationIndicatorsFix.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/DuplicateInactivationIndicatorsFix.java
@@ -16,19 +16,20 @@ import org.ihtsdo.termserver.scripting.fixes.BatchFix;
  * INFRA-2480 Finding concept and description inactivation indicators that are duplicated
  * and remove the unpublished version
 */
-public class DuplicateInactivationIndicatorsFix extends BatchFix implements RF2Constants{
+public class DuplicateInactivationIndicatorsFix extends BatchFix {
 	
-	protected DuplicateInactivationIndicatorsFix(BatchFix clone) {
+	protected DuplicateInactivationIndicatorsFix(final BatchFix clone) {
 		super(clone);
 	}
 
-	public static void main(String[] args) throws TermServerScriptException, IOException, InterruptedException {
-		DuplicateInactivationIndicatorsFix fix = new DuplicateInactivationIndicatorsFix(null);
+	public static void main(final String[] args) throws TermServerScriptException, IOException, InterruptedException {
+		final DuplicateInactivationIndicatorsFix fix = new DuplicateInactivationIndicatorsFix(null);
 		try {
 			fix.selfDetermining = true;
 			fix.init(args);
-			//Recover the current project state from TS (or local cached archive) to allow quick searching of all concepts
-			fix.loadProjectSnapshot(false); //Load all descriptions
+			// Recover the current project state from TS (or local cached archive) to allow
+			// quick searching of all concepts
+			fix.loadProjectSnapshot(false); // Load all descriptions
 			fix.postInit();
 			fix.processFile();
 		} finally {
@@ -37,53 +38,61 @@ public class DuplicateInactivationIndicatorsFix extends BatchFix implements RF2C
 	}
 
 	@Override
-	public int doFix(Task task, Concept concept, String info) throws TermServerScriptException, ValidationFailure {
-		//We will not load the concept because the Browser endpoint does not populate the full array of inactivation indicators
+	public int doFix(final Task task, final Concept concept, final String info)
+			throws TermServerScriptException, ValidationFailure {
+		// We will not load the concept because the Browser endpoint does not populate
+		// the full array of inactivation indicators
 		int changesMade = 0;
 		try {
 			changesMade = fixDuplicateInactivationIndicators(task, concept, false);
-		} catch (TermServerScriptException e) {
-			throw new TermServerScriptException ("Failed to remove duplicate inactivation indicator on " + concept, e);
+		} catch (final TermServerScriptException e) {
+			throw new TermServerScriptException("Failed to remove duplicate inactivation indicator on " + concept, e);
 		}
 		return changesMade;
 	}
 
-	private int fixDuplicateInactivationIndicators(Task task, Concept c, boolean trialRun) throws TermServerScriptException {
+	private int fixDuplicateInactivationIndicators(final Task task, final Concept c, final boolean trialRun)
+			throws TermServerScriptException {
 		int changesMade = 0;
-			InactivationIndicatorEntry[] ciis = getDuplicateInactivationIndicators(c, c.getInactivationIndicatorEntries());
-			for (InactivationIndicatorEntry cii : ciis) {
-				debug ("Removing duplicate: " + cii);
-				report (task, c, Severity.LOW, ReportActionType.REFSET_MEMBER_REMOVED, cii);
-				tsClient.deleteRefsetMember(cii.getId(), task.getBranchPath(), false);
+		final InactivationIndicatorEntry[] ciis = getDuplicateInactivationIndicators(c,
+				c.getInactivationIndicatorEntries());
+		for (final InactivationIndicatorEntry cii : ciis) {
+			debug("Removing duplicate: " + cii);
+			report(task, c, Severity.LOW, ReportActionType.REFSET_MEMBER_REMOVED, cii);
+			tsClient.deleteRefsetMember(cii.getId(), task.getBranchPath(), false);
+			changesMade++;
+		}
+		for (final Description d : c.getDescriptions()) {
+			final InactivationIndicatorEntry[] diis = getDuplicateInactivationIndicators(d,
+					d.getInactivationIndicatorEntries());
+			for (final InactivationIndicatorEntry dii : diis) {
+				debug("Removing duplicate: " + dii);
+				report(task, c, Severity.LOW, ReportActionType.REFSET_MEMBER_REMOVED, dii);
+				tsClient.deleteRefsetMember(dii.getId(), task.getBranchPath(), false);
 				changesMade++;
 			}
-			for (Description d : c.getDescriptions()) {
-				InactivationIndicatorEntry[] diis = getDuplicateInactivationIndicators(d, d.getInactivationIndicatorEntries());
-				for (InactivationIndicatorEntry dii : diis) {
-					debug ("Removing duplicate: " + dii);
-					report (task, c, Severity.LOW, ReportActionType.REFSET_MEMBER_REMOVED, dii);
-					tsClient.deleteRefsetMember(dii.getId(), task.getBranchPath(), false);
-					changesMade ++;
-				}
-			}
+		}
 		return changesMade;
 	}
 
 	@Override
 	protected List<Component> identifyComponentsToProcess() {
-		//Work through all inactive concepts and check the inactivation indicator on all
-		//active descriptions
-		info ("Identifying concepts to process");
-		List<Component> processMe = new ArrayList<Component>();
-		for (Concept c : gl.getAllConcepts()) {
-			/*if (c.getConceptId().equals("28979006")) {
-				debug ("Check me");
-			}*/
+		// Work through all inactive concepts and check the inactivation indicator on
+		// all
+		// active descriptions
+		info("Identifying concepts to process");
+		final List<Component> processMe = new ArrayList<Component>();
+		for (final Concept c : gl.getAllConcepts()) {
+			/*
+			 * if (c.getConceptId().equals("28979006")) { debug ("Check me"); }
+			 */
 			if (!c.isActive()) {
-				InactivationIndicatorEntry[] ciis = getDuplicateInactivationIndicators(c, c.getInactivationIndicatorEntries());
+				final InactivationIndicatorEntry[] ciis = getDuplicateInactivationIndicators(c,
+						c.getInactivationIndicatorEntries());
 				if (ciis.length == 0) {
-					for (Description d : c.getDescriptions()) {
-						InactivationIndicatorEntry[] diis = getDuplicateInactivationIndicators(d, d.getInactivationIndicatorEntries());
+					for (final Description d : c.getDescriptions()) {
+						final InactivationIndicatorEntry[] diis = getDuplicateInactivationIndicators(d,
+								d.getInactivationIndicatorEntries());
 						if (diis.length > 0) {
 							processMe.add(c);
 							break;
@@ -94,28 +103,28 @@ public class DuplicateInactivationIndicatorsFix extends BatchFix implements RF2C
 				}
 			}
 		}
-		info ("Identified " + processMe.size() + " concepts to process");
+		info("Identified " + processMe.size() + " concepts to process");
 		return processMe;
 	}
 
-	private InactivationIndicatorEntry[] getDuplicateInactivationIndicators(Component c,
-			List<InactivationIndicatorEntry> inactivationIndicatorEntries) {
-		
-		List<InactivationIndicatorEntry> duplicates = new ArrayList<>();
-		List<InactivationIndicatorEntry> keepers = new ArrayList<>();
-		
-		for (InactivationIndicatorEntry thisEntry : inactivationIndicatorEntries) {
-			//Check against every other entry
-			for (InactivationIndicatorEntry thatEntry : inactivationIndicatorEntries) {
-				//If we've already decided we're keeping this entry, skip
+	private InactivationIndicatorEntry[] getDuplicateInactivationIndicators(final Component c,
+			final List<InactivationIndicatorEntry> inactivationIndicatorEntries) {
+
+		final List<InactivationIndicatorEntry> duplicates = new ArrayList<>();
+		final List<InactivationIndicatorEntry> keepers = new ArrayList<>();
+
+		for (final InactivationIndicatorEntry thisEntry : inactivationIndicatorEntries) {
+			// Check against every other entry
+			for (final InactivationIndicatorEntry thatEntry : inactivationIndicatorEntries) {
+				// If we've already decided we're keeping this entry, skip
 				if (thisEntry.getId().equals(thatEntry.getId()) || keepers.contains(thisEntry)) {
 					continue;
 				}
-				if (thisEntry.getRefsetId().equals(thatEntry.getRefsetId()) &&
-						thisEntry.getReferencedComponentId().equals(thatEntry.getReferencedComponentId()) &&
-						thisEntry.getInactivationReasonId().equals(thatEntry.getInactivationReasonId())) {
-					debug ("Found duplicates for " + c + ": " + thisEntry + " + " + thatEntry);
-					//Delete the unpublished one
+				if (thisEntry.getRefsetId().equals(thatEntry.getRefsetId())
+						&& thisEntry.getReferencedComponentId().equals(thatEntry.getReferencedComponentId())
+						&& thisEntry.getInactivationReasonId().equals(thatEntry.getInactivationReasonId())) {
+					debug("Found duplicates for " + c + ": " + thisEntry + " + " + thatEntry);
+					// Delete the unpublished one
 					if (thisEntry.getEffectiveTime() == null || thisEntry.getEffectiveTime().isEmpty()) {
 						duplicates.add(thisEntry);
 						keepers.add(thatEntry);
@@ -123,9 +132,9 @@ public class DuplicateInactivationIndicatorsFix extends BatchFix implements RF2C
 						duplicates.add(thatEntry);
 						keepers.add(thisEntry);
 					} else {
-						//Only a problem historically if they're both active
+						// Only a problem historically if they're both active
 						if (thisEntry.isActive() && thatEntry.isActive()) {
-							warn ("Both entries look published! " + thisEntry.getEffectiveTime());
+							warn("Both entries look published! " + thisEntry.getEffectiveTime());
 						}
 					}
 				}
@@ -135,7 +144,7 @@ public class DuplicateInactivationIndicatorsFix extends BatchFix implements RF2C
 	}
 
 	@Override
-	protected List<Component> loadLine(String[] lineItems)
+	protected List<Component> loadLine(final String[] lineItems)
 			throws TermServerScriptException {
 		throw new NotImplementedException("This class self determines concepts to process");
 	}
