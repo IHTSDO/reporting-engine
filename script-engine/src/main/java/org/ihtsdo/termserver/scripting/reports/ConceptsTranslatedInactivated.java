8787dc6fa7f38e54c8fbd8a960965dbaeb7673df
pgwilliams <petergwilliams@hotmail.com>
2020-05-26 16:01:46 +0100

    Inactivated translated concepts report for MS users

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/reports/ConceptsTranslatedInactivated.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/reports/ConceptsTranslatedInactivated.java
@@ -11,30 +11,35 @@ import org.ihtsdo.termserver.scripting.domain.*;
 import org.ihtsdo.termserver.scripting.util.SnomedUtils;
 import org.snomed.otf.scheduler.domain.*;
 import org.snomed.otf.scheduler.domain.Job.ProductionStatus;
-import org.snomed.otf.scheduler.domain.JobParameter.Type;
 import org.springframework.util.StringUtils;
 
-public class ConceptsInactivated extends TermServerReport implements ReportClass {
+public class ConceptsTranslatedInactivated extends TermServerReport implements ReportClass {
 	
-	static final String RELEASE = "Release Package";
-	private String prevRelease;
-	private String thisEffectiveTime;
+	private String intEffectiveTime;
 	
 	public static void main(String[] args) throws TermServerScriptException, IOException {
-		Map<String, String> params = new HashMap<>();
-		params.put(RELEASE, "dev_xSnomedCT_InternationalRF2_PREALPHA_20200731T120000Z.zip");
-		TermServerReport.run(ConceptsInactivated.class, args, params);
+		TermServerReport.run(ConceptsTranslatedInactivated.class, args, new HashMap<>());
 	}
 	
 	public void init (JobRun run) throws TermServerScriptException {
-		ReportSheetManager.targetFolderId = "1PWtDYFfqLoUwk17HlFgNK648Mmra-1GA"; //General QA Reports
+		ReportSheetManager.targetFolderId = "15WXT1kov-SLVi4cvm2TbYJp_vBMr4HZJ"; //Release QA Reports
 		subHierarchyECL = run.getParamValue(ECL);
 		super.init(run);
+		if (project.getKey().equals("MAIN")) {
+			throw new TermServerScriptException("Inactivated Translated Concepts report cannot be run against MAIN");
+		}
 	}
 	
 	public void postInit() throws TermServerScriptException {
+		
+		if (project.getMetadata() != null && project.getMetadata().getDependencyRelease() != null) {
+			intEffectiveTime = project.getMetadata().getDependencyRelease();
+		} else {
+			throw new TermServerScriptException ("MS Project expected. " + project.getKey() + " is not configured with a dependency release effectiveTime");
+		}
+		
 		String[] columnHeadings = new String[] {
-				"Id, FSN, SemTag, Reason, Assoc Type, Assoc Value"};
+				"Id, FSN, SemTag, Translation(s), Reason, Assoc Type, Assoc Value, Translation(s)"};
 		String[] tabNames = new String[] {	
 				"Concepts Inactivated"};
 		super.postInit(tabNames, columnHeadings, false);
@@ -42,17 +47,13 @@ public class ConceptsInactivated extends TermServerReport implements ReportClass
 	
 	@Override
 	public Job getJob() {
-		JobParameters params = new JobParameters()
-				.add(ECL).withType(JobParameter.Type.ECL).withDefaultValue("<< " + ROOT_CONCEPT)
-				.add(RELEASE).withType(Type.STRING)
-				.build();
 		return new Job()
-				.withCategory(new JobCategory(JobType.REPORT, JobCategory.GENERAL_QA))
-				.withName("Inactivated Concepts")
-				.withDescription("This report lists all concepts inactivated in the current release cycle along with the reason and historical association, optionally restricted to a subset defined by an ECL expression.  The issue count here is the total number of concepts inactivated.")
+				.withCategory(new JobCategory(JobType.REPORT, JobCategory.RELEASE_VALIDATION))
+				.withName("Inactivated Translated Concepts")
+				.withDescription("This report lists translated International concepts which have been inactivated in the latest release along with historically associated replacements which may or may not hold translations.  The issue count here is the total number of concepts inactivated where the replacements require a translation.")
 				.withProductionStatus(ProductionStatus.PROD_READY)
-				.withParameters(params)
-				.withTag(INT)
+				.withParameters(new JobParameters())
+				.withTag(MS)
 				.build();
 	}
 	
@@ -67,39 +68,46 @@ public class ConceptsInactivated extends TermServerReport implements ReportClass
 		for (Concept c : scopeAndSort(conceptsOfInterest)) {
 			boolean reported = false;
 			InactivationIndicator i = c.getInactivationIndicator();
+			String translations = getTranslations(c);
 			for (AssociationEntry a : c.getAssociations(ActiveState.ACTIVE, true)) {
 				String assocType = SnomedUtils.getAssociationType(a);
 				Concept assocValue = gl.getConcept(a.getTargetComponentId());
-				report (c, i, assocType, assocValue);
+				String assocTranslations = getTranslations(assocValue);
+				if (StringUtils.isEmpty(assocTranslations)) {
+					countIssue(c);
+				}
+				report (c, translations, i, assocType, assocValue, assocTranslations);
 				reported = true;
+				
 			}
 			if (!reported) {
 				report (c, i , "N/A");
 			}
-			countIssue(c);
+			
 		}
 	}
 	
 	private boolean inScope(Concept c) {
-		//We want inactive concepts modified either in the current release cycle or in the 
-		//latest release if we're looking at a particular release package
-		return !c.isActive() && ( (thisEffectiveTime == null && StringUtils.isEmpty(c.getEffectiveTime()) ||
-				(thisEffectiveTime != null && thisEffectiveTime.equals(c.getEffectiveTime()))));
+		//For this report we're interested in International Concepts inactivated
+		//in the last (dependency) release which have translations in the target module
+		if (!c.isActive() && c.getEffectiveTime().equals(intEffectiveTime) 
+			&& hasTranslation(c)) {
+			return true;
+		}
+		return false;
 	}
 	
-	@Override
-	protected void loadProjectSnapshot(boolean fsnOnly) throws TermServerScriptException, InterruptedException, IOException {
-		prevRelease = getJobRun().getParamValue(RELEASE);
-		if (prevRelease != null) {
-			getProject().setKey(prevRelease);
-			super.loadProjectSnapshot(fsnOnly);
-			thisEffectiveTime = gl.getCurrentEffectiveTime();
-			info ("Detected this effective time as " + thisEffectiveTime);
-		} else {
-			super.loadProjectSnapshot(fsnOnly);
-		}
-	};
-	
+	private boolean hasTranslation(Concept c) {
+		return !StringUtils.isEmpty(getTranslations(c));
+	}
+
+	private String getTranslations(Concept c) {
+		return c.getDescriptions(ActiveState.ACTIVE).stream()
+			.filter(d -> inScope(d))
+			.map(d -> d.getTerm())
+			.collect(Collectors.joining(", \n"));
+	}
+
 	private List<Concept> scopeAndSort(Collection<Concept> superSet) {
 		//We're going to sort on top level hierarchy, then alphabetically
 		//filter for appropriate scope at the same time - avoids problems with FSNs without semtags
