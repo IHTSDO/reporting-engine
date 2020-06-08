ba96a52d345b4d6e28e5d134f9f2dff43663e63f
pgwilliams <petergwilliams@hotmail.com>
2020-06-08 11:15:27 +0100

    QI-300 Add report to list (filtered) active concepts which have incoming historical associations

---
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/DuplicateInactAssocFix.java
@@ -0,0 +1,239 @@
+package org.ihtsdo.termserver.scripting.fixes;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.commons.lang.NotImplementedException;
+import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
+import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Task;
+import org.ihtsdo.otf.exception.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.ValidationFailure;
+
+import org.ihtsdo.termserver.scripting.domain.*;
+import org.ihtsdo.termserver.scripting.fixes.BatchFix;
+import org.springframework.util.StringUtils;
+
+/*
+ * INFRA-2480 Finding concept and description inactivation indicators that are duplicated
+ * and remove the unpublished version
+ * INFRA-5156 Add ability to delete duplicated historic associations at the same time
+*/
+public class DuplicateInactAssocFix extends BatchFix {
+	
+	protected DuplicateInactAssocFix(final BatchFix clone) {
+		super(clone);
+	}
+
+	public static void main(final String[] args) throws TermServerScriptException, IOException, InterruptedException {
+		final DuplicateInactAssocFix fix = new DuplicateInactAssocFix(null);
+		try {
+			fix.selfDetermining = true;
+			fix.init(args);
+			// Recover the current project state from TS (or local cached archive) to allow
+			// quick searching of all concepts
+			fix.loadProjectSnapshot(false); // Load all descriptions
+			fix.postInit();
+			fix.processFile();
+		} finally {
+			fix.finish();
+		}
+	}
+
+	@Override
+	public int doFix(final Task task, final Concept concept, final String info)
+			throws TermServerScriptException, ValidationFailure {
+		// We will not load the concept because the Browser endpoint does not populate
+		// the full array of inactivation indicators
+		int changesMade = 0;
+		try {
+			changesMade = fixDuplicateInactivationIndicators(task, concept, false);
+		} catch (final TermServerScriptException e) {
+			throw new TermServerScriptException("Failed to remove duplicate inactivation indicator on " + concept, e);
+		}
+		return changesMade;
+	}
+
+	private int fixDuplicateInactivationIndicators(final Task t, final Concept c, final boolean trialRun)
+			throws TermServerScriptException {
+		int changesMade = 0;
+		
+		final InactivationIndicatorEntry[] ciis = getDuplicateInactivationIndicators(c, c.getInactivationIndicatorEntries(), t);
+		for (final InactivationIndicatorEntry cii : ciis) {
+			debug((dryRun?"Dry Run, not ":"") + "Removing duplicate: " + cii);
+			report(t, c, Severity.LOW, ReportActionType.REFSET_MEMBER_REMOVED, cii);
+			if (!dryRun) {
+				tsClient.deleteRefsetMember(cii.getId(), t.getBranchPath(), false);
+			}
+			changesMade++;
+		}
+		
+		final AssociationEntry[] as = getDuplicateAssociations(c, c.getAssociations(), t);
+		for (final AssociationEntry a : as) {
+			debug((dryRun?"Dry Run, not ":"") + "Removing duplicate: " + a);
+			report(t, c, Severity.LOW, ReportActionType.REFSET_MEMBER_REMOVED, a);
+			if (!dryRun) {
+				tsClient.deleteRefsetMember(a.getId(), t.getBranchPath(), false);
+			}
+			changesMade++;
+		}
+		
+		for (final Description d : c.getDescriptions()) {
+			final InactivationIndicatorEntry[] diis = getDuplicateInactivationIndicators(d, d.getInactivationIndicatorEntries(), t);
+			for (final InactivationIndicatorEntry dii : diis) {
+				debug((dryRun?"Dry Run, not ":"") + "Removing duplicate: " + dii);
+				report(t, c, Severity.LOW, ReportActionType.REFSET_MEMBER_REMOVED, dii);
+				if (!dryRun) {
+					tsClient.deleteRefsetMember(dii.getId(), t.getBranchPath(), false);
+				}
+				changesMade++;
+			}
+		}
+		return changesMade;
+	}
+
+	@Override
+	protected List<Component> identifyComponentsToProcess() {
+		// Work through all inactive concepts and check the inactivation indicators on
+		// active descriptions
+		info("Identifying concepts to process");
+		final List<Component> processMe = new ArrayList<Component>();
+		for (final Concept c : gl.getAllConcepts()) {
+			/*if (c.getConceptId().equals("519811015")) 
+			{ 
+				debug ("Check me"); 
+			}*/
+			 
+			if (!c.isActive()) {
+				final AssociationEntry[] as = getDuplicateAssociations(c, c.getAssociations(), null);
+				if (as.length > 0) {
+					processMe.add(c);
+				} else {
+					final InactivationIndicatorEntry[] ciis = getDuplicateInactivationIndicators(c, c.getInactivationIndicatorEntries(), null);
+					if (ciis.length == 0) {
+						for (final Description d : c.getDescriptions()) {
+							final InactivationIndicatorEntry[] diis = getDuplicateInactivationIndicators(d, d.getInactivationIndicatorEntries(), null);
+							if (diis.length > 0) {
+								processMe.add(c);
+								break;
+							}
+						}
+					} else {
+						processMe.add(c);
+					}
+				}
+			}
+		}
+		info("Identified " + processMe.size() + " concepts to process");
+		return processMe;
+	}
+
+	private InactivationIndicatorEntry[] getDuplicateInactivationIndicators(final Component c,
+			final List<InactivationIndicatorEntry> inactivationIndicatorEntries, Task t) {
+
+		final List<InactivationIndicatorEntry> duplicates = new ArrayList<>();
+		final List<InactivationIndicatorEntry> keepers = new ArrayList<>();
+
+		for (final InactivationIndicatorEntry thisEntry : inactivationIndicatorEntries) {
+			// Check against every other entry
+			for (final InactivationIndicatorEntry thatEntry : inactivationIndicatorEntries) {
+				// If we've already decided we're keeping this entry, skip
+				if (thisEntry.getId().equals(thatEntry.getId()) || keepers.contains(thisEntry) || duplicates.contains(thisEntry)) {
+					continue;
+				}
+				if (thisEntry.getRefsetId().equals(thatEntry.getRefsetId())
+						&& thisEntry.getReferencedComponentId().equals(thatEntry.getReferencedComponentId())
+						&& thisEntry.getInactivationReasonId().equals(thatEntry.getInactivationReasonId())) {
+					debug("Found duplicates for " + c + ": " + thisEntry + " + " + thatEntry);
+					// Delete the unpublished one.   If they're both unpublished, get that refset entry 
+					// directly to check the published flag.   Don't worry if we're just finding the concept though - t == null
+					if (t != null && StringUtils.isEmpty(thisEntry.getEffectiveTime()) && StringUtils.isEmpty(thatEntry.getEffectiveTime())) {
+						warn("Both entries look modified, checking TS for published status of " + thisEntry);
+						RefsetMember r = tsClient.getRefsetMember(thisEntry.getId(), getBranchPath(t));
+						warn("Result: Is " + (r.getReleased()?"":"not ") + "published.");
+						if (r.getReleased()) {
+							duplicates.add(thatEntry);
+							keepers.add(thisEntry);
+						} else {
+							duplicates.add(thisEntry);
+							keepers.add(thatEntry);
+						}
+					} else if (StringUtils.isEmpty(thisEntry.getEffectiveTime())) {
+						duplicates.add(thisEntry);
+						keepers.add(thatEntry);
+					} else if (StringUtils.isEmpty(thatEntry.getEffectiveTime())) {
+						duplicates.add(thatEntry);
+						keepers.add(thisEntry);
+					} else {
+						// Only a problem historically if they're both active
+						if (thisEntry.isActive() && thatEntry.isActive()) {
+							warn("Both entries look published! " + thisEntry.getEffectiveTime());
+						}
+					}
+				}
+			}
+		}
+		return duplicates.toArray(new InactivationIndicatorEntry[] {});
+	}
+	
+	private String getBranchPath(Task t) {
+		//If we're in dry run, we need to use the project path rather than the task
+		if (dryRun) {
+			return t.getBranchPath().substring(0, t.getBranchPath().lastIndexOf('/'));
+		}
+		return t.getBranchPath();
+	}
+
+	//TODO Create a common interface for reference set entries and remove this method.
+	private AssociationEntry[] getDuplicateAssociations(final Component c, final List<AssociationEntry> AssociationEntries, Task t) {
+		final List<AssociationEntry> duplicates = new ArrayList<>();
+		final List<AssociationEntry> keepers = new ArrayList<>();
+
+		for (final AssociationEntry thisEntry : AssociationEntries) {
+			// Check against every other entry
+			for (final AssociationEntry thatEntry : AssociationEntries) {
+				// If we've already decided we're keeping this entry, skip
+				if (thisEntry.getId().equals(thatEntry.getId()) || keepers.contains(thisEntry) || duplicates.contains(thisEntry)) {
+					continue;
+				}
+				if (thisEntry.getRefsetId().equals(thatEntry.getRefsetId())
+						&& thisEntry.getRefsetId().equals(thatEntry.getRefsetId())
+						&& thisEntry.getReferencedComponentId().equals(thatEntry.getReferencedComponentId())
+						&& thisEntry.getTargetComponentId().equals(thatEntry.getTargetComponentId())) {
+					debug("Found duplicates for " + c + ": " + thisEntry + " + " + thatEntry);
+					// Delete the unpublished one
+					if (t != null && StringUtils.isEmpty(thisEntry.getEffectiveTime()) && StringUtils.isEmpty(thatEntry.getEffectiveTime())) {
+						warn("Both entries look modified, checking TS for published status of " + thisEntry);
+						RefsetMember r = tsClient.getRefsetMember(thisEntry.getId(), getBranchPath(t));
+						warn("Result: Is " + (r.getReleased()?"":"not ") + "published.");
+						if (r.getReleased()) {
+							duplicates.add(thatEntry);
+							keepers.add(thisEntry);
+						} else {
+							duplicates.add(thisEntry);
+							keepers.add(thatEntry);
+						}
+					} else if (thisEntry.getEffectiveTime() == null || thisEntry.getEffectiveTime().isEmpty()) {
+						duplicates.add(thisEntry);
+						keepers.add(thatEntry);
+					} else if (thatEntry.getEffectiveTime() == null || thatEntry.getEffectiveTime().isEmpty()) {
+						duplicates.add(thatEntry);
+						keepers.add(thisEntry);
+					} else {
+						// Only a problem historically if they're both active
+						if (thisEntry.isActive() && thatEntry.isActive()) {
+							warn("Both entries look published! " + thisEntry.getEffectiveTime());
+						}
+					}
+				}
+			}
+		}
+		return duplicates.toArray(new AssociationEntry[] {});
+	}
+
+	@Override
+	protected List<Component> loadLine(final String[] lineItems)
+			throws TermServerScriptException {
+		throw new NotImplementedException("This class self determines concepts to process");
+	}
+
+}
