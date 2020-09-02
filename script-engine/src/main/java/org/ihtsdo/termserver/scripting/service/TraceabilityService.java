7576ccbc04414e811c392bf0285281e861b8b7c3
pgwilliams <petergwilliams@hotmail.com>
2020-09-02 11:36:38 +0100

    RP-325 Fix for report rows going missing on successive updates

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/service/TraceabilityService.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/service/TraceabilityService.java
@@ -22,7 +22,7 @@ public class TraceabilityService {
 	TraceabilityServiceClient client;
 	TermServerScript ts;
 	private static int BATCH_SIZE = 100;
-	Map<Long, ReportRow> batchedReportRowMap = new LinkedHashMap<>();
+	Map<Long, List<ReportRow>> batchedReportRowMap = new LinkedHashMap<>();
 	String areaOfInterest;
 	
 	public TraceabilityService(JobRun jobRun, TermServerScript ts, String areaOfInterest) {
@@ -34,7 +34,14 @@ public class TraceabilityService {
 	public void populateTraceabilityAndReport(int reportTabIdx, Concept c, Object... details) throws TermServerScriptException {
 		//We'll cache this row until we have enough to be worth making a call to traceability
 		Long conceptId = Long.parseLong(c.getConceptId());
-		batchedReportRowMap.put(conceptId, new ReportRow(reportTabIdx, c, details));
+		
+		//Do we already have a row for this concept?
+		List<ReportRow> rows = batchedReportRowMap.get(conceptId);
+		if (rows == null) {
+			rows = new ArrayList<>();
+			batchedReportRowMap.put(conceptId, rows);
+		}
+		rows.add(new ReportRow(reportTabIdx, c, details));
 		if (batchedReportRowMap.size() >= BATCH_SIZE) {
 			flush();
 		}
@@ -47,9 +54,7 @@ public class TraceabilityService {
 	}
 
 	private void populateReportRowsWithTraceabilityInfo() {
-		List<Long> conceptIds = batchedReportRowMap.values().stream()
-				.map(r -> Long.parseLong(r.c.getConceptId()))
-				.collect(Collectors.toList());
+		List<Long> conceptIds = new ArrayList<>(batchedReportRowMap.keySet());
 		List<Activity> traceabilityInfo = client.getConceptActivity(conceptIds, areaOfInterest, ActivityType.CONTENT_CHANGE);
 		if (traceabilityInfo.size() == 0) {
 			logger.warn("Failed to recover any traceability information for {} concepts", conceptIds.size());
@@ -59,8 +64,9 @@ public class TraceabilityService {
 			info[0] = activity.getUser().getUsername();
 			info[1] = activity.getBranch().getBranchPath();
 			info[2] = activity.getCommitDate().toInstant().atZone(ZoneId.systemDefault()).toString();
-			//TODO Check for multiple rows for single concept
-			batchedReportRowMap.get(getConceptId(activity, conceptIds)).traceabilityInfo = info;
+			for (ReportRow row : batchedReportRowMap.get(getConceptId(activity, conceptIds))) {
+				row.traceabilityInfo = info;
+			}
 		}
 	}
 	
@@ -76,11 +82,13 @@ public class TraceabilityService {
 	}
 
 	private void report() throws TermServerScriptException {
-		for (ReportRow row : batchedReportRowMap.values()) {
-			if (row.details == null) {
-				ts.report(row.reportTabIdx, row.c, row.traceabilityInfo);
-			} else {
-				ts.report(row.reportTabIdx, row.c, row.details, row.traceabilityInfo);
+		for (List<ReportRow> conceptRows : batchedReportRowMap.values()) {
+			for (ReportRow row : conceptRows) {
+				if (row.details == null) {
+					ts.report(row.reportTabIdx, row.c, row.traceabilityInfo);
+				} else {
+					ts.report(row.reportTabIdx, row.c, row.details, row.traceabilityInfo);
+				}
 			}
 		}
 	}
