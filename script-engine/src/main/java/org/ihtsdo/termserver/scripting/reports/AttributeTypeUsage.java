c6ca1e122d1a81bcb1753a8a9723776a025c2875
pgwilliams <petergwilliams@hotmail.com>
2020-10-19 13:57:48 +0100

    Ad-hoc report for listing attribute types in use

---
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/reports/AttributeTypeUsage.java
@@ -0,0 +1,68 @@
+package org.ihtsdo.termserver.scripting.reports;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.ihtsdo.otf.exception.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.ReportClass;
+import org.ihtsdo.termserver.scripting.dao.ReportSheetManager;
+import org.ihtsdo.termserver.scripting.domain.*;
+import org.snomed.otf.scheduler.domain.*;
+import org.snomed.otf.scheduler.domain.Job.ProductionStatus;
+
+/**
+**/
+public class AttributeTypeUsage extends TermServerReport implements ReportClass {
+	public static final String TARGET_ATTRIBUTE_TYPE = "Attribute Type";
+	private Concept targetAttributeType;
+	private CharacteristicType charType = CharacteristicType.STATED_RELATIONSHIP;
+	
+	public static void main(String[] args) throws TermServerScriptException, IOException {
+		Map<String, String> params = new HashMap<>();
+		params.put(ECL, "*");
+		params.put(TARGET_ATTRIBUTE_TYPE, "766939001 |Plays role (attribute)|");
+		TermServerReport.run(AttributeTypeUsage.class, args, params);
+	}
+	
+	public void init (JobRun run) throws TermServerScriptException {
+		ReportSheetManager.targetFolderId = "1F-KrAwXrXbKj5r-HBLM0qI5hTzv-JgnU"; //Ad-hoc Reports
+		additionalReportColumns="FSN, SemTag, Relationship";
+		subsetECL = run.getMandatoryParamValue(ECL);
+		super.init(run);
+	}
+	
+	@Override
+	public void postInit() throws TermServerScriptException {
+		super.postInit();
+		targetAttributeType = gl.getConcept(jobRun.getMandatoryParamValue(TARGET_ATTRIBUTE_TYPE));
+	}
+	
+	@Override
+	public Job getJob() {
+		JobParameters params = new JobParameters()
+				.add(ECL).withType(JobParameter.Type.ECL).withMandatory()
+				.add(TARGET_ATTRIBUTE_TYPE).withType(JobParameter.Type.CONCEPT).withMandatory()
+				.build();
+		
+		return new Job()
+				.withCategory(new JobCategory(JobType.REPORT, JobCategory.ADHOC_QUERIES))
+				.withName("Attribute Type Usage")
+				.withDescription("This report lists all instances of a particular attribute type being used.") 
+				.withProductionStatus(ProductionStatus.HIDEME)
+				.withParameters(params)
+				.withTag(INT)
+				.build();
+	}
+	
+	public void runJob() throws TermServerScriptException {
+		ArrayList<Concept> subset = new ArrayList<>(findConcepts(subsetECL));
+		subset.sort(Comparator.comparing(Concept::getFsn));
+		for (Concept c : subset) {
+			for (Relationship r : c.getRelationships(charType, targetAttributeType, ActiveState.ACTIVE)) {
+				report (c, r);
+				countIssue(c);
+			}
+		}
+	}
+	
+}
