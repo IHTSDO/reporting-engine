821bcaaa33b51df7aedaec146152af8e44868c50
Darryn McGaw <dmcg@ihtsdo.org>
2020-11-02 12:40:26 +0000

    RP-397 Update wordsInReverse method in ReleaseIssuesReport to only loop active Concepts and Descriptions

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/reports/release/ReleaseIssuesReport.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/reports/release/ReleaseIssuesReport.java
@@ -572,11 +572,10 @@ public class ReleaseIssuesReport extends TermServerReport implements ReportClass
 		String issueStr = "Potential mistyped word in Description";
 		initialiseSummary(issueStr);
 
-		for (Concept c : gl.getAllConcepts()) {
+		for (Concept c : allActiveConcepts) {
 			if (inScope(c) && (includeLegacyIssues || recentlyTouched.contains(c))) {
-				ActiveState activeState = includeLegacyIssues ? ActiveState.BOTH : ActiveState.ACTIVE;
 				nextDescription:
-				for (Description d : c.getDescriptions(activeState)) {
+				for (Description d : c.getDescriptions(ActiveState.ACTIVE)) {
 					String[] words = d.getTerm().split(" ");
 					for (String currentWord : words) {
 						String currentWordInReverse = new StringBuilder(currentWord).reverse().toString();
