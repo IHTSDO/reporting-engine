0587ce6bcb01ccf1dcc232ece123469fe3d413e8
pgwilliams <petergwilliams@hotmail.com>
2021-03-03 10:13:36 +0000

    QI-816 Replace various parents in subhierarchy

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/ReplaceParents.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/ReplaceParents.java
@@ -11,14 +11,11 @@ import org.ihtsdo.termserver.scripting.util.SnomedUtils;
 
 
 /*
-For DRUG-422, DRUG-431
-Driven by a text file of concepts, move specified concepts to exist under
-a parent concept.
+QI-816 Replace Stated Parents where safe to do so
 */
 public class ReplaceParents extends BatchFix implements RF2Constants{
 	
-	Relationship newParentRel;
-	String newParent = "763158003"; // |Medicinal product (product)| 
+	Map<Concept, Concept> parentReplacementMap = new HashMap<>();
 	
 	protected ReplaceParents(BatchFix clone) {
 		super(clone);
@@ -28,9 +25,10 @@ public class ReplaceParents extends BatchFix implements RF2Constants{
 		ReplaceParents fix = new ReplaceParents(null);
 		try {
 			fix.reportNoChange = true;
-			fix.populateEditPanel = false;
+			fix.populateEditPanel = true;
 			fix.populateTaskDescription = true;
-			fix.additionalReportColumns = "ACTION_DETAIL, DEF_STATUS, PARENT_COUNT, ATTRIBUTE_COUNT";
+			fix.selfDetermining = true;
+			fix.keepIssuesTogether = true;
 			fix.init(args);
 			//Recover the current project state from TS (or local cached archive) to allow quick searching of all concepts
 			fix.loadProjectSnapshot(true); 
@@ -42,8 +40,12 @@ public class ReplaceParents extends BatchFix implements RF2Constants{
 	}
 
 	private void postLoadInit() throws TermServerScriptException {
-		Concept parentConcept =  gl.getConcept(newParent);
-		newParentRel = new Relationship(null, IS_A, parentConcept, 0);
+		subsetECL = "< 55680006 |Drug overdose (disorder)|";
+		Concept overdose = gl.getConcept("1149222004 |Overdose (disorder)|");
+		parentReplacementMap.put(gl.getConcept("55680006 |Drug overdose (disorder)|"), overdose);
+		parentReplacementMap.put(gl.getConcept("59369008 |Accidental drug overdose (disorder)|"), overdose);
+		parentReplacementMap.put(gl.getConcept("59274003 |Intentional drug overdose (disorder)|"), overdose);
+		super.postInit();
 	}
 
 	@Override
@@ -53,7 +55,7 @@ public class ReplaceParents extends BatchFix implements RF2Constants{
 		int changesMade = replaceParents(t, loadedConcept);
 		
 		//All concepts should be fully defined, if possible
-		if (loadedConcept.getDefinitionStatus().equals(DefinitionStatus.PRIMITIVE)) {
+		/*if (loadedConcept.getDefinitionStatus().equals(DefinitionStatus.PRIMITIVE)) {
 			if (countAttributes(loadedConcept, CharacteristicType.STATED_RELATIONSHIP) > 0) {
 				loadedConcept.setDefinitionStatus(DefinitionStatus.FULLY_DEFINED);
 				changesMade++;
@@ -61,59 +63,58 @@ public class ReplaceParents extends BatchFix implements RF2Constants{
 			} else {
 				report (t, loadedConcept, Severity.HIGH, ReportActionType.VALIDATION_CHECK, "Unable to mark fully defined - no attributes!");
 			}
-		}
-		
+		}*/
 		updateConcept(t, loadedConcept, info);
 		return changesMade;
 	}
 
-	private int replaceParents(Task task, Concept loadedConcept) throws TermServerScriptException {
+	private int replaceParents(Task t, Concept c) throws TermServerScriptException {
 		
 		int changesMade = 0;
-		Set<Relationship> parentRels = new HashSet<Relationship> (loadedConcept.getRelationships(CharacteristicType.STATED_RELATIONSHIP, 
+		Concept replacementParent = gl.getConcept("1149222004 |Overdose (disorder)|");
+		Set<Relationship> parentRels = new HashSet<Relationship> (c.getRelationships(CharacteristicType.STATED_RELATIONSHIP, 
 																		IS_A,
 																		ActiveState.ACTIVE));
-		String parentCount = Integer.toString(parentRels.size());
-		String attributeCount = Integer.toString(countAttributes(loadedConcept, CharacteristicType.STATED_RELATIONSHIP));
 		
-		String semTag = SnomedUtils.deconstructFSN(loadedConcept.getFsn())[1];
-		switch (semTag) {
-			case "(medicinal product form)" : loadedConcept.setConceptType(ConceptType.MEDICINAL_PRODUCT_FORM);
-												break;
-			case "(product)" : loadedConcept.setConceptType(ConceptType.PRODUCT);
-								break;
-			case "(medicinal product)" : loadedConcept.setConceptType(ConceptType.MEDICINAL_PRODUCT);
-										 break;
-			case "(clinical drug)" : loadedConcept.setConceptType(ConceptType.CLINICAL_DRUG);
-										break;
-			default : loadedConcept.setConceptType(ConceptType.UNKNOWN);
-		}
 		
-		boolean replacementNeeded = true;
-		for (Relationship parentRel : parentRels) {
-			if (!parentRel.equals(newParentRel)) {
-				removeParentRelationship (task, parentRel, loadedConcept, newParentRel.getTarget().toString(), null);
-				changesMade++;
-			} else {
-				replacementNeeded = false;
+		//Calculate the proximal primitive parent to ensure we're not losing information
+		List<Concept> ppps = determineProximalPrimitiveParents(c);
+		for (Concept ppp : ppps) {
+			if (!ppp.equals(replacementParent) && !parentReplacementMap.containsKey(ppp)) {
+				report (t, c, Severity.HIGH, ReportActionType.VALIDATION_ERROR, "Calculated PPP indicates potential loss of information", ppp);
+				return NO_CHANGES_MADE;
 			}
 		}
 		
-		if (replacementNeeded) {
-			Relationship thisNewParentRel = newParentRel.clone(null);
-			thisNewParentRel.setSource(loadedConcept);
-			loadedConcept.addRelationship(thisNewParentRel);
-			changesMade++;
-			String msg = "Single parent set to " + newParent;
-			report (task, loadedConcept, Severity.LOW, ReportActionType.RELATIONSHIP_INACTIVATED, msg, loadedConcept.getDefinitionStatus().toString(), parentCount, attributeCount);
+		boolean replacementMade = false;
+		
+		//TODO This is a special situation as we're forcing the map to a single value
+		for (Relationship parentRel : parentRels) {
+			if (replacementMade) {
+				c.removeRelationship(parentRel);
+				report (t, c, Severity.LOW, ReportActionType.RELATIONSHIP_INACTIVATED, parentRel);
+			} else {
+				changesMade += replaceParent(t, c, parentRel.getTarget(), replacementParent);
+				replacementMade = true;
+			}
 		}
 		return changesMade;
 	}
 
 	@Override
-	protected List<Component> loadLine(String[] lineItems) throws TermServerScriptException {
-		Concept c = gl.getConcept(lineItems[0]);
-		return Collections.singletonList(c);
+	protected List<Component> identifyComponentsToProcess() throws TermServerScriptException {
+		List<Component> toProcess = new ArrayList<>();
+		Concept[] types = new Concept[] { CAUSE_AGENT };
+		Set<Concept> targetParents = parentReplacementMap.keySet();
+		for (Concept c : findConcepts(subsetECL, true, true)) {
+			Set<Concept> parents = c.getParents(CharacteristicType.STATED_RELATIONSHIP);
+			if (!Collections.disjoint(parents, targetParents)) {
+				//If we have a causative agent, add that as an issue
+				SnomedUtils.getTargets(c, types, CharacteristicType.INFERRED_RELATIONSHIP);
+				toProcess.add(c);
+			}
+		}
+		return toProcess;
 	}
 
 }
