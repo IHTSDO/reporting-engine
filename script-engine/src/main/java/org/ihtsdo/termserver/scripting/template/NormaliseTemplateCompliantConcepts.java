e2c47d7c9d4764999dff01dc8e8eae3c84bd7edf
pgwilliams <petergwilliams@hotmail.com>
2021-03-25 14:21:13 +0000

    QI-825 Normalise Calculus

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/template/NormaliseTemplateCompliantConcepts.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/template/NormaliseTemplateCompliantConcepts.java
@@ -391,8 +391,9 @@ public class NormaliseTemplateCompliantConcepts extends TemplateFix {
 		templateNames = new String[] { "templates/morphologies/Posterior displacement.json" };
 		templateNames = new String[] { "templates/morphologies/Separation.json" };
 		templateNames = new String[] { "templates/morphologies/Retention.json" };
-		*/
 		templateNames = new String[] { "templates/procedures/MRI.json" };
+		*/
+		templateNames = new String[] { "templates/morphologies/Calculus.json" };
 		//TODO We're seeing 'HIGH' warnings about existing parents being redundant in presence of PPP but before the PPP gets added. Investigate
 		//I think this might happen when we set a PPP which is lower than the existing parent.
 		super.init(args);
