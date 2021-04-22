bd964d63901bfb74130b70374addffce730c0613
pgwilliams <petergwilliams@hotmail.com>
2021-04-22 17:50:58 +0100

    RP-456 Debug to ensure authToken is what we think it is

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/job/mq/Receiver.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/job/mq/Receiver.java
@@ -7,6 +7,7 @@ import org.snomed.otf.scheduler.domain.JobRun;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class Receiver {
@@ -18,7 +19,11 @@ public class Receiver {
 	
 	@JmsListener(destination = "${schedule.manager.queue.request}")
 	public void receiveMessage(JobRun jobRun) {
-		logger.info("Received request to run {} with parameters: {} ", jobRun, jobRun.getParameters());
+		String cookieStart = "AuthToken Missing";
+		if (!StringUtils.isEmpty(jobRun.getAuthToken()) && jobRun.getAuthToken().length() > 16) {
+			cookieStart = jobRun.getAuthToken().substring(0,15);
+		}
+		logger.info("Received request to run {} with parameters: {} and authToken starting: {}", jobRun, jobRun.getParameters(), cookieStart);
 		jobManager.run(jobRun);
 	}
 
