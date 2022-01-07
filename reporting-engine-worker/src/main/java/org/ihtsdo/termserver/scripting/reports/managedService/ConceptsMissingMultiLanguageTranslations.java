a989cb4d9a4a430a6b010034af2b2db79162fa93
pgwilliams <petergwilliams@hotmail.com>
2022-01-07 12:07:46 +0000

    RP-536 Flip parameter logic with re-wording of 'Include Int Concepts' -> 'Extension Concepts Only'

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/managedService/ConceptsMissingMultiLanguageTranslations.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/managedService/ConceptsMissingMultiLanguageTranslations.java
@@ -16,7 +16,7 @@ import org.snomed.otf.script.dao.ReportSheetManager;
 
 public class ConceptsMissingMultiLanguageTranslations extends TermServerReport implements ReportClass {
 	
-	private static final String INCLUDE_INT_CONCEPTS = "Include international concepts";
+	private static final String EXTENSION_CONCEPTS_ONLY = "Extension Concepts Only";
 	//private static final String INCLUDE_UNTRANSLATED_CONCEPTS = "Include untranslated concepts";
 	private static final String INCLUDE_INACTIVE_CONCEPTS = "Include inactive concepts";
 	Set<String> expectedLanguages = new HashSet<>();
@@ -26,7 +26,7 @@ public class ConceptsMissingMultiLanguageTranslations extends TermServerReport i
 	
 	public static void main(String[] args) throws TermServerScriptException, IOException {
 		Map<String, String> params = new HashMap<>();
-		params.put(INCLUDE_INT_CONCEPTS, "true");
+		params.put(EXTENSION_CONCEPTS_ONLY, "false");
 		//params.put(INCLUDE_UNTRANSLATED_CONCEPTS, "false");
 		params.put(INCLUDE_INACTIVE_CONCEPTS, "false");
 		TermServerReport.run(ConceptsMissingMultiLanguageTranslations.class, args, params);
@@ -36,7 +36,7 @@ public class ConceptsMissingMultiLanguageTranslations extends TermServerReport i
 		ReportSheetManager.targetFolderId = "15WXT1kov-SLVi4cvm2TbYJp_vBMr4HZJ"; //Release QA Reports
 		getArchiveManager().setRunIntegrityChecks(false);
 		subsetECL = run.getParamValue(ECL);
-		includeIntConcepts = run.getParamBoolean(INCLUDE_INT_CONCEPTS);
+		includeIntConcepts = !run.getParamBoolean(EXTENSION_CONCEPTS_ONLY);
 		//includeUntranslatedConcepts=run.getParamBoolean(INCLUDE_UNTRANSLATED_CONCEPTS);
 		includeUntranslatedConcepts = false;
 		includeInactiveConcepts = run.getParamBoolean(INCLUDE_INACTIVE_CONCEPTS);
@@ -74,7 +74,7 @@ public class ConceptsMissingMultiLanguageTranslations extends TermServerReport i
 	public Job getJob() {
 		JobParameters params = new JobParameters()
 				.add(ECL).withType(Type.ECL)
-				.add(INCLUDE_INT_CONCEPTS).withType(JobParameter.Type.BOOLEAN).withDefaultValue(true)
+				.add(EXTENSION_CONCEPTS_ONLY).withType(JobParameter.Type.BOOLEAN).withDefaultValue(false)
 				//.add(INCLUDE_UNTRANSLATED_CONCEPTS).withType(JobParameter.Type.BOOLEAN).withDefaultValue(false)
 				.add(INCLUDE_INACTIVE_CONCEPTS).withType(JobParameter.Type.BOOLEAN).withDefaultValue(false)
 				.build();
