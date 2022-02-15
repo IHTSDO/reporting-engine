cd2275b6df2a272bc01d2d5bd35f6e8916c8ab8a
pgwilliams <petergwilliams@hotmail.com>
2022-02-15 16:41:32 +0000

    INFRA-8071 Run Summary Component Stats for the published packages

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/release/SummaryComponentStats.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/release/SummaryComponentStats.java
@@ -50,10 +50,11 @@ public class SummaryComponentStats extends HistoricDataUser implements ReportCla
 	
 	public static void main(String[] args) throws TermServerScriptException, IOException {
 		Map<String, String> params = new HashMap<>();
-		params.put(PREV_RELEASE, "SnomedCT_InternationalRF2_PRODUCTION_20210731T120000Z.zip");
-		params.put(THIS_RELEASE, "xSnomedCT_InternationalRF2_MEMBER_20220131T120000Z.zip");
+		//params.put(PREV_RELEASE, "SnomedCT_InternationalRF2_PRODUCTION_20210731T120000Z.zip");
+		//params.put(THIS_RELEASE, "SnomedCT_InternationalRF2_PRODUCTION_20220131T120000Z.zip");
 		//params.put(REPORT_OUTPUT_TYPES, "S3");
 		//params.put(REPORT_FORMAT_TYPE, "JSON");
+		params.put(MODULES, "731000124108");
 		TermServerReport.run(SummaryComponentStats.class, args, params);
 	}
 
@@ -81,7 +82,7 @@ public class SummaryComponentStats extends HistoricDataUser implements ReportCla
 	}
 	
 	public void init (JobRun run) throws TermServerScriptException {
-		ReportSheetManager.targetFolderId = "15WXT1kov-SLVi4cvm2TbYJp_vBMr4HZJ"; //Release QA
+		ReportSheetManager.targetFolderId = "1od_0-SCbfRz0MY-AYj_C0nEWcsKrg0XA"; //Release Stats
 		summaryDataMap = new HashMap<>();
 		refsetDataMap = new HashMap<>();
 		
