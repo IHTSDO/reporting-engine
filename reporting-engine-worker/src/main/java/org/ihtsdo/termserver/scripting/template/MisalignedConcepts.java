54fe654ec13f4dcab9d7ca1554a958dcd6ff55fa
pgwilliams <petergwilliams@hotmail.com>
2022-03-18 09:47:55 +0000

    QI-943 Run Biopsy misaligned concepts report

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/template/MisalignedConcepts.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/template/MisalignedConcepts.java
@@ -47,8 +47,8 @@ public class MisalignedConcepts extends TemplateFix implements ReportClass {
 	
 	public static void main(String[] args) throws TermServerScriptException, IOException {
 		Map<String, String> params = new HashMap<>();
-		params.put(ECL, "<<  302495003 |Arthrotomy (procedure)|");
-		params.put(TEMPLATE, "71388002 |Procedure (procedure)| : [[~0..1]]{[[~1..1]]  363702006 |Has focus (attribute)|  = [[+id(<< 399269003 |Arthropathy (disorder)| )]]}[[~1..1]]{[[~1..1]]  260686004 |Method (attribute)|  = [[+id( << 129287005 |Incision - action (qualifier value)| )]],[[~1..1]]  405813007 |Procedure site - Direct (attribute)|  = [[+id(<<  39352004 |Joint structure (body structure)| )]],[[~0..1]]  260507000 |Access (attribute)| = [[+id(< 309795001 |Surgical access values (qualifier value)| )]]}[[~0..*]]{[[~1..1]]  260686004 |Method (attribute)|  = [[+id(<< 129284003 |Surgical action (qualifier value)| )]],[[~0..1]]  405813007 |Procedure site - Direct (attribute)|  = [[+id(<<  91723000 |Anatomical structure (body structure)| )]],[[~0..1]]  405814001 |Procedure site - Indirect (attribute)|  = [[+id(<<  91723000 |Anatomical structure (body structure)| )]],[[~0..1]]  363701004 |Direct substance (attribute)|  = [[+id (<< 105590001 |Substance (substance)| )]],[[~0..1]]  363699004 |Direct device (attribute)|  = [[+id(<  49062001 |Device (physical object)| )]],[[~0..1]]  363700003 |Direct morphology (attribute)|  = [[+id(<<  49755003 |Morphologically abnormal structure (morphologic abnormality)| )]],[[~0..1]]  363701004 |Direct substance (attribute)|  = [[+id(<< 261217004 |Substance (attribute)| )]]}");
+		//params.put(ECL, "<<  302495003 |Arthrotomy (procedure)|");
+		//params.put(TEMPLATE, "71388002 |Procedure (procedure)| : [[~0..1]]{[[~1..1]]  363702006 |Has focus (attribute)|  = [[+id(<< 399269003 |Arthropathy (disorder)| )]]}[[~1..1]]{[[~1..1]]  260686004 |Method (attribute)|  = [[+id( << 129287005 |Incision - action (qualifier value)| )]],[[~1..1]]  405813007 |Procedure site - Direct (attribute)|  = [[+id(<<  39352004 |Joint structure (body structure)| )]],[[~0..1]]  260507000 |Access (attribute)| = [[+id(< 309795001 |Surgical access values (qualifier value)| )]]}[[~0..*]]{[[~1..1]]  260686004 |Method (attribute)|  = [[+id(<< 129284003 |Surgical action (qualifier value)| )]],[[~0..1]]  405813007 |Procedure site - Direct (attribute)|  = [[+id(<<  91723000 |Anatomical structure (body structure)| )]],[[~0..1]]  405814001 |Procedure site - Indirect (attribute)|  = [[+id(<<  91723000 |Anatomical structure (body structure)| )]],[[~0..1]]  363701004 |Direct substance (attribute)|  = [[+id (<< 105590001 |Substance (substance)| )]],[[~0..1]]  363699004 |Direct device (attribute)|  = [[+id(<  49062001 |Device (physical object)| )]],[[~0..1]]  363700003 |Direct morphology (attribute)|  = [[+id(<<  49755003 |Morphologically abnormal structure (morphologic abnormality)| )]],[[~0..1]]  363701004 |Direct substance (attribute)|  = [[+id(<< 261217004 |Substance (attribute)| )]]}");
 		params.put(INCLUDE_COMPLEX, "false");
 		params.put(INCLUDE_ORPHANET, "true");
 		params.put(KNOWN_COMPLETE, "true");
@@ -391,8 +391,12 @@ public class MisalignedConcepts extends TemplateFix implements ReportClass {
 		templateNames = new String[] { "templates/Poisoning.json"};
 		
 		templateNames = new String[] { "templates/drugs/MP only.json" };
+		
+		templateNames = new String[] { "templates/procedures/CT Guided.json" };
+		
+		templateNames = new String[] { "templates/procedures/Imaging Guided Biopsy.json" };
 		*/
-		//templateNames = new String[] { "templates/procedures/CT Guided.json" };
+		templateNames = new String[] { "templates/procedures/Needle Biopsy.json" };
 		
 		super.init((String[])null);
 	}
