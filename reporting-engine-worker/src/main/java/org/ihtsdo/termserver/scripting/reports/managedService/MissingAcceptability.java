c97833a7b5b3fc73ec443b41e99ded8bcecd5b2c
pgwilliams <petergwilliams@hotmail.com>
2022-05-03 13:36:10 +0100

    RP-565 New report for NZ to detect descriptions missing from NZ LangRefset.  Tracks GB setting (optionally, by default)

---
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/managedService/MissingAcceptability.java
@@ -0,0 +1,123 @@
+package org.ihtsdo.termserver.scripting.reports.managedService;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.ihtsdo.otf.exception.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.ReportClass;
+import org.ihtsdo.termserver.scripting.domain.*;
+import org.ihtsdo.termserver.scripting.reports.TermServerReport;
+import org.ihtsdo.termserver.scripting.util.SnomedUtils;
+import org.snomed.otf.scheduler.domain.*;
+import org.snomed.otf.scheduler.domain.Job.ProductionStatus;
+import org.snomed.otf.scheduler.domain.JobParameter.Type;
+import org.snomed.otf.script.dao.ReportSheetManager;
+/**
+ * RP-565
+ */
+public class MissingAcceptability extends TermServerReport implements ReportClass {
+	
+	private static final String INCLUDE_INACTIVE_CONCEPTS = "Include inactive concepts";
+	private static final String TRACK_DIALECT = "Track dialect";
+
+	private String defaultLangRefset = null;
+	private boolean includeInactiveConcepts = false;
+	private String trackDialect = null;
+	
+	public static void main(String[] args) throws TermServerScriptException, IOException {
+		Map<String, String> params = new HashMap<>();
+		params.put(INCLUDE_INACTIVE_CONCEPTS, "false");
+		params.put(TRACK_DIALECT, "GB");
+		TermServerReport.run(MissingAcceptability.class, args, params);
+	}
+	
+	public void init (JobRun run) throws TermServerScriptException {
+		ReportSheetManager.targetFolderId = "1mvrO8P3n94YmNqlWZkPJirmFKaFUnE0o"; //Managed Service
+		subsetECL = run.getParamValue(ECL);
+		includeInactiveConcepts = run.getParamBoolean(INCLUDE_INACTIVE_CONCEPTS);
+		
+		String trackDialectStr = run.getParamValue(TRACK_DIALECT);
+		if (!StringUtils.isEmpty(trackDialectStr)) {
+			switch(trackDialectStr) {
+				case "US" : trackDialect = US_ENG_LANG_REFSET;
+					break;
+				case "GB" : trackDialect = GB_ENG_LANG_REFSET;
+					break;
+				default : throw new IllegalArgumentException("Unable to identify refsetId from '" + trackDialectStr + "'");
+			}
+		}
+		
+		super.init(run);
+		if (project.getKey().equals("MAIN")) {
+			throw new TermServerScriptException("Missing Acceptability Report cannot be run against MAIN");
+		}
+	}
+	
+	public void postInit() throws TermServerScriptException {
+		defaultLangRefset = project.getMetadata().getDefaultLangRefset();
+		String[] columnHeadings = new String[] {"SCTID, FSN, SemTag, Descriptions"};
+		String[] tabNames = new String[] {	"Missing LangRefset Entry"};
+		super.postInit(tabNames, columnHeadings, false);
+	}
+
+	@Override
+	public Job getJob() {
+		JobParameters params = new JobParameters()
+				.add(ECL).withType(Type.ECL)
+				.add(INCLUDE_INACTIVE_CONCEPTS).withType(JobParameter.Type.BOOLEAN).withDefaultValue(false)
+				.add(TRACK_DIALECT).withType(Type.DROPDOWN).withOptions("US", "GB")
+				.build();
+		return new Job()
+				.withCategory(new JobCategory(JobType.REPORT, JobCategory.RELEASE_VALIDATION))
+				.withName("Terms Missing Acceptability")
+				.withDescription("This reports lists all descriptions which are missing a lang refset acceptability in the default language reference set.")
+				.withProductionStatus(ProductionStatus.PROD_READY)
+				.withParameters(params)
+				.withTag(MS)
+				.withExpectedDuration(30)
+				.build();
+	}
+	
+	public void runJob() throws TermServerScriptException {
+		Collection<Concept> conceptsOfInterest;
+		if (subsetECL != null && !subsetECL.isEmpty()) {
+			conceptsOfInterest = findConcepts(subsetECL);
+		} else {
+			conceptsOfInterest = gl.getAllConcepts();
+		}
+		
+		for (Concept c : scopeAndSort(conceptsOfInterest)) {
+			String descriptionsToReport = "";
+			for (Description d : c.getDescriptions(ActiveState.ACTIVE)) {
+				//Are we tracking some existing English Dialect?
+				if (trackDialect == null || d.getAcceptability(trackDialect) != null) {
+					if (d.getAcceptability(defaultLangRefset) == null) {
+						if (descriptionsToReport.length() > 0) {
+							descriptionsToReport += "\n";
+						}
+						descriptionsToReport += d;
+					}
+				}
+			}
+			
+			if (descriptionsToReport.length() > 0) {
+				report (c, descriptionsToReport);
+				countIssue(c);
+			}
+		}
+	}
+	
+	private List<Concept> scopeAndSort(Collection<Concept> superSet) {
+		return superSet.stream()
+		.filter (c -> inScope(c))
+		.sorted((c1, c2) -> SnomedUtils.compareSemTagFSN(c1,c2))
+		.collect(Collectors.toList());
+	}
+	
+	private boolean inScope(Concept c) {
+		return includeInactiveConcepts || c.isActive();
+	}
+
+}
