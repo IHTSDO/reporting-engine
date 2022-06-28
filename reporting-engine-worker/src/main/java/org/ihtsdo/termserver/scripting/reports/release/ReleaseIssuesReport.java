53769778f7199712e1c3449e5d8de86b25b41fb7
pgwilliams <petergwilliams@hotmail.com>
2022-06-28 15:44:46 +0100

    MSSP-1560 Clarify Release Issues report output location

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/release/ReleaseIssuesReport.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/release/ReleaseIssuesReport.java
@@ -103,7 +103,7 @@ public class ReleaseIssuesReport extends TermServerReport implements ReportClass
 	}
 	
 	public void init (JobRun run) throws TermServerScriptException {
-		ReportSheetManager.targetFolderId = "15WXT1kov-SLVi4cvm2TbYJp_vBMr4HZJ"; //Release QA
+		ReportSheetManager.targetFolderId = "15WXT1kov-SLVi4cvm2TbYJp_vBMr4HZJ"; //Release Validation
 		super.init(run);
 		includeLegacyIssues = run.getParameters().getMandatoryBoolean(INCLUDE_ALL_LEGACY_ISSUES);
 		additionalReportColumns = "FSN, Semtag, Issue, Legacy, C/D/R Active, Detail";
