acf0721925033faa7daaa7260f9b87dd6c9e7fd4
pgwilliams <petergwilliams@hotmail.com>
2022-09-21 16:17:20 +0100

    QI-1179 Generic enhancement of group manipulation

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/ReplaceAttributeValues.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/ReplaceAttributeValues.java
@@ -3,7 +3,6 @@ package org.ihtsdo.termserver.scripting.fixes;
 import java.util.*;
 
 import org.apache.commons.lang.exception.ExceptionUtils;
-import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Task;
 import org.ihtsdo.otf.exception.TermServerScriptException;
 import org.ihtsdo.termserver.scripting.ValidationFailure;
