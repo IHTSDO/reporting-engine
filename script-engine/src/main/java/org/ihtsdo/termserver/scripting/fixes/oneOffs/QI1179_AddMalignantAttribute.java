18e4c5f512a9868368aff3123e397b58ea25df5e
pgwilliams <petergwilliams@hotmail.com>
2022-09-21 16:18:22 +0100

    QI-1179 Various tweaks to remodeling steps as discussed in Jira ticket

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/oneOffs/QI1179_AddMalignantAttribute.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/oneOffs/QI1179_AddMalignantAttribute.java
@@ -9,6 +9,7 @@ import org.ihtsdo.otf.rest.client.terminologyserver.pojo.*;
 import org.ihtsdo.otf.exception.TermServerScriptException;
 import org.ihtsdo.termserver.scripting.ValidationFailure;
 import org.ihtsdo.termserver.scripting.domain.*;
+import org.ihtsdo.termserver.scripting.domain.RelationshipTemplate.Mode;
 import org.ihtsdo.termserver.scripting.fixes.BatchFix;
 import org.ihtsdo.termserver.scripting.util.SnomedUtils;
 import org.snomed.otf.script.dao.ReportSheetManager;
@@ -22,6 +23,7 @@ public class QI1179_AddMalignantAttribute extends BatchFix {
 	private RelationshipTemplate relTemplate;
 	private RelationshipTemplate workAroundToRemove;
 	String inclusionText = "primary";
+	private Map<Concept, Concept> replaceValuesMap;
 	
 	protected QI1179_AddMalignantAttribute(BatchFix clone) {
 		super(clone);
@@ -47,10 +49,12 @@ public class QI1179_AddMalignantAttribute extends BatchFix {
 	}
 
 	private void postLoadInit() throws TermServerScriptException {
-		subsetECL = "< 372087000 |Primary malignant neoplasm (disorder)| MINUS  << 269475001 |Malignant tumor of lymphoid, hemopoietic AND/OR related tissue (disorder)|";
+		subsetECL = "< 372087000 |Primary malignant neoplasm (disorder)| MINUS ( << 269475001 |Malignant tumor of lymphoid, hemopoietic AND/OR related tissue (disorder)| OR (< 372087000 |Primary malignant neoplasm (disorder)| : 370135005 |Pathological process (attribute)| = 1234914003 |Malignant proliferation of primary neoplasm (qualifier value)|))";
 		relTemplate = new RelationshipTemplate(PATHOLOGICAL_PROCESS, gl.getConcept("1234914003 |Malignant proliferation of primary neoplasm|"));
 		workAroundToRemove = new RelationshipTemplate(ASSOC_MORPH, gl.getConcept("86049000 |Malignant neoplasm, primary (morphologic abnormality)|"));
 		exclusions = new HashSet<>();
+		replaceValuesMap = new HashMap<>();
+		replaceValuesMap.put(gl.getConcept("86049000 |Malignant neoplasm, primary|"), gl.getConcept("1240414004 |Malignant neoplasm morphology|"));
 		super.postInit();
 	}
 
@@ -58,6 +62,9 @@ public class QI1179_AddMalignantAttribute extends BatchFix {
 	public int doFix(Task task, Concept concept, String info) throws TermServerScriptException {
 		int changesMade = 0;
 		try {
+			if (concept.getId().equals("285645000")) {
+				debug("here");
+			}
 			Concept loadedConcept = loadConcept(concept, task.getBranchPath());
 			changesMade = addAttribute(task, loadedConcept);
 			if (changesMade > 0) {
@@ -75,22 +82,60 @@ public class QI1179_AddMalignantAttribute extends BatchFix {
 
 	private int addAttribute(Task t, Concept c) throws TermServerScriptException {
 		int changesMade = 0;
+		boolean groupRemoved = false;
 		if (isExcluded(c)) {
 			report(t, c, Severity.MEDIUM, ReportActionType.VALIDATION_CHECK, "Concept Excluded due to lexical rule");
 		} else {
 			List<RelationshipGroup> groupsToProcess = c.getRelationshipGroups(CharacteristicType.STATED_RELATIONSHIP, FINDING_SITE);
 			for (RelationshipGroup g : groupsToProcess) {
+				boolean thisGroupRemoved = false;
 				//Is this an additional group that we need to remove?
 				if (groupsToProcess.size() > 1 && g.containsTypeValue(workAroundToRemove)) {
 					//Does the finding site in this group match the finding site in another group?
+					//Or also remove if self grouped
 					Concept thisFindingSiteValue = g.getValueForType(FINDING_SITE, true);
 					RelationshipTemplate findMe = new RelationshipTemplate(FINDING_SITE, thisFindingSiteValue);
-					if (SnomedUtils.hasAttributeInAnotherGroup(c, findMe, g.getGroupId())) {
-						removeRelationshipGroup(t, c, g);
+					if (g.size() == 1 || SnomedUtils.hasAttributeInAnotherGroup(c, findMe, g.getGroupId())) {
+						changesMade += removeRelationshipGroup(t, c, g);
+						groupRemoved = true;
+						thisGroupRemoved = true;
 					}
 				}
-				Relationship attrib = relTemplate.createRelationship(c, g.getGroupId(), null);
-				changesMade += addRelationship(t, c, attrib);
+				
+				if (!thisGroupRemoved) {
+					Relationship attrib = relTemplate.createRelationship(c, g.getGroupId(), null);
+					changesMade += addRelationship(t, c, attrib, Mode.UNIQUE_TYPE_IN_THIS_GROUP);
+				}
+			}
+			
+			//Self grouped would not be picked up in above due to lack of finding site
+			//We can also add in the pathological process here
+			for (RelationshipGroup g : c.getRelationshipGroups(CharacteristicType.STATED_RELATIONSHIP, ASSOC_MORPH)) {
+				if (g.size() == 1) {
+				if (g.containsTypeValue(workAroundToRemove)) {
+						changesMade +=  removeRelationshipGroup(t, c, g);
+						groupRemoved = true;
+					} else {
+						Relationship attrib = relTemplate.createRelationship(c, g.getGroupId(), null);
+						changesMade += addRelationship(t, c, attrib, Mode.UNIQUE_TYPE_IN_THIS_GROUP);
+					}
+				}
+			}
+			
+			if (groupRemoved) {
+				shuffleDown(t, c);
+			}
+			
+			//Now swap out all our attribute values that we're replacing - if they're still here
+			for (Concept targetTarget : replaceValuesMap.keySet()) {
+				//Switch all stated relationships from the findValue to the replaceValue
+				for (Relationship r : c.getRelationships(CharacteristicType.STATED_RELATIONSHIP, ActiveState.ACTIVE)) {
+					if (r.getTarget().equals(targetTarget)) {
+						Relationship replacement = r.clone();
+						replacement.setTarget(replaceValuesMap.get(targetTarget));
+						changesMade += replaceRelationship(t, c, r, replacement);
+					}
+				}
 			}
 			return changesMade;
 		}
