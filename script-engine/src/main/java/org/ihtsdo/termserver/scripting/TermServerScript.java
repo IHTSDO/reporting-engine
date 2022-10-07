25258c2669fc58f9b8fc04359ff03db47f502fdc
pgwilliams <petergwilliams@hotmail.com>
2022-10-07 11:12:54 +0100

    QI-1165 Add common support for improved concept cloning for creating new concepts
    Also support for overriding the branch that ECL is run against

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/TermServerScript.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/TermServerScript.java
@@ -1142,7 +1142,6 @@ public abstract class TermServerScript extends Script implements ScriptConstants
 	public Collection<Concept> findConceptsWithoutEffectiveTime(String ecl) throws TermServerScriptException {
 		Collection<Concept> concepts = findConcepts(ecl);
 		concepts.removeIf(Concept::hasEffectiveTime);
-
 		return concepts;
 	}
 	
