a3104230df39a5e7125ef2b18bc48d61095a0100
pgwilliams <petergwilliams@hotmail.com>
2022-11-21 14:16:47 +0000

    RP-674 Add ECL filtering and 'All Without' to Term contains X Report

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/TermContainsXReport.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/TermContainsXReport.java
@@ -23,7 +23,7 @@ import org.snomed.otf.script.dao.ReportSheetManager;
  */
 public class TermContainsXReport extends TermServerReport implements ReportClass {
 	
-	String[] textsToMatch;
+	String[] textsToMatch = null;
 	String[] textsToAvoid = null;
 	boolean reportConceptOnceOnly = true;
 	public static final String STARTS_WITH = "Starts With";
@@ -32,23 +32,33 @@ public class TermContainsXReport extends TermServerReport implements ReportClass
 	public static final String ATTRIBUTE_TYPE = "Attribute Type";
 	public static final String WITHOUT = "Without";
 	public static final String TERM_TYPES = "Term Type";
+	public static final String WITHOUT_MODE = "Without Mode";
+	public static final String ALL_WITHOUT = "All terms 'without'";
+	public static final String ANY_WITHOUT = "Any terms 'without'";
 	Concept attributeDetail;
 	boolean startsWith = false;
 	boolean wholeWord = false;
+	private enum WithoutMode {ALL_WITHOUT, ANY_WITHOUT};
+	private WithoutMode withoutMode;
 	
 	private List<String> targetTypes;
 	
 	public static void main(String[] args) throws TermServerScriptException, IOException {
 		Map<String, Object> params = new HashMap<>();
 		params.put(STARTS_WITH, "N");
-		params.put(SUB_HIERARCHY, ROOT_CONCEPT.toString());
-		params.put(WORDS, "angiography, angiogram, arteriography, arteriogram");
-		params.put(WITHOUT, "fluoroscopic, fluoroscopy, computed tomography, CT, magnetic resonance, MR, MRA, MRI");
+		//params.put(ECL, "<< 372087000 |Primary malignant neoplasm (disorder)| ");
+		params.put(ECL, "<< 363346000 |Malignant neoplastic disease (disorder)| MINUS (<< 372087000 |Primary malignant neoplasm (disorder)| OR << 269475001 |Malignant tumor of lymphoid, hemopoietic AND/OR related tissue (disorder)|)"); 
+		//params.put(WORDS, "angiography, angiogram, arteriography, arteriogram");
+		//params.put(WITHOUT, "fluoroscopic, fluoroscopy, computed tomography, CT, magnetic resonance, MR, MRA, MRI");
+		//params.put(WITHOUT, "primary");
+		params.put(WORDS, "primary");
+		params.put(WITHOUT_MODE, ANY_WITHOUT);
 		params.put(WHOLE_WORD, "false");
 		params.put(ATTRIBUTE_TYPE, null);
 		List<String> descTypes = new ArrayList<>();
 		descTypes.add("FSN");
-		descTypes.add("DEFN");
+		descTypes.add("PT");
+		descTypes.add("SYN");
 		params.put(TERM_TYPES, descTypes);
 		TermServerReport.run(TermContainsXReport.class, params, args);
 	}
@@ -59,15 +69,29 @@ public class TermContainsXReport extends TermServerReport implements ReportClass
 		runStandAlone = false; //We need a proper path lookup for MS projects
 		super.init(run);
 		getArchiveManager().setPopulateHierarchyDepth(true);
-		textsToMatch = run.getMandatoryParamValue(WORDS).split(COMMA);
-		for (int i=0; i < textsToMatch.length; i++) {
-			textsToMatch[i] = textsToMatch[i].toLowerCase().trim();
+		
+		if (!StringUtils.isEmpty(run.getParamValue(WORDS))) {
+			textsToMatch = run.getParamValue(WORDS).split(COMMA);
+			for (int i=0; i < textsToMatch.length; i++) {
+				textsToMatch[i] = textsToMatch[i].toLowerCase().trim();
+			}
 		}
 		
 		if (!StringUtils.isEmpty(run.getParamValue(WITHOUT))) {
-			textsToAvoid = run.getMandatoryParamValue(WITHOUT).toLowerCase().split(COMMA);
+			textsToAvoid = run.getParamValue(WITHOUT).toLowerCase().split(COMMA);
 			for (int i=0; i < textsToAvoid.length; i++) {
 				textsToAvoid[i] = textsToAvoid[i].toLowerCase().trim();
+			}
+		}
+		
+		if (!StringUtils.isEmpty(run.getParamValue(WITHOUT_MODE))) {
+			String withoutModeStr = run.getParamValue(WITHOUT_MODE);
+			switch (withoutModeStr) {
+				case ALL_WITHOUT : withoutMode = WithoutMode.ALL_WITHOUT;
+						break;
+				case ANY_WITHOUT : withoutMode = WithoutMode.ANY_WITHOUT;
+						break;
+				default: throw new IllegalArgumentException("Did not recognise 'Without Mode': " + withoutModeStr);
 			}
 		}
 		
@@ -84,11 +108,12 @@ public class TermContainsXReport extends TermServerReport implements ReportClass
 	@Override
 	public Job getJob() {
 		JobParameters params = new JobParameters()
-				.add(SUB_HIERARCHY).withType(JobParameter.Type.CONCEPT).withDefaultValue(ROOT_CONCEPT)
+				.add(ECL).withType(JobParameter.Type.ECL)
 				.add(STARTS_WITH).withType(JobParameter.Type.BOOLEAN).withMandatory().withDefaultValue(false)
 				.add(WHOLE_WORD).withType(JobParameter.Type.BOOLEAN).withMandatory().withDefaultValue(false)
-				.add(WORDS).withType(JobParameter.Type.STRING).withMandatory().withDescription("Use a comma to separate multiple words in an 'or' search")
+				.add(WORDS).withType(JobParameter.Type.STRING).withDescription("Use a comma to separate multiple words in an 'or' search")
 				.add(WITHOUT).withType(JobParameter.Type.STRING).withDescription("Use a comma to separate multiple words in an 'or' search")
+				.add(WITHOUT_MODE).withType(JobParameter.Type.DROPDOWN).withValues(ALL_WITHOUT, ANY_WITHOUT).withDefaultValue(ALL_WITHOUT)
 				.add(TERM_TYPES).withType(JobParameter.Type.CHECKBOXES).withOptions("FSN", "PT", "SYN", "DEFN").withDefaultValues("FSN","PT")
 				.add(ATTRIBUTE_TYPE).withType(JobParameter.Type.CONCEPT).withDescription("Optional. Will show the attribute values per concept for the specified attribute type.  For example in Substances, show me all concepts that are used as a target for 738774007 |Is modification of (attribute)| by specifying that attribute type in this field.")
 				.build();
@@ -96,7 +121,7 @@ public class TermContainsXReport extends TermServerReport implements ReportClass
 		return new Job()
 				.withCategory(new JobCategory(JobType.REPORT, JobCategory.ADHOC_QUERIES))
 				.withName("Term contains X")
-				.withDescription("This report lists all concepts containing the specified words, with optional attribute details.  Search for multiple words (in an either/or fashion) using a comma to separate.  'Without' words must not be present, again comma separated")
+				.withDescription("This report lists all concepts containing the specified words, with optional attribute details.  Search for multiple words (in an either/or fashion) using a comma to separate.  'Without' words must not be present, again comma separated.  Note that PT and SYN are considered distinct, so SYN - in this context - means synonyms other than the Preferred Term.")
 				.withProductionStatus(ProductionStatus.PROD_READY)
 				.withParameters(params)
 				.withTag(INT)
@@ -104,13 +129,18 @@ public class TermContainsXReport extends TermServerReport implements ReportClass
 	}
 	
 	public void runJob() throws TermServerScriptException {
-		Collection<Concept> conceptsOfInterest = gl.getAllConcepts();
-		if (subHierarchy != null) {
-			conceptsOfInterest = subHierarchy.getDescendents(NOT_SET);
+		List<Concept> conceptsOfInterest = new ArrayList<>(gl.getAllConcepts());
+		if (subsetECL != null && !subsetECL.isEmpty()) {
+			conceptsOfInterest = new ArrayList<>(findConcepts(subsetECL));
 		}
+		conceptsOfInterest.sort(Comparator.comparing(Concept::getFsnSafely));
 		
 		nextConcept:
 		for (Concept c : conceptsOfInterest) {
+			
+			if (c.getId().equals("307651005")) {
+				debug("Here");
+			}
 			if (c.isActive()) {
 				if (whiteListedConceptIds.contains(c.getId())) {
 					incrementSummaryInformation(WHITE_LISTED_COUNT);
@@ -129,36 +159,60 @@ public class TermContainsXReport extends TermServerReport implements ReportClass
 						term = " " + term + " ";
 						altTerm = term.replaceAll("[^A-Za-z0-9]", " ");
 					}
-					for (String matchText : textsToMatch) {
+					
+					if (textsToMatch!= null && textsToMatch.length > 0) {
+						for (String matchText : textsToMatch) {
+							if (textsToAvoid != null) {
+								for (String avoidText : textsToAvoid) {
+									if (term.contains(avoidText)) {
+										if (withoutMode == WithoutMode.ALL_WITHOUT) {
+											continue nextConcept;
+										} else {
+											continue nextDescription;
+										}
+									}
+								}
+							}
+							if (wholeWord) {
+								matchText = " " + matchText + " ";
+							}
+							if ( (!startsWith && (term.contains(matchText) || altTerm.contains(matchText))) ||
+									(startsWith && (term.startsWith(matchText) || altTerm.startsWith(matchText)))) {
+								report(c,matchText, d);
+								reported = true;
+								incrementSummaryInformation("Matched '" + matchText.trim() + "'");
+							}
+						}
+						if (reported && reportConceptOnceOnly) {
+							continue nextConcept;
+						}
+					} else {
+						// Are we just looking for terms that don't contain some string
 						if (textsToAvoid != null) {
 							for (String avoidText : textsToAvoid) {
 								if (term.contains(avoidText)) {
-									continue nextDescription;
+									if (withoutMode == WithoutMode.ALL_WITHOUT) {
+										continue nextConcept;
+									} else {
+										continue nextDescription;
+									}
 								}
 							}
 						}
-						if (wholeWord) {
-							matchText = " " + matchText + " ";
-						}
-						if ( (!startsWith && (term.contains(matchText) || altTerm.contains(matchText))) ||
-								(startsWith && (term.startsWith(matchText) || altTerm.startsWith(matchText)))) {
-							String[] hiearchies = getHierarchies(c);
-							String cs = SnomedUtils.translateCaseSignificanceFromEnum(c.getFSNDescription().getCaseSignificance());
-							String ds = SnomedUtils.translateDefnStatus(c.getDefinitionStatus());
-							report(c, ds, matchText, d, cs, getAttributeDetail(c), hiearchies[1], hiearchies[2]);
+						
+						if (getJobRun().getParamValue(WITHOUT) != null && withoutMode == WithoutMode.ANY_WITHOUT) {
+							report(c, "Did not contain: " + getJobRun().getParamValue(WITHOUT), d);
 							reported = true;
-							incrementSummaryInformation("Matched '" + matchText.trim() + "'");
-							countIssue(c);
 						}
 					}
-					if (reported && reportConceptOnceOnly) {
-						continue nextConcept;
-					}
+				}
+				if (getJobRun().getParamValue(WITHOUT) != null && withoutMode == WithoutMode.ALL_WITHOUT) {
+					report(c, "No terms contained: " + getJobRun().getParamValue(WITHOUT));
 				}
 			}
 		}
 	
-		if (Arrays.asList(textsToMatch).contains("TEST_WATCHER")) {
+		if (textsToMatch != null && Arrays.asList(textsToMatch).contains("TEST_WATCHER")) {
 			info("Testing watcher functionality.  Pausing for 15 minutes");
 			try {
 				Thread.sleep(15 * 60 * 1000);
@@ -166,7 +220,19 @@ public class TermContainsXReport extends TermServerReport implements ReportClass
 				info("Watcher Testing interrupted prematurely");
 			}
 		}
+		
+		if (textsToMatch != null && Arrays.asList(textsToMatch).contains("KILL_REPORT")) {
+			throw new TermServerScriptException("'KILL_REPORT' instruction received.");
+		}
 	
+	}
+	
+	protected boolean report (Concept c, Object...details) throws TermServerScriptException {
+		String[] hiearchies = getHierarchies(c);
+		String cs = SnomedUtils.translateCaseSignificanceFromEnum(c.getFSNDescription().getCaseSignificance());
+		String ds = SnomedUtils.translateDefnStatus(c.getDefinitionStatus());
+		countIssue(c);
+		return super.report(c, ds, details, cs, getAttributeDetail(c), hiearchies[1], hiearchies[2]);
 	}
 	
 	private boolean isTargetDescriptionType(Description d) {
