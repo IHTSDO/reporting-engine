c0ccd86e12796ba3fbe800af3049a6e06e9b4015
pgwilliams <petergwilliams@hotmail.com>
2022-11-23 14:21:33 +0000

    QI-1176 Add generalised support for recovering calculated PPPs when setting PPP fails, for further processing

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/template/NormaliseTemplateCompliantConcepts.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/template/NormaliseTemplateCompliantConcepts.java
@@ -491,7 +491,7 @@ public class NormaliseTemplateCompliantConcepts extends TemplateFix {
 		//If the proximal primitive parent matches that of the matching template, we can set that
 		List<String> focusConceptIds = conceptToTemplateMap.get(c).getLogicalTemplate().getFocusConcepts();
 		if (focusConceptIds.size() == 1) {
-			changesMade += checkAndSetProximalPrimitiveParent(t, c, gl.getConcept(focusConceptIds.get(0)), false, allowCompromisePPP);
+			changesMade += checkAndSetProximalPrimitiveParent(t, c, gl.getConcept(focusConceptIds.get(0)), false, allowCompromisePPP, null);
 		} else {
 			report (t, c, Severity.CRITICAL, ReportActionType.VALIDATION_ERROR, "Cannot remodel PPP - template specifies multiple focus concepts");
 		}
