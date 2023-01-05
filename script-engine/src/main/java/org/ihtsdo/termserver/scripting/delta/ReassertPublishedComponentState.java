849625d733b3552f9a7305f979613cdea8ef4c0c
Peter Groves Williams <petergwilliams@hotmail.com>
2023-01-05 17:42:59 +0000

    ReassertPublishedComponentState can also be used for INT.  Moving out of MS

---
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/ReassertPublishedComponentState.java
@@ -0,0 +1,101 @@
+package org.ihtsdo.termserver.scripting.delta;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.ihtsdo.otf.exception.TermServerScriptException;
+import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
+import org.ihtsdo.termserver.scripting.domain.*;
+import org.ihtsdo.termserver.scripting.util.SnomedUtils;
+import org.snomed.otf.scheduler.domain.*;
+import org.snomed.otf.script.dao.ReportSheetManager;
+
+/**
+ * MSSP-1661 Need to reassert various components back to their previously published state,
+ * but restricted to those exact components
+ */
+public class ReassertPublishedComponentState extends DeltaGenerator {
+	
+	String[] componentsToProcess = new String[] {
+		"54516021",
+		"529188028",
+		"529190027",
+		"529191028",
+		"529192024",
+		"1656929029",
+		"1730286025",
+		"1730287023",
+		"2207364021",
+		"2219896024",
+		"2448852021",
+		"2519488028",
+		"2655557022",
+		"2655558028",
+		"2779333027",
+		"2854001027",
+		"3421766020",
+		"3468948025",
+		"6995423026",
+		"6995424021",
+		"6995425022",
+		"9299711024",
+		"12142143025"
+	};
+	
+	public static void main(String[] args) throws TermServerScriptException, IOException, InterruptedException {
+		ReassertPublishedComponentState delta = new ReassertPublishedComponentState();
+		try {
+			delta.getArchiveManager().setPopulateReleasedFlag(true);
+			delta.runStandAlone = false;
+			delta.inputFileHasHeaderRow = true;
+			delta.newIdsRequired = false; // We'll only be inactivating existing relationships
+			delta.init(args);
+			delta.loadProjectSnapshot(false);
+			delta.postInit();
+			delta.process();
+			delta.getRF2Manager().flushFiles(true);  //Flush and Close
+			if (!dryRun) {
+				SnomedUtils.createArchive(new File(delta.outputDirName));
+			}
+		} finally {
+			delta.finish();
+			if (delta.descIdGenerator != null) {
+				info(delta.descIdGenerator.finish());
+			}
+		}
+	}
+	
+	public void init (JobRun run) throws TermServerScriptException {
+		getArchiveManager().setReleasedFlagPopulated(true);
+		ReportSheetManager.targetFolderId = "1mvrO8P3n94YmNqlWZkPJirmFKaFUnE0o"; //Managed Service
+		subsetECL = run.getParamValue(ECL);
+		super.init(run);
+	}
+	
+	public void postInit() throws TermServerScriptException {
+		String[] columnHeadings = new String[] {
+				"Id, FSN, SemTag, Component Reasserted"};
+		String[] tabNames = new String[] {	
+				"Reassertions"};
+		
+		super.postInit(tabNames, columnHeadings, false);
+	}
+	
+	public void process() throws TermServerScriptException {
+		//Set<String> componentsToProcess = new HashSet<>(Arrays.asList(processMe.split(",")));
+		for (String componentId : componentsToProcess) {
+			Component component = gl.getComponent(componentId);
+			if (component == null) {
+				report((Concept)null, componentId, "Not found in " + project.getKey());
+			} else {
+				Concept owningConcept = gl.getComponentOwner(component.getId());
+				report (owningConcept, component);
+				component.setDirty();
+				outputRF2(owningConcept, true);  //Will only output dirty fields.
+				component.setClean();  //Revert this in case we output other components for this concept.
+				//Don't want to output this one multiple times.
+			}
+		}
+	}
+
+}
