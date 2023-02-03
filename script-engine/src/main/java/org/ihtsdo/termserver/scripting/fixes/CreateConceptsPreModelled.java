e82fbe68abc14456d1fc2e54c43f0461adecd49d
pgwilliams <petergwilliams@hotmail.com>
2023-02-03 16:40:48 +0000

    LE-3 recover saved concept SCTID and populate into alternate identifier file (sheet)

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/CreateConceptsPreModelled.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/CreateConceptsPreModelled.java
@@ -1,6 +1,8 @@
 package org.ihtsdo.termserver.scripting.fixes;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Task;
@@ -8,6 +10,7 @@ import org.ihtsdo.otf.exception.TermServerScriptException;
 import org.ihtsdo.termserver.scripting.ValidationFailure;
 import org.ihtsdo.termserver.scripting.domain.*;
 import org.ihtsdo.termserver.scripting.fixes.BatchFix;
+import org.ihtsdo.termserver.scripting.reports.loinc.ConceptWrapper;
 import org.ihtsdo.termserver.scripting.reports.loinc.LoincUtils;
 import org.snomed.otf.script.dao.ReportManager;
 
@@ -18,27 +21,32 @@ import org.snomed.otf.script.dao.ReportManager;
 public class CreateConceptsPreModelled extends BatchFix implements ScriptConstants{
 	
 	private List<Component> conceptsToCreate;
+	private Map<Concept, ConceptWrapper> conceptWrapperMap;  //Needed so we can find the original homes of our concepts once they're created
 	private int tabIdx;
 	
-	public CreateConceptsPreModelled(ReportManager rm, int tabIdx, Set<Concept> concepts) {
+	public CreateConceptsPreModelled(ReportManager rm, int tabIdx, Set<? extends ConceptWrapper> conceptsWrapped) {
 		super(null);
 		this.setReportManager(rm);
 		this.tabIdx = tabIdx;
-		this.conceptsToCreate = asComponents(concepts);
+		conceptWrapperMap = conceptsWrapped.stream()
+				.collect(Collectors.toMap(ConceptWrapper::getConcept, Function.identity()));
+		this.conceptsToCreate = asComponents(conceptWrapperMap.keySet());
 		this.selfDetermining = true;
 	}
 
 	@Override
 	protected int doFix(Task t, Concept c, String info) throws TermServerScriptException, ValidationFailure {
-		String loincNum = LoincUtils.getLoincNumFromDescription(c);
+		//What was this concept original wrapped in?
+		ConceptWrapper wrapping = conceptWrapperMap.get(c);
 		try {
 			String scg = c.toExpression(CharacteristicType.STATED_RELATIONSHIP);
 			//Remove any temporary identifiers before creating
 			c.setId(null);
 			Concept createdConcept = createConcept(t, c, info, false);
-			report(tabIdx, t, createdConcept, Severity.NONE, ReportActionType.CONCEPT_ADDED, loincNum, scg);
+			wrapping.setConcept(createdConcept);  //Save it so our calling function can retain this
+			report(tabIdx, t, createdConcept, Severity.NONE, ReportActionType.CONCEPT_ADDED, wrapping.getWrappedId(), scg);
 		} catch (Exception e) {
-			report(tabIdx, t, c, Severity.CRITICAL, ReportActionType.API_ERROR, loincNum, e.getMessage());
+			report(tabIdx, t, c, Severity.CRITICAL, ReportActionType.API_ERROR, wrapping.getWrappedId(), e.getMessage());
 		}
 		return CHANGE_MADE;
 	}
