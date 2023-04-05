7ac990ab1f74ce772fbb5980c263cbace680d465
Peter Groves Williams <petergwilliams@hotmail.com>
2023-04-05 15:50:47 +0100

    INFRA-10708 Add additional tabs for various layouts of acronym usage

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/reports/oneOffs/ListAcronymDescriptions.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/reports/oneOffs/ListAcronymDescriptions.java
@@ -30,9 +30,15 @@ public class ListAcronymDescriptions extends TermServerReport implements ReportC
 	
 	public void postInit() throws TermServerScriptException {
 		String[] columnHeadings = new String[] { "SCTID, FSN, SemTag, Acryonym",
-				"SCTID, FSN, SemTag, Acronym",};
+				"SCTID, FSN, SemTag, Acronym",
+				"SCTID, FSN, SemTag, Acronym",
+				"SCTID, FSN, SemTag, Acronym",
+				"SCTID, FSN, SemTag, Acronym"};
 		String[] tabNames = new String[] {	"Acronyms without Expansion",
-				"Acryonyms with Capitalised Expansion"};
+				"Acryonyms with Capitalised Expansion",
+				"Acryonyms used within term",
+				"Acryonyms oddly expanded",
+				"Acryonyms correctly expanded"};
 		super.postInit(tabNames, columnHeadings, false);
 	}
 	
@@ -54,6 +60,8 @@ public class ListAcronymDescriptions extends TermServerReport implements ReportC
 
 	public void runJob() throws TermServerScriptException {
 		initialiseSummaryInformation(ISSUE_COUNT);
+		//Set<Description> alreadyReported = new HashSet<>();
+		//Concept c = gl.getConcept("716601009"); {
 		for (Concept c : SnomedUtils.sort(gl.getAllConcepts())) {
 			if (!c.isActive()) {
 				continue;
@@ -76,7 +84,62 @@ public class ListAcronymDescriptions extends TermServerReport implements ReportC
 				report(SECONDARY_REPORT, c, acronymsStr);
 				countIssue(c);
 			}
+			
+			acronymsStr = c.getDescriptions(ActiveState.ACTIVE).stream()
+					.map(d -> d.getTerm())
+					.filter(t -> containsAcronym(t))
+					.collect(Collectors.joining(",\n"));
+			if (!StringUtils.isEmpty(acronymsStr)) {
+				report(TERTIARY_REPORT, c, acronymsStr);
+				countIssue(c);
+			}
+			
+			acronymsStr = c.getDescriptions(ActiveState.ACTIVE).stream()
+					.map(d -> d.getTerm())
+					.filter(t -> !isAcronym(t))
+					.filter(t -> t.contains(" - "))
+					.filter(t -> startsWithAcronym(t))
+					.filter(t -> !hasCapitalisedExpansion(t))
+					.collect(Collectors.joining(",\n"));
+			if (!StringUtils.isEmpty(acronymsStr)) {
+				report(QUATERNARY_REPORT, c, acronymsStr);
+				countIssue(c);
+			}
+			
+			acronymsStr = c.getDescriptions(ActiveState.ACTIVE).stream()
+					.map(d -> d.getTerm())
+					.filter(t -> t.contains(" - "))
+					.filter(t -> startsWithAcronym(t))
+					.filter(t -> !hasCapitalisedExpansion(t))
+					.collect(Collectors.joining(",\n"));
+			if (!StringUtils.isEmpty(acronymsStr)) {
+				report(QUINARY_REPORT, c, acronymsStr);
+				countIssue(c);
+			}
 		}
+	}
+
+	private boolean startsWithAcronym(String term) {
+		boolean lastCharacterWasCapital = false;
+		boolean twoOrMoreConsecutiveCapitalsEncountered = false;
+		int idxLastChar = term.length() - 1;
+		for (int i=0; i<term.length(); i++) {
+			if (Character.isUpperCase(term.charAt(i))) {
+				if (lastCharacterWasCapital) {
+					twoOrMoreConsecutiveCapitalsEncountered = true;
+				} else {
+					lastCharacterWasCapital = true;
+				}
+			} else if (Character.isAlphabetic(term.charAt(i)) && Character.isLowerCase(term.charAt(i))) {
+				return false;
+			}
+			
+			if (twoOrMoreConsecutiveCapitalsEncountered 
+					&& (i == idxLastChar || term.charAt(i) == ' ')) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private boolean hasCapitalisedExpansion(String term) {
@@ -93,6 +156,49 @@ public class ListAcronymDescriptions extends TermServerReport implements ReportC
 
 	private boolean isAcronym(String term) {
 		return term.equals(term.toUpperCase()) && allLetters(term);
+	}
+	
+	private boolean containsAcronym(String term) {
+		if (isAcronym(term)) {
+			return false;
+		}
+		boolean lastCharacterWasCapital = false;
+		boolean twoOrMoreConsecutiveCapitalsEncountered = false;
+		int idxLastChar = term.length() - 1;
+		
+		//Skip as many capital letters as we have
+		int i = 0;
+		while (Character.isUpperCase(term.charAt(i)) && i <= idxLastChar) {
+			i++;
+		}
+
+		for (; i<term.length(); i++) {
+			char c = term.charAt(i);
+			if (Character.isUpperCase(c)) {
+				if (lastCharacterWasCapital) {
+					twoOrMoreConsecutiveCapitalsEncountered = true;
+				} else {
+					lastCharacterWasCapital = true;
+				}
+			} else {
+				if (c == ' ') {
+					lastCharacterWasCapital = false;
+					continue;
+				} else if (!Character.isAlphabetic(c)) {
+					//We'll just skip these without halting our consideration of what is an acronym
+					continue;
+				} else if (Character.isLowerCase(c)) {
+					lastCharacterWasCapital = false;
+					continue;
+				}
+			}
+			
+			if (twoOrMoreConsecutiveCapitalsEncountered 
+					&& (i == idxLastChar || term.charAt(i) == ' ')) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private boolean allLetters(String term) {
