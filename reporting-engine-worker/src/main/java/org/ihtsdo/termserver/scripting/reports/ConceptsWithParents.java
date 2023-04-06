6441f30aa0637d5859c04777b555e047d4b5ef2a
Jim <jim@cornmell.com>
2023-04-06 12:48:38 +0100

    RP-537 updated report for terms and sorting also added Spock tests ---------
    Co-authored-by: Jim Cornmell <jco@snomed.org>

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/ConceptsWithParents.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/ConceptsWithParents.java
@@ -2,6 +2,7 @@ package org.ihtsdo.termserver.scripting.reports;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.ihtsdo.otf.exception.TermServerScriptException;
@@ -13,64 +14,121 @@ import org.snomed.otf.scheduler.domain.Job.ProductionStatus;
 import org.snomed.otf.script.dao.ReportSheetManager;
 
 public class ConceptsWithParents extends TermServerReport implements ReportClass {
-	
-	Map<String, Map<String, Concept>> semanticTagHierarchy = new HashMap<>();
-	
-	public static void main(String[] args) throws TermServerScriptException, IOException {
-		TermServerReport.run(ConceptsWithParents.class, args);
-	}
-	
-	public void init (JobRun run) throws TermServerScriptException {
-		ReportSheetManager.targetFolderId = "1F-KrAwXrXbKj5r-HBLM0qI5hTzv-JgnU"; //Ad-hoc Reports
-		additionalReportColumns="FSN, SemTag, DEF_STATUS, Immediate Stated Parent, Immediate Inferred Parents ,Inferred Grand Parents";
-		super.init(run);
-	}
+    private static final String TERMS_FILTER = "Filter for terms";
+    private static final String EXAMPLE_MAIN_PARAM_ECL = "<< 1144725004";
+    private static final String EXAMPLE_MAIN_PARAM_TERMS_FILTER = "capsule";
+    private static final Pattern CONTAINS_TERM_PATTERN = Pattern.compile("\\{\\{\\s*term", Pattern.MULTILINE);
+    public static final String COMMA_NEWLINE_DELIMITER = ",\n";
 
-	@Override
-	public Job getJob() {
-		JobParameters params = new JobParameters()
-				.add(HIERARCHIES).withType(JobParameter.Type.CONCEPT_LIST).withMandatory().build();
+    public static void main(String[] args) throws TermServerScriptException, IOException {
+        Map<String, String> params = new HashMap<>();
+        params.put(ECL, EXAMPLE_MAIN_PARAM_ECL);
+        params.put(TERMS_FILTER, EXAMPLE_MAIN_PARAM_TERMS_FILTER);
+        TermServerReport.run(ConceptsWithParents.class, args, params);
+    }
 
-		return new Job()
-				.withCategory(new JobCategory(JobType.REPORT, JobCategory.ADHOC_QUERIES))
-				.withName("Concepts with Parents")
-				.withDescription("This report lists all parents and grandparents of concepts in the specified hierarchies. " +
-						"Note that since this report is not listing any problems, the 'Issues' count will always be 0.")
-				.withProductionStatus(ProductionStatus.PROD_READY)
-				.withParameters(params)
-				.withTag(INT)
-				.build();
-	}
+    public void init(JobRun run) throws TermServerScriptException {
+        ReportSheetManager.targetFolderId = "1F-KrAwXrXbKj5r-HBLM0qI5hTzv-JgnU"; //Ad-hoc Reports
+        additionalReportColumns = "FSN, SemTag, DEF_STATUS, Immediate Stated Parent, Immediate Inferred Parents ,Inferred Grand Parents";
+        super.init(run);
+    }
 
-	public void runJob() throws TermServerScriptException {
+    @Override
+    public Job getJob() {
+        JobParameters params = new JobParameters()
+                .add(ECL).withDescription("foo").withType(JobParameter.Type.ECL).withMandatory().withDescription("Specify")
+                .add(TERMS_FILTER).withDescription("Optional.  Use a comma to separate multiple terms.  This will be ignored if the ECL contains a term filter.").withType(JobParameter.Type.STRING)
+                .build();
 
-		Set<Concept> conceptsOfInterest = new HashSet<>();
-		for (String hierarchyStr : jobRun.getMandatoryParamValue(HIERARCHIES).split(COMMA)) {
-			Concept hierarchy = gl.getConcept(hierarchyStr);
-			conceptsOfInterest.addAll(gl.getDescendantsCache().getDescendentsOrSelf(hierarchy));
-		}
-		
-		for (Concept c : conceptsOfInterest) {
-			if (whiteListedConceptIds.contains(c.getId())) {
-				incrementSummaryInformation(WHITE_LISTED_COUNT);
-				continue;
-			}
-			Set<Concept> statedParents = c.getParents(CharacteristicType.STATED_RELATIONSHIP);
-			String statedParentsStr = statedParents.stream().map(p->p.toString())
-					.collect(Collectors.joining(",\n"));
-			Set<Concept> parents = c.getParents(CharacteristicType.INFERRED_RELATIONSHIP);
-			String parentsStr = parents.stream().map(p->p.toString())
-					.collect(Collectors.joining(",\n"));
-			String parentsParentsStr = parents.stream()
-					.flatMap(p->p.getParents(CharacteristicType.INFERRED_RELATIONSHIP).stream())
-					.map(pp->pp.toString())
-					.collect(Collectors.joining(",\n"));
-			String defn = SnomedUtils.translateDefnStatus(c.getDefinitionStatus());
-			report (c, defn, statedParentsStr, parentsStr, parentsParentsStr);
-			incrementSummaryInformation("Concepts reported");
-		}
-	}
+        return new Job()
+                .withCategory(new JobCategory(JobType.REPORT, JobCategory.ADHOC_QUERIES))
+                .withName("Concepts with Parents")
+                .withDescription(
+                        "This report lists all parents and grandparents of concepts in the specified hierarchies. " +
+                                "Note that since this report is not listing any problems, the 'Issues' count will always be 0.")
+                .withProductionStatus(ProductionStatus.PROD_READY)
+                .withParameters(params)
+                .withTag(INT)
+                .build();
+    }
 
+    public void runJob() throws TermServerScriptException {
+        String eclQuery = appendTermsFilter(jobRun.getMandatoryParamValue(ECL), jobRun.getParamValue(TERMS_FILTER));
+        Collection<Concept> conceptsOfInterest = findConcepts(eclQuery);
+        List<Concept> sortedListOfConceptsOfInterest = SnomedUtils.sort(conceptsOfInterest);
 
+        for (Concept concept : sortedListOfConceptsOfInterest) {
+            if (whiteListedConceptIds.contains(concept.getId())) {
+                incrementSummaryInformation(WHITE_LISTED_COUNT);
+                continue;
+            }
 
+            Set<Concept> statedParents = concept.getParents(CharacteristicType.STATED_RELATIONSHIP);
+            String statedParentsStr = statedParents.stream()
+                    .map(Concept::toString)
+                    .distinct()
+                    .sorted()
+                    .collect(Collectors.joining(COMMA_NEWLINE_DELIMITER));
+            Set<Concept> inferredParents = concept.getParents(CharacteristicType.INFERRED_RELATIONSHIP);
+            String inferredParentsStr = inferredParents.stream()
+                    .map(Concept::toString)
+                    .distinct()
+                    .sorted()
+                    .collect(Collectors.joining(COMMA_NEWLINE_DELIMITER));
+            String grandParentsStr = inferredParents.stream()
+                    .flatMap(parent -> parent.getParents(CharacteristicType.INFERRED_RELATIONSHIP).stream())
+                    .map(Concept::toString)
+                    .distinct()
+                    .sorted()
+                    .collect(Collectors.joining(COMMA_NEWLINE_DELIMITER));
+            String definition = SnomedUtils.translateDefnStatus(concept.getDefinitionStatus());
+
+            report(concept, definition, statedParentsStr, inferredParentsStr, grandParentsStr);
+            countIssue(concept);
+            incrementSummaryInformation("Concepts reported");
+        }
+    }
+
+    /**
+     * Append a terms filter to the ECL query. Although if the ECL already contains a term filter, it will be ignored.
+     *
+     * @param ecl ECL to add terms to.
+     * @param terms String containing comma separated terms.
+     * @return new ECL with terms filter.
+     */
+    private String appendTermsFilter(String ecl, String terms) {
+        StringBuilder resultEcl = new StringBuilder();
+
+        if (ecl == null || ecl.isEmpty()) {
+            return "";
+        }
+
+        resultEcl.append(ecl.trim());
+
+        if (terms == null || terms.isEmpty() || (ecl != null && CONTAINS_TERM_PATTERN.matcher(ecl).find())) {
+            return resultEcl.toString().replaceAll("\\s+", " ");
+        }
+
+        String[] termList = terms.split(",");
+
+        if (termList.length == 1) {
+            resultEcl.append(" {{term = \"").append(termList[0]).append("\"}}");
+            return resultEcl.toString().replaceAll("\\s+", " ");
+        }
+
+        resultEcl.append(" {{term = (");
+        boolean addSpace = false;
+
+        for (String term : termList) {
+            if (addSpace) {
+                resultEcl.append(' ');
+            }
+
+            addSpace = true;
+            resultEcl.append('"').append(term.trim()).append('"');
+        }
+
+        resultEcl.append(")}}");
+        return resultEcl.toString().replaceAll("\\s+", " ");
+    }
 }
---
+++ reporting-engine-worker/src/test/groovy/org/ihtsdo/termserver/scripting/reports/ConceptsWithParentsTest.groovy
@@ -0,0 +1,137 @@
+package org.ihtsdo.termserver.scripting.reports
+
+import org.ihtsdo.otf.RF2Constants
+import org.ihtsdo.termserver.scripting.domain.Concept
+import org.snomed.otf.scheduler.domain.JobRun
+import spock.lang.Specification
+
+class ConceptsWithParentsTest extends Specification {
+    def "test append ecl '#ecl' and terms filter '#terms'"() {
+        expect:
+            (new ConceptsWithParents()).appendTermsFilter(ecl, terms) == excpectedEcl
+
+        where:
+            ecl                                                                   | terms            || excpectedEcl
+            null                                                                  | null             || ''
+            null                                                                  | 'heart,card'     || ''
+            ''                                                                    | 'heart,card'     || ''
+            ' <<  1144725004 '                                                    | 'capsule'        || '<< 1144725004 {{term = "capsule"}}'
+            ' <<  38975235 '                                                      | null             || '<< 38975235'
+            ' <<  38975235 '                                                      | ''               || '<< 38975235'
+            ' <<  38975235 '                                                      | ' heart , card ' || '<< 38975235 {{term = ("heart" "card")}}'
+            ' <<  38975235  {{term = ("heart" "card")}} '                         | ' foo , bar '    || '<< 38975235 {{term = ("heart" "card")}}'
+            ' <  56265001 |Heart disease|  {{ C definitionStatus = primitive }} ' | ' heart , card ' || '< 56265001 |Heart disease| {{ C definitionStatus = primitive }} {{term = ("heart" "card")}}'
+    }
+
+    def "run the job and test results"() {
+        given:
+            Concept c100 = new Concept('100', 'FSN: 100')
+            Concept c110 = new Concept('110', 'FSN: 110')
+            Concept c111 = new Concept('111', 'FSN: 111')
+            Concept c112 = new Concept('112', 'FSN: 112')
+            Concept c120 = new Concept('120', 'FSN: 120')
+            Concept c121 = new Concept('121', 'FSN: 121')
+            Concept c122 = new Concept('122', 'FSN: 122')
+            Concept c200 = new Concept('200', 'FSN: 200')
+            Concept c210 = new Concept('210', 'FSN: 210')
+            Concept c211 = new Concept('211', 'FSN: 211')
+            Concept c212 = new Concept('212', 'FSN: 212')
+            Concept c220 = new Concept('220', 'FSN: 220')
+            Concept c221 = new Concept('221', 'FSN: 221')
+            Concept c222 = new Concept('222', 'FSN: 222')
+            c100.semTag = "(body structure 100)"
+            c110.semTag = "(body structure 110)"
+            c111.semTag = "(body structure 111)"
+            c112.semTag = "(body structure 112)"
+            c120.semTag = "(body structure 120)"
+            c121.semTag = "(body structure 121)"
+            c122.semTag = "(body structure 122)"
+            c200.semTag = "(body structure 200)"
+            c210.semTag = "(body structure 210)"
+            c211.semTag = "(body structure 211)"
+            c212.semTag = "(body structure 212)"
+            c220.semTag = "(body structure 220)"
+            c221.semTag = "(body structure 221)"
+            c222.semTag = "(body structure 222)"
+
+            // Inferred grand-parents.
+            c110.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c112)
+            c120.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c121)
+            c120.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c122)
+            c110.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c111)
+            // Stated parents.
+            c100.addParent(RF2Constants.CharacteristicType.STATED_RELATIONSHIP, c120)
+            c100.addParent(RF2Constants.CharacteristicType.STATED_RELATIONSHIP, c110)
+            // Inferred parents.
+            c100.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c110)
+            c100.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c120)
+
+            // Inferred grand-parents.
+            // NOTE: Randomised order for c2XX also duplicated parents/grand-parents ....
+            c220.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c221)
+            c220.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c222)
+            c210.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c211)
+            c210.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c212)
+            c210.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c221) // Deliberate duplication.
+            // Stated parents.
+            c200.addParent(RF2Constants.CharacteristicType.STATED_RELATIONSHIP, c220)
+            c200.addParent(RF2Constants.CharacteristicType.STATED_RELATIONSHIP, c220) // Deliberate duplication.
+            c200.addParent(RF2Constants.CharacteristicType.STATED_RELATIONSHIP, c210)
+            // Inferred parents.
+            c200.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c210)
+            c200.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c220)
+            c200.addParent(RF2Constants.CharacteristicType.INFERRED_RELATIONSHIP, c210) // Deliberate duplication.
+
+            List<Concept> conceptsOfInterest = new ArrayList<Concept>()
+            conceptsOfInterest.add(c200)
+            conceptsOfInterest.add(c100)
+            JobRun jobRunMock = Mock()
+            jobRunMock.getMandatoryParamValue('ECL') >> '<< 100'
+            jobRunMock.getParamValue('Filter for terms') >> 'capsule'
+            def cwp = Spy(new ConceptsWithParents(jobRun: jobRunMock))
+            cwp.findConcepts(_) >> conceptsOfInterest
+            cwp.report(*_) >> null
+
+            int reportRow = 0
+
+        when:
+            cwp.runJob()
+
+        then:
+            cwp.summaryDetails["Concepts reported"] == 2
+            cwp.summaryDetails["Issue count"] == 2
+
+            2 * cwp.countIssue(_)
+            0 * cwp.incrementSummaryInformation('White Listed Count')
+            2 * cwp.incrementSummaryInformation('Concepts reported', 1)
+
+            2 * cwp.report(*_) >>
+                    {
+                        reportMethodArguments ->
+                            Concept val = reportMethodArguments[0]
+                            String[] rest = reportMethodArguments[1]
+
+                            if (reportRow == 0) {
+                                assert val.conceptId == "100"
+                                assert val.fsn == 'FSN: 100'
+                                assert val.semTag == '(body structure 100)'
+                                assert rest[0] == 'P'
+                                assert rest[1] == '110 |FSN: 110|,\n120 |FSN: 120|'
+                                assert rest[2] == '110 |FSN: 110|,\n120 |FSN: 120|'
+                                assert rest[3] == '111 |FSN: 111|,\n112 |FSN: 112|,\n121 |FSN: 121|,\n122 |FSN: 122|'
+                            } else {
+                                assert val.conceptId == "200"
+                                assert val.fsn == 'FSN: 200'
+                                assert val.semTag == '(body structure 200)'
+                                assert rest[0] == 'P'
+                                assert rest[1] == '210 |FSN: 210|,\n220 |FSN: 220|'
+                                assert rest[2] == '210 |FSN: 210|,\n220 |FSN: 220|'
+                                assert rest[3] == '211 |FSN: 211|,\n212 |FSN: 212|,\n221 |FSN: 221|,\n222 |FSN: 222|'
+                            }
+
+                            reportRow += 1
+                    }
+
+            reportRow == 2
+    }
+}
