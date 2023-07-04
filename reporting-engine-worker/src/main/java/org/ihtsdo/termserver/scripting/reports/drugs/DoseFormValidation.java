ef18535ece9d2026cfae98e61fee78c30bfbcb3c
pgwilliams <petergwilliams@hotmail.com>
2023-07-04 16:55:33 +0100

    RP-740 Split off drugs validation for Dose Form validation into new report

---
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/drugs/DoseFormValidation.java
@@ -0,0 +1,191 @@
+package org.ihtsdo.termserver.scripting.reports.drugs;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
+import org.ihtsdo.otf.exception.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.ReportClass;
+import org.ihtsdo.termserver.scripting.domain.*;
+import org.ihtsdo.termserver.scripting.reports.TermServerReport;
+import org.ihtsdo.termserver.scripting.util.DrugUtils;
+import org.ihtsdo.termserver.scripting.util.SnomedUtils;
+import org.snomed.otf.scheduler.domain.*;
+import org.snomed.otf.scheduler.domain.Job.ProductionStatus;
+import org.snomed.otf.script.dao.ReportSheetManager;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+
+public class DoseFormValidation extends TermServerReport implements ReportClass {
+	
+	private List<Concept> allDrugs;
+	private static String RECENT_CHANGES_ONLY = "Recent Changes Only";
+	
+	String[] semTagHiearchy = new String[] { "(product)", "(medicinal product)", "(medicinal product form)", "(clinical drug)" };
+	
+	private Concept[] doseFormTypes = new Concept[] {HAS_MANUFACTURED_DOSE_FORM};
+	private Map<Concept, Boolean> acceptableMpfDoseForms = new HashMap<>();
+	private Map<Concept, Boolean> acceptableCdDoseForms = new HashMap<>();	
+	private Map<String, Integer> issueSummaryMap = new HashMap<>();
+	
+	private boolean isRecentlyTouchedConceptsOnly = false;
+	private Set<Concept> recentlyTouchedConcepts;
+	
+	public static void main(String[] args) throws TermServerScriptException, IOException {
+		Map<String, String> params = new HashMap<>();
+		params.put(RECENT_CHANGES_ONLY, "true");
+		TermServerReport.run(DoseFormValidation.class, args, params);
+	}
+	
+	public void init (JobRun run) throws TermServerScriptException {
+		ReportSheetManager.targetFolderId = "1wtB15Soo-qdvb0GHZke9o_SjFSL_fxL3";  //DRUGS/Validation
+		additionalReportColumns = "FSN, SemTag, Issue, Data, Detail";  //DRUGS-267
+		super.init(run);
+	}
+	
+	public void postInit() throws TermServerScriptException {
+		String[] columnHeadings = new String[] { "SCTID, FSN, Semtag, Issue, Details, Details, Details, Further Details",
+				"Issue, Count"};
+		String[] tabNames = new String[] {	"Issues",
+				"Summary"};
+		allDrugs = SnomedUtils.sort(gl.getDescendantsCache().getDescendents(MEDICINAL_PRODUCT));
+		populateAcceptableDoseFormMaps();
+		
+		super.postInit(tabNames, columnHeadings, false);
+		
+		if (jobRun.getParamBoolean(RECENT_CHANGES_ONLY)) {
+			isRecentlyTouchedConceptsOnly = true;
+			recentlyTouchedConcepts = SnomedUtils.getRecentlyTouchedConcepts(gl.getAllConcepts());
+		}
+	}
+
+	@Override
+	public Job getJob() {
+		JobParameters params = new JobParameters()
+				.add(RECENT_CHANGES_ONLY)
+					.withType(JobParameter.Type.BOOLEAN)
+					.withDefaultValue(true)
+			.build();
+		return new Job()
+				.withCategory(new JobCategory(JobType.REPORT, JobCategory.DRUGS))
+				.withName("Dose Form Validation")
+				.withDescription("This report checks CD and MPF Dose forms against a curated file of acceptable dose forms.")
+				.withProductionStatus(ProductionStatus.PROD_READY)
+				.withTag(INT)
+				.withParameters(params)
+				.build();
+	}
+	
+	public void runJob() throws TermServerScriptException {
+		validateDrugsModeling();
+		populateSummaryTab();
+		info("Summary tab complete, all done.");
+	}
+
+	private void validateDrugsModeling() throws TermServerScriptException {
+		double conceptsConsidered = 0;
+		//for (Concept c : Collections.singleton(gl.getConcept("776935006"))) {
+		for (Concept c : allDrugs) {
+			if (isRecentlyTouchedConceptsOnly && !recentlyTouchedConcepts.contains(c)) {
+				continue;
+			}
+			
+			DrugUtils.setConceptType(c);
+			
+			double percComplete = (conceptsConsidered++/allDrugs.size())*100;
+			if (conceptsConsidered%4000==0) {
+				info("Percentage Complete " + (int)percComplete);
+			}
+			
+			//DRUGS-784
+			if (isCD(c) || isMPF(c)) {
+				validateAcceptableDoseForm(c);
+			}
+		}
+		info ("Drugs validation complete");
+	}
+	
+
+	private void validateAcceptableDoseForm(Concept c) throws TermServerScriptException {
+		String issueStr1 = c.getConceptType() + " uses unlisted dose form";
+		String issueStr2 = c.getConceptType() + " uses unacceptable dose form";
+		initialiseSummary(issueStr1);
+		initialiseSummary(issueStr2);
+		
+		Map<Concept, Boolean> acceptableDoseForms;
+		if (isMPF(c)) {
+			acceptableDoseForms = acceptableMpfDoseForms;
+		} else {
+			acceptableDoseForms = acceptableCdDoseForms;
+		}
+		
+		acceptableDoseForms.put(gl.getConcept("785898006 |Conventional release solution for irrigation (dose form)|"), Boolean.TRUE);
+		acceptableDoseForms.put(gl.getConcept("785910004 |Prolonged-release intralesional implant (dose form)|"), Boolean.TRUE);
+		
+		Concept thisDoseForm = SnomedUtils.getTarget(c, doseFormTypes, UNGROUPED, CharacteristicType.INFERRED_RELATIONSHIP);
+		//Is this dose form acceptable?
+		if (acceptableDoseForms.containsKey(thisDoseForm)) {
+			if (acceptableDoseForms.get(thisDoseForm).equals(Boolean.FALSE)) {
+				report (c, issueStr2, thisDoseForm);
+			}
+		} else {
+			report (c, issueStr1, thisDoseForm);
+		}
+	}
+
+	private void populateSummaryTab() throws TermServerScriptException {
+		issueSummaryMap.entrySet().stream()
+				.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+				.forEach(e -> reportSafely (SECONDARY_REPORT, (Component)null, e.getKey(), e.getValue()));
+		
+		int total = issueSummaryMap.entrySet().stream()
+				.map(e -> e.getValue())
+				.collect(Collectors.summingInt(Integer::intValue));
+		reportSafely (SECONDARY_REPORT, (Component)null, "TOTAL", total);
+	}
+
+	protected void initialiseSummary(String issue) {
+		issueSummaryMap.merge(issue, 0, Integer::sum);
+	}
+	
+	protected boolean report (Concept c, Object...details) throws TermServerScriptException {
+		//First detail is the issue
+		issueSummaryMap.merge(details[0].toString(), 1, Integer::sum);
+		countIssue(c);
+		return super.report (PRIMARY_REPORT, c, details);
+	}
+	
+	private void populateAcceptableDoseFormMaps() throws TermServerScriptException {
+		String fileName = "resources/acceptable_dose_forms.tsv";
+		debug ("Loading " + fileName );
+		try {
+			List<String> lines = Files.readLines(new File(fileName), Charsets.UTF_8);
+			boolean isHeader = true;
+			for (String line : lines) {
+				String[] items = line.split(TAB);
+				if (!isHeader) {
+					Concept c = gl.getConcept(items[0]);
+					acceptableMpfDoseForms.put(c, items[2].equals("yes"));
+					acceptableCdDoseForms.put(c, items[3].equals("yes"));
+				} else {
+					isHeader = false;
+				}
+			}
+		} catch (IOException e) {
+			throw new TermServerScriptException("Unable to read " + fileName, e);
+		}
+	}
+	
+	private boolean isMPF(Concept concept) {
+		return concept.getConceptType().equals(ConceptType.MEDICINAL_PRODUCT_FORM) || 
+				concept.getConceptType().equals(ConceptType.MEDICINAL_PRODUCT_FORM_ONLY);
+	}
+	
+	private boolean isCD(Concept concept) {
+		return concept.getConceptType().equals(ConceptType.CLINICAL_DRUG);
+	}
+	
+}
