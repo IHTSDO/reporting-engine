f20944f683eef4903bcc43f4204612f40608be24
pgwilliams <petergwilliams@hotmail.com>
2023-07-13 15:59:13 +0100

    QI-1258 Bring code into line with removal of 'withWait' flag in flush

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/oneOffs/MSSP1750_RemoveUSLangRefsetENGB.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/oneOffs/MSSP1750_RemoveUSLangRefsetENGB.java
@@ -32,7 +32,7 @@ public class MSSP1750_RemoveUSLangRefsetENGB extends DeltaGenerator implements S
 			delta.loadProjectSnapshot(false); //Need all descriptions loaded.
 			delta.postInit();
 			delta.process();
-			delta.flushFiles(false, true); //Need to flush files before zipping
+			delta.flushFiles(false); //Need to flush files before zipping
 			SnomedUtils.createArchive(new File(delta.outputDirName));
 		} finally {
 			delta.finish();
