00dcfeae2a51ff34fefeb2a5ab16ec45d1f189a8
Elena Ilyukhina <eil@ihtsdo.org>
2023-08-16 16:10:07 +0100

    FRI-628 - Changed report visibility to allow for testing

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/release/PackageComparisonReport.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/release/PackageComparisonReport.java
@@ -128,7 +128,7 @@ public class PackageComparisonReport extends SummaryComponentStats implements Re
 				.withDescription("This report compares two packages (zip archives) using Unix scripts with output captured into usual Google Sheets")
 				.withParameters(params)
 				.withTag(INT).withTag(MS)
-				.withProductionStatus(ProductionStatus.HIDEME)
+				.withProductionStatus(ProductionStatus.PROD_READY)
 				.withExpectedDuration(40)
 				.build();
 	}
