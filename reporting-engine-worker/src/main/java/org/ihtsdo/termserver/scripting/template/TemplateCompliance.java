075cb478acf55ffd835baa3d97b44b1ee8feec4f
pgwilliams <petergwilliams@hotmail.com>
2023-08-31 11:23:26 +0100

    RP-783 Remove 'Skip Known Complete' functionality

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/template/TemplateCompliance.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/template/TemplateCompliance.java
@@ -38,9 +38,7 @@ public class TemplateCompliance extends TemplateFix implements ReportClass {
 	private Map<Concept, List<String>> conceptDiagnosticsStated = new HashMap<>();
 	public static final String INCLUDE_COMPLEX = "Include complex cases";
 	public static final String INCLUDE_ORPHANET = "Include Orphanet";
-	public static final String KNOWN_COMPLETE = "Skip Known Complete";
-	public static final String KNOWN_COMPLETE_ECL = " <<2775001 OR <<3218000 OR <<3723001 OR <<5294002 OR <<7890003 OR <<8098009 OR <<17322007 OR <<20376005 OR <<34014006 OR <<40733004 OR <<52515009 OR <<85828009 OR <<87628006 OR <<95896000 OR <<109355002 OR <<118616009 OR <<125605004 OR <<125643001 OR <<125666000 OR <<125667009 OR <<125670008 OR <<126537000 OR <<128139000 OR <<128294001 OR <<128477000 OR <<128482007 OR <<131148009 OR <<193570009 OR <<233776003 OR <<247441003 OR <<276654001 OR <<283682007 OR <<298180004 OR <<307824009 OR <<312608009 OR <<362975008 OR <<399963005 OR <<399981008 OR <<400006008 OR <<400178008 OR <<416462003 OR <<416886008 OR <<417893002 OR <<428794004 OR <<429040005 OR <<432119003 OR <<441457006 OR <<419199007 OR <<55342001 OR <<128462008 OR <<363346000 OR <<372087000 OR <<785851003 OR <<308492005";
-	
+
 	List<Concept> alreadyReportAligned = new ArrayList<>();
 	List<Concept> alreadyReportedExcluded = new ArrayList<>();
 	
@@ -63,7 +61,6 @@ public class TemplateCompliance extends TemplateFix implements ReportClass {
 		
 		params.put(INCLUDE_COMPLEX, "false");
 		params.put(INCLUDE_ORPHANET, "false");
-		params.put(KNOWN_COMPLETE, "false");
 		TermServerReport.run(TemplateCompliance.class, args, params);
 	}
 	
@@ -77,9 +74,6 @@ public class TemplateCompliance extends TemplateFix implements ReportClass {
 					.withType(JobParameter.Type.BOOLEAN)
 					.withDefaultValue(true)
 				.add(INCLUDE_ORPHANET)
-					.withType(JobParameter.Type.BOOLEAN)
-					.withDefaultValue(true)
-				.add(KNOWN_COMPLETE)
 					.withType(JobParameter.Type.BOOLEAN)
 					.withDefaultValue(true)
 				.add(TEMPLATE)
@@ -131,10 +125,6 @@ public class TemplateCompliance extends TemplateFix implements ReportClass {
 			includeComplexTemplates = jobRun.getParameters().getMandatoryBoolean(INCLUDE_COMPLEX);
 			includeOrphanet = jobRun.getParameters().getMandatoryBoolean(INCLUDE_ORPHANET);
 			subsetECL = jobRun.getMandatoryParamValue(ECL);
-			
-			if (jobRun.getParameters().getMandatoryBoolean(KNOWN_COMPLETE)) {
-				subsetECL = "( " + subsetECL + " ) MINUS ( " + KNOWN_COMPLETE_ECL + " )";
-			}
 
 			String templateServerUrl = jobRun.getMandatoryParamValue(SERVER_URL);
 			if (!templateServerUrl.endsWith("template-service/") && (!templateServerUrl.endsWith("template-service"))) {
