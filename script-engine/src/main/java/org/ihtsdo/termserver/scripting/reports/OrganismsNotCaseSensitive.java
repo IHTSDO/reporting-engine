9e920e4406316b862540d10c3c251a5115f7d63b
Peter G. Groves Williams <pwi@snomed.org>
2023-09-15 16:52:38 +0100

    MSSP-2013 Add additional 'skipped' terms to list of cI Organisms

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/reports/OrganismsNotCaseSensitive.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/reports/OrganismsNotCaseSensitive.java
@@ -22,14 +22,32 @@ public class OrganismsNotCaseSensitive extends TermServerReport {
 			"Shiga", "Human", "Extended", "Subgenus","Adult","Multiple", "Influenza", "Enterotoxigenic", "Egg",
 			"Measles", "Form", "Subdivision", "Larva", "Enterohemorrhagic", "Enteropathogenic", "Enteroinvasive",
 			"Infraclass", "Superclass", "Toxigenic", "Cyst", "Subspecies", "Subphylum", "Subkingdom", "Mammalian",
-			"Rotavirus", "Equine", "Kingdom", "Domain", "Division"
+			"Rotavirus", "Equine", "Kingdom", "Domain", "Division",
+			"Carbapenem", "Yeast", "Linezolid", "Trophozoite", "Ballistoconidium", "Fluoroquinolone", "Aerobic",
+			"Methicillin", "Nymph", "Arthrospore", "Vancomycin", "Amastigote", "Mold", "Metallo-beta-lactamase",
+			"Proglottid", "Small-colony-forming", "Intraspecies", "Arthroconidium", "Vesicular", "Sporozoite",
+			"Spherule", "Filariform", "Hemolytically", "Enteroaggregative", "Diffuse", "Carbapenemase-producing",
+			"Infertile", "Chlamydoconidium", "Oocyst", "Helical", "Beta-hemolytic", "Nontoxigenic", "Curved",
+			"Catalase", "Anaerobic", "Cephalosporin", "Non group", "Saccharolytic", "Gamma-hemolytic", "Clarithromycin",
+			"Sexual", "Atypical", "Multidrug-resistant", "Infrakingdom", "Merozoite", "Mature", "Non-mucoid", "Pandemic",
+			"Non-multiple", "Fertile", "Rapid", "Rhabditiform", "Fermentative", "Hydrogen", "Provisional", "Ookinete",
+			"Sporocyst", "Schizont", "Procercoid", "Developing", "Asaccharolytic", "Pseudoalteromonas", "Annelloconidium",
+			"Penicillinase-producing", "Blastoconidium", "Asexual", "Plerocercoid", "Gametocyte", "Vibrio", "Isometric",
+			"Large-colony-forming", "Zygote", "Porcine", "Microgametocyte", "Extensively", "Sporont", "Penicillin",
+			"Fusiform", "Decorticated", "Reptilian", "Non-fermentative", "Spore", "Serogroup", "Basidiospore",
+			"Trypomastigote", "Aerotolerant", "Gamete", "Epimastigote", "Glycopeptide", "Coagulase", "Pupa", "Ampicillin",
+			"Borderline", "Promastigote", "Imago", "Mitosporic", "Fastidious", "Coryneform", "Domesticated", "Immature",
+			"Hypnozoite", "Conidium", "Non-motile", "Spectinomycin-resistant", "Phialoconidium", "Meront", "Macroconidium",
+			"Alpha-hemolytic", "Corticated", "Tobacco", "Tetracycline-resistant", "Microconidium", "Catalase-negative",
+			"Tissue", "Ascospore", "Nutritionally", "Coagulase-negative", "Multilocular", "Attenuated", "Magnetotactic",
+			"Merozoite", "Macrogametocyte", "Elastase-producing", "Hepatitis"
 	);
 
 	public static void main(String[] args) throws TermServerScriptException, IOException {
 		OrganismsNotCaseSensitive report = new OrganismsNotCaseSensitive();
 		try {
 			ReportSheetManager.targetFolderId = "1bwgl8BkUSdNDfXHoL__ENMPQy_EdEP7d";
-			report.additionalReportColumns = "FSN, SemanticTag, Substance Description, Organism Description, Case Significance Mismatch";
+			report.additionalReportColumns = "FSN, SemanticTag, ";
 			report.init(args);
 			report.loadProjectSnapshot(false);  //Load all descriptions
 			report.postInit();
@@ -44,6 +62,9 @@ public class OrganismsNotCaseSensitive extends TermServerReport {
 
 	private void reportOrganismsInSubstances() throws TermServerScriptException {
 		int organismsChecked = 0;
+		report(PRIMARY_REPORT, "Specifically these are organisms where the FSN is cI ie initial character case insensitive");
+		report(PRIMARY_REPORT, "");
+
 		for (Concept organism : ORGANISM.getDescendents(NOT_SET)) {
 			if (organism.getFSNDescription().getCaseSignificance().equals(CaseSignificance.INITIAL_CHARACTER_CASE_INSENSITIVE)
 				&& !startsWithSkippedWord(organism.getFsn())) {
