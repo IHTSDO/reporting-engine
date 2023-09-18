ed9dad51656a4696d8055d2b641731d106c03e76
Elena Ilyukhina <eil@ihtsdo.org>
2023-09-18 16:52:58 +0100

    MSSP-2268 - Fixed the issue when not all data was written into an RF2 file

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/InactivateLangRefsetMembersInWrongLanguage.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/InactivateLangRefsetMembersInWrongLanguage.java
@@ -1,7 +1,6 @@
 package org.ihtsdo.termserver.scripting.delta;
 
 import org.ihtsdo.otf.exception.TermServerScriptException;
-import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Metadata;
 import org.ihtsdo.termserver.scripting.GraphLoader;
 import org.ihtsdo.termserver.scripting.client.TermServerClient;
@@ -13,31 +12,25 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class InactivateLangRefsetMembersInWrongLanguage extends DeltaGenerator implements ScriptConstants {
 
-	/*private String refsetToCheck = "2021000195106";  //FR-CH Langrefset id
-	private String expectedLangCode = "fr";*/
 	private static final Logger LOGGER = LoggerFactory.getLogger(InactivateLangRefsetMembersInWrongLanguage.class);
 
 	public static void main(String[] args) throws TermServerScriptException, IOException, InterruptedException {
 		InactivateLangRefsetMembersInWrongLanguage delta = new InactivateLangRefsetMembersInWrongLanguage();
 		try {
-			delta.newIdsRequired = false; // We'll only be inactivating existing relationships
+			delta.newIdsRequired = false; // We'll only be inactivating existing members
 			TermServerClient.supportsIncludeUnpublished = false;   //This code not yet available in MS
 			delta.init(args);
-			delta.loadProjectSnapshot(false);  //Just FSN, not working with all descriptions here
+			delta.loadProjectSnapshot(false);
 			delta.postInit();
 			delta.startTimer();
 			delta.process();
-			SnomedUtils.createArchive(new File(delta.outputDirName));
+			delta.createOutputArchive();
 		} finally {
 			delta.finish();
-			if (delta.descIdGenerator != null) {
-				LOGGER.info(delta.descIdGenerator.finish());
-			}
 		}
 	}
 
@@ -58,34 +51,14 @@ public class InactivateLangRefsetMembersInWrongLanguage extends DeltaGenerator i
 							l.setActive(false);
 							l.setEffectiveTime(null);
 							c.setModified();
-							report(c, c.getFSNDescription(), Severity.LOW, ReportActionType.LANG_REFSET_INACTIVATED, d, l);
+							report(c, Severity.LOW, ReportActionType.LANG_REFSET_INACTIVATED, d, l);
 						}
 					}
 				}
 			}
-		/*}
-
-
-
-		for (Concept c : GraphLoader.getGraphLoader().getAllConcepts()) {
-			for (Description d : c.getDescriptions()) {
-				//Skip any descriptions in the expected language
-				if (d.getLang().equals(expectedLangCode)) {
-					continue;
-				}
-				for (LangRefsetEntry l : d.getLangRefsetEntries(ActiveState.ACTIVE, refsetToCheck)) {
-					if (l.getAcceptabilityId().equals(SCTID_PREFERRED_TERM)) {
-						l.setActive(false);
-						l.setEffectiveTime(null);
-						c.setModified();
-						report(c, Severity.LOW, ReportActionType.LANG_REFSET_INACTIVATED, d, l);
-					}
-				}
-			}*/
 
 			if (c.isModified()) {
 				incrementSummaryInformation("Concepts modified");
-				outputRF2(c);  //Will only output dirty fields.
 			}
 		}
 	}
@@ -100,6 +73,13 @@ public class InactivateLangRefsetMembersInWrongLanguage extends DeltaGenerator i
 		Metadata metadata = project.getMetadata();
 		refsetLangCodeMap.putAll(metadata.getLangRefsetLangMapping());
 		return refsetLangCodeMap;
+	}
+
+	private void createOutputArchive() throws TermServerScriptException {
+		outputModifiedComponents(true);
+		getRF2Manager().flushFiles(true); //Just flush the RF2, we might want to keep the report going
+		File archive = SnomedUtils.createArchive(new File(outputDirName));
+		report(null, Severity.NONE, ReportActionType.INFO, "Created " + archive.getName());
 	}
 
 }
