babe343dbd144226d7a3a18fb1b3d5f1d1411a80
pgwilliams <petergwilliams@hotmail.com>
2023-10-11 10:26:58 +0100

    PIP-379 Partial Commit for SpringBoot3 changes

--- schedule-manager/src/main/java/org/ihtsdo/snowowl/authoring/scheduler/api/service/ScheduleServiceImpl.java
+++ schedule-manager/src/main/java/org/ihtsdo/snowowl/authoring/scheduler/api/service/ScheduleServiceImpl.java
@@ -3,7 +3,7 @@ package org.ihtsdo.snowowl.authoring.scheduler.api.service;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 import org.apache.commons.lang.StringUtils;
 import org.ihtsdo.otf.rest.exception.BusinessServiceException;
