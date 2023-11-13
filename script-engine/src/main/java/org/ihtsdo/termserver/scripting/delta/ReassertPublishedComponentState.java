ff0f9085493fcf6c75c3968042572bd0813a4dae
pgwilliams <petergwilliams@hotmail.com>
2023-11-13 13:38:56 +0000

    MSSP-2411 Reassert published NO components

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/ReassertPublishedComponentState.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/ReassertPublishedComponentState.java
@@ -126,7 +126,12 @@ public class ReassertPublishedComponentState extends DeltaGenerator {
 						"3333dcf8-332d-b99b-7d59-6c4d34b63cf2",
 						"75cb4cac-ac05-fae4-1b35-d251a90198ad",
 						"d140b306-ecea-2518-852e-a9e4203650b9",
-						"e2b9b0ce-4444-9aab-7b95-d9d19cd15ec2"};
+						"e2b9b0ce-4444-9aab-7b95-d9d19cd15ec2"};*/
+
+	String[] componentsToProcess = new String[] {
+			"895479026",
+			"895478023",
+			"895477029"};
 
 	String[] componentsToProcess = new String[] {
 			"895479026",
