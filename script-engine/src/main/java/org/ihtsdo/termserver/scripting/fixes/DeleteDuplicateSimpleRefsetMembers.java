76422394c5573fd44ca6b66349d3fbc2d76db2cd
pgwilliams <petergwilliams@hotmail.com>
2024-01-23 17:06:52 +0000

    INFRA-12274 Add class to switch and delete duplicate simple refset members

---
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/DeleteDuplicateSimpleRefsetMembers.java
@@ -0,0 +1,97 @@
+package org.ihtsdo.termserver.scripting.fixes;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.ihtsdo.otf.exception.TermServerScriptException;
+import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
+import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Task;
+import org.ihtsdo.termserver.scripting.domain.Concept;
+import org.ihtsdo.termserver.scripting.domain.Description;
+import org.ihtsdo.termserver.scripting.domain.RefsetMember;
+import org.ihtsdo.termserver.scripting.domain.ScriptConstants;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class DeleteDuplicateSimpleRefsetMembers extends BatchFix implements ScriptConstants{
+
+	private String refsetOfInterest = "723264001";  // Lateralizeable body structure reference set
+
+	protected DeleteDuplicateSimpleRefsetMembers(BatchFix clone) {
+		super(clone);
+	}
+
+	public static void main(String[] args) throws TermServerScriptException, IOException, InterruptedException {
+		DeleteDuplicateSimpleRefsetMembers fix = new DeleteDuplicateSimpleRefsetMembers(null);
+		try {
+			fix.populateEditPanel = false;
+			fix.populateTaskDescription = false;
+			fix.selfDetermining = true;
+			fix.runStandAlone = false;
+			fix.getArchiveManager().setPopulateReleasedFlag(true);
+			fix.getArchiveManager().setLoadOtherReferenceSets(true);
+			fix.init(args);
+			fix.additionalReportColumns = "Active, Details";
+			//Recover the current project state from TS (or local cached archive) to allow quick searching of all concepts
+			fix.loadProjectSnapshot(false); 
+			fix.postInit();
+			fix.processFile();
+		} finally {
+			fix.finish();
+		}
+	}
+
+	@Override
+	public int doFix(Task t, Concept c, String info) throws TermServerScriptException {
+		//We'll assume there's only one published and one inactive, and if not, exception out.
+		RefsetMember publishedRM = getOtherRefsetMember(c, true);
+		RefsetMember newRM = getOtherRefsetMember(c, false);
+
+		if (!newRM.isActive()) {
+			throw new TermServerScriptException("Unexpected data condition: new refset member is not active: " + newRM);
+		}
+		if (publishedRM.isActive()) {
+			throw new TermServerScriptException("Unexpected data condition: published refset member is active: " + publishedRM);
+		}
+
+		publishedRM.setActive(true);
+		updateRefsetMember(t, publishedRM, "");
+		report(t, c, Severity.LOW, ReportActionType.REFSET_MEMBER_REACTIVATED, publishedRM);
+
+		deleteRefsetMember(t, newRM.getId(), false);
+		report(t, c, Severity.LOW, ReportActionType.REFSET_MEMBER_DELETED, newRM);
+		return CHANGE_MADE;
+	}
+
+	private RefsetMember getOtherRefsetMember(Concept c, boolean isReleased) throws TermServerScriptException {
+		List<RefsetMember> members = c.getOtherRefsetMembers().stream()
+				.filter(rm -> rm.getRefsetId().equals(refsetOfInterest))
+				.filter(rm -> (isReleased == rm.isReleased()))
+				.collect(Collectors.toList());
+		if (members.size() > 1 || members.size() == 0) {
+			throw new TermServerScriptException("Unexpected number of refset members (" + members.size() + ") for " + c + " isReleased=" + isReleased);
+		}
+
+		return members.get(0);
+	}
+
+	protected List<Component> identifyComponentsToProcess() throws TermServerScriptException {
+		Set<Concept> allAffected = new TreeSet<Concept>();  //We want to process in the same order each time, in case we restart and skip some.
+		nextConcept:
+		for (Concept c : gl.getAllConcepts()) {
+			boolean alreadySeen = false;
+			for (RefsetMember rm : c.getOtherRefsetMembers()) {
+				if (rm.getRefsetId().equals(refsetOfInterest)) {
+					if (alreadySeen) {
+						allAffected.add(c);
+						continue nextConcept;
+					} else {
+						alreadySeen = true;
+					}
+				}
+			}
+		}
+		return new ArrayList<Component>(allAffected);
+	}
+	
+}
