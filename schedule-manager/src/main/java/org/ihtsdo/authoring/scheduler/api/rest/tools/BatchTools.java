48d8e7752d9dfa204f1f9c352a07b60cdea9ada7
Jim Cornmell <jco@snomed.org>
2024-02-07 13:49:34 +0000

    RP-852 Stop infinite json data.

--- schedule-manager/src/main/java/org/ihtsdo/authoring/scheduler/api/rest/tools/BatchTools.java
+++ schedule-manager/src/main/java/org/ihtsdo/authoring/scheduler/api/rest/tools/BatchTools.java
@@ -3,12 +3,11 @@ package org.ihtsdo.authoring.scheduler.api.rest.tools;
 import org.ihtsdo.authoring.scheduler.api.repository.JobRunBatchRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.snomed.otf.scheduler.domain.JobRun;
 import org.snomed.otf.scheduler.domain.JobRunBatch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Limit;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class BatchTools {
@@ -27,12 +26,29 @@ public class BatchTools {
             batches = jobRunBatchRepository.findByOrderByIdDesc(Limit.of(limit.intValue()));
         }
 
+        // Can't use JsonIgnore annotation, as we need the json in some situations (ActivMQ),
+        // so clear the data we don't want, to stop any infinite recursion!
+        batches.forEach(batch -> batch.setJobRuns(null));
+
         return batches;
     }
 
     public JobRunBatch getBatch(Long id) {
-        Optional<JobRunBatch> batch = jobRunBatchRepository.findById(id);
-        LOGGER.info("Get Batch: {} (is present {})", id, batch.isPresent());
-        return batch.orElse(null);
+        JobRunBatch batch = jobRunBatchRepository.findById(id).orElse((null));
+
+        if (batch == null) {
+            LOGGER.warn("Get Batch: {} (is not present)", id);
+            return null;
+        }
+
+        LOGGER.info("Get Batch: {} (is present)", id);
+
+        // Can't use JsonIgnore annotation, as we need the json in some situations (ActivMQ),
+        // so clear the data we don't want, to stop any infinite recursion!
+        for (JobRun jobRun : batch.getJobRuns()) {
+            jobRun.setBatch(null);
+        }
+
+        return batch;
     }
 }
