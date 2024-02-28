e336a6c3a8c3fb03ee3325988a91a697a9023150
pgwilliams <petergwilliams@hotmail.com>
2024-02-28 07:06:23 +0000

    INFRA-12263 Allow for lateralized concept to already exist in source code system.  Also improve terming

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/ExtractExtensionComponentsAndLateralize.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/ExtractExtensionComponentsAndLateralize.java
@@ -1,6 +1,7 @@
 package org.ihtsdo.termserver.scripting.delta;
 
 import org.ihtsdo.otf.exception.TermServerScriptException;
+import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
 import org.ihtsdo.otf.utils.StringUtils;
 import org.ihtsdo.termserver.scripting.domain.*;
 import org.ihtsdo.termserver.scripting.util.SnomedUtils;
@@ -16,40 +17,67 @@ public class ExtractExtensionComponentsAndLateralize extends ExtractExtensionCom
 
 	private static Set<Concept> bodyStructures = null;
 
-	protected void doAdditionalProcessing(Concept c) throws TermServerScriptException {
+	protected void doAdditionalProcessing(Concept c, List<Component> componentsToProcess) throws TermServerScriptException {
+		//Now it might be that the source extension already has this concept lateralized
+		//eg 847081000000101 |Balloon dilatation of bronchus using fluoroscopic guidance (procedure)|
 		LOGGER.info("Creating lateralized concepts for " + c);
 		try {
-			createLateralizedConcept(c, LEFT);
-			createLateralizedConcept(c, RIGHT);
-			createLateralizedConcept(c, BILATERAL);
+			extractOrCreateLateralizedConcept(c, LEFT, componentsToProcess);
+			extractOrCreateLateralizedConcept(c, RIGHT, componentsToProcess);
+			extractOrCreateLateralizedConcept(c, BILATERAL, componentsToProcess);
 		} catch (TermServerScriptException e) {
-			report(c,  Severity.CRITICAL, ReportActionType.API_ERROR, "Failed to create lateralized concepts for " + c + " due to " + e.getMessage());
+			report(c,  Severity.CRITICAL, ReportActionType.API_ERROR, "Failed to create lateralized concepts for " + c + " due to: " + e.getMessage());
+		}
+	}
+
+	private void extractOrCreateLateralizedConcept(Concept c, Concept laterality, List<Component> componentsToProcess) throws TermServerScriptException {
+		String lateralityStr = laterality.getPreferredSynonym().toLowerCase();
+		Concept existingLateralizedConcept;
+		if (laterality.equals(BILATERAL)) {
+			existingLateralizedConcept = findBilateralDescendent(c);
+		} else {
+			existingLateralizedConcept = findLateralizedCounterpart(c, laterality, false);
+		}
+
+		if (existingLateralizedConcept == null) {
+			createLateralizedConcept(c, laterality);
+		} else {
+			//If this concept is already on our list to process, we don't need to take any action.
+			if (componentsToProcess.contains(existingLateralizedConcept)) {
+				report(c, Severity.LOW, ReportActionType.INFO, "Lateralized concept already scheduled for processed " + existingLateralizedConcept);
+				return;
+			}
+			report(c, Severity.MEDIUM, ReportActionType.INFO, "Lateralized concept already exists for " + c + " with laterality " + laterality + " as " + existingLateralizedConcept + ", adding to list to process");
+			//Indicate that we don't need to lateralize this concept any further! doAdditionalProcessing = false
+			extractComponent(existingLateralizedConcept, componentsToProcess, false);
 		}
 	}
 
 	private void createLateralizedConcept(Concept c, Concept laterality) throws TermServerScriptException {
 		Concept clone = c.cloneAsNewConcept();
 		//Lateralize the concepts in the role groups
-		for (RelationshipGroup g : c.getRelationshipGroups(CharacteristicType.STATED_RELATIONSHIP)) {
+		Collection<RelationshipGroup> originalGroups = clone.getRelationshipGroups(CharacteristicType.STATED_RELATIONSHIP);
+		for (RelationshipGroup g : originalGroups) {
 			if (laterality.equals(LEFT) || laterality.equals(BILATERAL)) {
-				lateralizeGroup(g, LEFT);
+				lateralizeGroup(g, LEFT, false);
 			}
 			if (laterality.equals(RIGHT)) {
-				lateralizeGroup(g, RIGHT);
+				lateralizeGroup(g, RIGHT, false);
 			}
 			if (laterality.equals(BILATERAL)) {
 				RelationshipGroup g2 = g.clone(SnomedUtils.getFirstFreeGroup(clone));
-				lateralizeGroup(g2, RIGHT);
+				lateralizeGroup(g2, RIGHT, true);
+				clone.addRelationshipGroup(g2, null);
 			}
 		}
 		lateralizeFsn(c, clone, laterality);
 		report(clone, Severity.LOW, ReportActionType.CONCEPT_ADDED, SnomedUtils.getDescriptions(clone), clone.toExpression(CharacteristicType.STATED_RELATIONSHIP));
 	}
 
-	private void lateralizeGroup(RelationshipGroup g, Concept laterality) throws TermServerScriptException {
+	private void lateralizeGroup(RelationshipGroup g, Concept laterality, boolean overrideCurrentLaterality) throws TermServerScriptException {
 		for (Relationship r : g.getRelationships()) {
 			if (isBodyStructure(r.getTarget())) {
-				Concept lateralizedBodyStructure = findLateralizedCounterpart(r.getTarget(), laterality);
+				Concept lateralizedBodyStructure = findLateralizedCounterpart(r.getTarget(), laterality, overrideCurrentLaterality);
 				if (lateralizedBodyStructure == null) {
 					throw new TermServerScriptException("Failed to find lateralized counterpart for " + r.getTarget() + " with laterality " + laterality);
 				}
@@ -64,16 +92,38 @@ public class ExtractExtensionComponentsAndLateralize extends ExtractExtensionCom
 		//Can we find the cut point for the specified body structure in the fsn?
 		for (String bodyStructureStr : getBodyStructureStrs(original)) {
 			if (clone.getFsn().contains(bodyStructureStr)) {
-				String laterlizedFsn = clone.getFsn().replace(bodyStructureStr, lateralityStr + " " + bodyStructureStr);
+				String laterlizedFsn;
+				//If it's an X of Y, then we'll do X of <left|right> Y
+				if (bodyStructureStr.contains(" of ")) {
+					String lateralizedBodyStructureStr = bodyStructureStr.replace(" of ", " of " + lateralityStr + " ");
+					laterlizedFsn = clone.getFsn().replace(bodyStructureStr, lateralizedBodyStructureStr);
+				} else {
+					laterlizedFsn = clone.getFsn().replace(bodyStructureStr, lateralityStr + " " + bodyStructureStr);
+				}
 				clone.getFSNDescription().setTerm(laterlizedFsn);
 				successfulLaterlization = true;
+				break;
 			}
 		}
-		
+
 		if (!successfulLaterlization) {
-			String allBodyStructures = getBodyStructureStrs(original).stream().collect(Collectors.joining(", "));
-			throw new TermServerScriptException("Failed to lateralize FSN for " + original + " with laterality " + laterality + " and body structure(s) " + allBodyStructures);
+			//We'll add the lateralized body structure to the end of the FSN
+			String[] fsnParts = SnomedUtils.deconstructFSN(clone.getFsn());
+			String lateralizedBodyStructurePT = getBodyStructurePT(clone);
+			if (laterality.equals(BILATERAL)) {
+				lateralizedBodyStructurePT = lateralizedBodyStructurePT.replace("left", "right and left");
+			}
+			String laterlizedFsn = fsnParts[0] + " of " + lateralizedBodyStructurePT + " " + fsnParts[1];
+			clone.getFSNDescription().setTerm(laterlizedFsn);
 		}
+		
+		/*if (!successfulLaterlization) {
+			String allBodyStructures = getBodyStructureStrs(original).stream().collect(Collectors.joining(", "));
+			if (StringUtils.isEmpty(allBodyStructures)) {
+				allBodyStructures = "None found";
+			}
+			throw new TermServerScriptException("Failed to lateralize FSN for " + original + " with laterality " + laterality + " and body structure(s): " + allBodyStructures);
+		}*/
         normalizeDescriptions(clone, laterality);
 	}
 
@@ -82,52 +132,99 @@ public class ExtractExtensionComponentsAndLateralize extends ExtractExtensionCom
 		clone.getDescriptions(ActiveState.ACTIVE).stream()
 				.filter(d -> !d.getType().equals(DescriptionType.FSN))
 				.forEach(d -> clone.removeDescription(d));
-		String pt = SnomedUtils.deconstructFSN(clone.getFsn())[0];
+		String pt = SnomedUtils.deconstructFSN(clone.getFSNDescription().getTerm())[0];
+		if (laterality.equals(BILATERAL)) {
+			pt = pt.replace("right and left", "bilateral");
+		}
 		Description ptDesc = Description.withDefaults(pt, DescriptionType.SYNONYM, Acceptability.PREFERRED);
 		clone.addDescription(ptDesc);
 
 		//Bilateral concepts will have an acceptable "both" synonym
 		if (laterality.equals(BILATERAL)) {
-			String bothTerm = pt.replace("bilateral", "both") + "s";
+			String bothTerm = pt.replace("bilateral", "both");
+			if (!bothTerm.endsWith("structure")) {
+				if (bothTerm.contains(" with ")) {
+					bothTerm = bothTerm.replace(" with ", "s with ");
+				} else if (bothTerm.contains(" using ")) {
+					bothTerm = bothTerm.replace(" using ", "s using ");
+				} else {
+					bothTerm += "s";
+				}
+			}
 			Description bothDesc = Description.withDefaults(bothTerm, DescriptionType.SYNONYM, Acceptability.ACCEPTABLE);
 			clone.addDescription(bothDesc);
 		}
     }
 
     private Set<String> getBodyStructureStrs(Concept original) throws TermServerScriptException {
-		Set<String> bodyStructureStrs = new HashSet<>();
+		Set<String> bodyStructureStrs = new LinkedHashSet<>();  //Preserve insertion order
 		for (RelationshipGroup g : original.getRelationshipGroups(CharacteristicType.STATED_RELATIONSHIP)) {
 			for (Relationship r : g.getRelationships()) {
 				if (isBodyStructure(r.getTarget())) {
 					for (Description d : r.getTarget().getDescriptions(ActiveState.ACTIVE, List.of(DescriptionType.SYNONYM))) {
 						bodyStructureStrs.add(d.getTerm().toLowerCase());
 					}
-					bodyStructureStrs.add(r.getTarget().getPreferredSynonym().toLowerCase());
+					String pt = r.getTarget().getPreferredSynonym().toLowerCase();
+					bodyStructureStrs.add(pt);
+
+					if (pt.contains("structure")) {
+						String ptWithoutStructure = pt.replace("structure", "").replace("  ", " ").trim();
+						bodyStructureStrs.add(ptWithoutStructure);
+						if (ptWithoutStructure.contains(" of ")) {
+							String ptWithoutStructureOf = ptWithoutStructure.replace(" of ", " ").replace("  ", " ").trim();
+							bodyStructureStrs.add(ptWithoutStructureOf);
+						}
+					}
+
+					//Also for the case X structure of Y, we'll try just "of Y"
+					if (pt.contains("structure of ")) {
+						String[] parts = pt.split("structure");
+						bodyStructureStrs.add(parts[1]);
+					}
 				}
 			}
 		}
 		return bodyStructureStrs;
 	}
 
-
-	private Concept findLateralizedCounterpart(Concept unlaterlizedConcept, Concept laterality) {
-		//Ideally our concept would be "Structure of X" and we'd look for "Structure of left X"
-		//So try that first, and if not, we'll try the laterality in every place.
-		String unlateralizedFsn = unlaterlizedConcept.getFsn();
-		String lateralityStr = laterality.getPreferredSynonym().toLowerCase();
-		Concept lateralizedBodyStructure = null;
-		if (unlateralizedFsn.startsWith("Structure of ")) {
-			String lateralizedFsn = unlateralizedFsn.replace("Structure of ", "Structure of " + lateralityStr + " ");
-			lateralizedBodyStructure = findBodyStructureWithFsn(lateralizedFsn);
+	private String getBodyStructurePT(Concept c) throws TermServerScriptException {
+		Set<String> bodyStructureStrs = new TreeSet<>();
+		for (RelationshipGroup g : c.getRelationshipGroups(CharacteristicType.STATED_RELATIONSHIP)) {
+			for (Relationship r : g.getRelationships()) {
+				if (isBodyStructure(r.getTarget())) {
+					String pt = r.getTarget().getPreferredSynonym().toLowerCase();
+					return pt;
+				}
+			}
 		}
-
-		if (lateralizedBodyStructure == null) {
-			lateralizedBodyStructure = findLateralizedCounterpartExhaustive(unlaterlizedConcept, lateralityStr);
-		}
-		return lateralizedBodyStructure;
+		return null;
 	}
 
-	private Concept findLateralizedCounterpartExhaustive(Concept unlaterlizedConcept, String lateralityStr) {
+
+	private Concept findLateralizedCounterpart(Concept unlaterlizedConcept, Concept laterality, boolean overrideCurrentLaterality) throws TermServerScriptException {
+		//Ideally our concept would be "Structure of X" and we'd look for "Structure of left X"
+		//So try that first, and if not, we'll try the laterality in every place.
+		Concept lateralizedCounterpart = null;
+		String lateralityStr = laterality.getPreferredSynonym().toLowerCase();
+		String unlateralizedFsn = unlaterlizedConcept.getFsn();
+
+		if (overrideCurrentLaterality) {
+			String lateralizedFsn = unlateralizedFsn.replace("left", lateralityStr).replace("right", lateralityStr);
+			lateralizedCounterpart = findBodyStructureWithFsn(lateralizedFsn);
+		}
+
+		if (lateralizedCounterpart == null && isBodyStructure(unlaterlizedConcept) && unlateralizedFsn.startsWith("Structure of ")) {
+			String lateralizedFsn = unlateralizedFsn.replace("Structure of ", "Structure of " + lateralityStr + " ");
+			lateralizedCounterpart = findBodyStructureWithFsn(lateralizedFsn);
+		}
+
+		if (lateralizedCounterpart == null) {
+			lateralizedCounterpart = findLateralizedCounterpartExhaustive(unlaterlizedConcept, lateralityStr);
+		}
+		return lateralizedCounterpart;
+	}
+
+	private Concept findLateralizedCounterpartExhaustive(Concept unlaterlizedConcept, String lateralityStr) throws TermServerScriptException {
 		//Try the laterality in every place
 		String unlateralizedFsn = unlaterlizedConcept.getFsn();
 		Concept lateralizedBodyStructure = null;
@@ -148,6 +245,10 @@ public class ExtractExtensionComponentsAndLateralize extends ExtractExtensionCom
 			}
 		}
 
+		return findSingleLateralizedChild(unlaterlizedConcept, lateralityStr);
+	}
+
+	private Concept findSingleLateralizedChild(Concept unlaterlizedConcept, String lateralityStr) {
 		//Now it might be that the text of the laterlized structure is different from that of the unlateralized structure
 		//For example, 955009 |Bronchial structure| is lateralized to 736637009 |Structure of left bronchus|
 		List<Concept> lateralizedChildren = new ArrayList<>();
@@ -158,15 +259,37 @@ public class ExtractExtensionComponentsAndLateralize extends ExtractExtensionCom
 		}
 
 		if (lateralizedChildren.size() > 1) {
-			throw new IllegalArgumentException("Multiple lateralized children found for " + unlaterlizedConcept);
+			String allChildrenStr = lateralizedChildren.stream().map(Concept::toString).collect(Collectors.joining(", "));
+			throw new IllegalArgumentException("Multiple lateralized children found for unlateralized: " + unlaterlizedConcept + ": " + allChildrenStr);
 		} else if (lateralizedChildren.size() == 1) {
-			lateralizedBodyStructure = lateralizedChildren.get(0);
+			return lateralizedChildren.get(0);
 		}
-		return lateralizedBodyStructure;
-
+		return null;
 	}
 
-	private Concept findBodyStructureWithFsn(String fsn) {
+
+	private Concept findBilateralDescendent(Concept c) {
+		//If this concept has one immediate LEFT child, and one immediate RIGHT child, then
+		//if BOTH of those concepts feature the same concept, then that is likely to be the bilateral one
+		Concept leftChild = findSingleLateralizedChild(c, LEFT.getPreferredSynonym().toLowerCase());
+		Concept rightChild = findSingleLateralizedChild(c, RIGHT.getPreferredSynonym().toLowerCase());
+		if (leftChild != null && rightChild != null) {
+			Set<Concept> leftChildren = leftChild.getChildren(CharacteristicType.INFERRED_RELATIONSHIP);
+			Set<Concept> rightChildren = rightChild.getChildren(CharacteristicType.INFERRED_RELATIONSHIP);
+			leftChildren.retainAll(rightChildren);
+			if (leftChildren.size() == 1) {
+				Concept bilateral = leftChildren.iterator().next();
+				LOGGER.info("Sanity check bilateral detection: " + bilateral);
+				return bilateral;
+			}
+		}
+		return null;
+	}
+
+	private Concept findBodyStructureWithFsn(String fsn) throws TermServerScriptException {
+		if (bodyStructures == null) {
+			bodyStructures = BODY_STRUCTURE.getDescendants(NOT_SET, CharacteristicType.INFERRED_RELATIONSHIP);
+		}
 		for (Concept c : bodyStructures) {
 			if (c.getFsn().equals(fsn)) {
 				return c;
