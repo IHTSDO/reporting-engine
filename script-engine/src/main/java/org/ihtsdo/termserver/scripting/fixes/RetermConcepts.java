2b343b7bc2912eb0b6c99a5ab6ee7bb19e70acb2
Peter Groves Williams <petergwilliams@hotmail.com>
2024-03-07 11:37:57 -0700

    INFRA-12620 Retermining for Angioplasty

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/RetermConcepts.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/RetermConcepts.java
@@ -22,12 +22,14 @@ public class RetermConcepts extends BatchFix {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(RetermConcepts.class);
 	
-	private String identifyingText = "bladder";
-	private String excludeText = "urinary";
+	private String identifyingText = "angioplasty";
+	private String excludeText = null;
 	private Map<String, String> replacementMap;
-	private boolean forcePTAlignment = true;
+	private String prefix = "Transluminal";
+	private boolean forcePTAlignment = false;
 	private Collection<Concept> requiredAttributeValue = null;
 	Map<String, Concept> currentFSNs = new HashMap<>();
+	List<DescriptionType> typesOfInterest = List.of(DescriptionType.FSN);
 	
 	protected RetermConcepts(BatchFix clone) {
 		super(clone);
@@ -52,18 +54,18 @@ public class RetermConcepts extends BatchFix {
 	}
 
 	public void postInit() throws TermServerScriptException {
-		subsetECL = "*";
+		subsetECL = "<< 418285008 |Angioplasty of blood vessel (procedure)|";
 		replacementMap = new HashMap<>();
 		//text should be lower case.
 		//TODO Make replacement match case, if there's a chance of it being the first word
-		replacementMap.put("bladder", "urinary bladder");
-		requiredAttributeValue = findConcepts("<< 89837001|Urinary bladder structure|");
+		//replacementMap.put("bladder", "urinary bladder");
+		
+		//requiredAttributeValue = findConcepts("<< 89837001|Urinary bladder structure|");
 
 		LOGGER.info("Obtaining all current FSNs...");
 		//There are duplicates.  In the case of duplicate, we'll keep the first one we find.
 		gl.getAllConcepts().stream()
 				.collect(Collectors.toMap(Concept::getFsn, Function.identity(), (existing, replacement) -> existing));
-
 		super.postInit();
 	}
 
@@ -89,40 +91,77 @@ public class RetermConcepts extends BatchFix {
 
 	private int reterm(Task t, Concept c) throws TermServerScriptException, ValidationFailure {
 		int changesMade = 0;
-		for (Map.Entry<String,String> entry : replacementMap.entrySet()) {
-			String find = entry.getKey();
-			String replace = entry.getValue();
-			for (Description d : c.getDescriptions(ActiveState.ACTIVE)) {
+		//for (Map.Entry<String,String> entry : replacementMap.entrySet()) {
+		//	String find = entry.getKey();
+		//	String replace = entry.getValue();
+			for (Description d : c.getDescriptions(ActiveState.ACTIVE, typesOfInterest)) {
 				//Skip FSNs & text definitions
 				/*if (!d.getType().equals(DescriptionType.SYNONYM)) {
 					continue;
 				}*/
 				//Only process FSN + PT
-				if (!d.isPreferred()) {
+				//Only process FSN
+				if (!d.getType().equals(DescriptionType.FSN)/*!d.isPreferred()*/ || d.getTerm().toLowerCase().contains(prefix)) {
 					continue;
 				}
+				
+				//
 				//In this case we're looking for an entire match
-				if ((d.getTerm().contains(find) || d.getTerm().contains(StringUtils.capitalizeFirstLetter(find)))
+				/*if ((d.getTerm().contains(find) || d.getTerm().contains(StringUtils.capitalizeFirstLetter(find)))
 						&& !d.getTerm().contains(excludeText)) {
 					if (!d.isReleased()) {
 						report(t, c, Severity.MEDIUM, ReportActionType.INFO, "New description this cycle");
 					}
-					String replacement = d.getTerm().replaceAll(find, replace);
+					String replacement = d.getTerm().replaceAll(find, replace);*/
+				if (d.getTerm().toLowerCase().contains(identifyingText)) {
+					String replacement = null;
+					boolean normalizePT = true;
+					if (d.getTerm().toLowerCase().startsWith(identifyingText)) {
+						replacement = prefix + " " + StringUtils.decapitalizeFirstLetter(d.getTerm());
+					} else {
+						replacement = d.getTerm().replaceAll(identifyingText, prefix.toLowerCase() + " " + identifyingText);
+						normalizePT = false;
+					}
+
 
 					//If the term is unchanged, try an upper case replacement
-					if (replacement.equals(d.getTerm())) {
-						replacement = d.getTerm().replaceAll(StringUtils.capitalizeFirstLetter(find), StringUtils.capitalizeFirstLetter(replace));
-					}
+					//if (replacement.equals(d.getTerm())) {
+					//	replacement = d.getTerm().replaceAll(StringUtils.capitalizeFirstLetter(find), StringUtils.capitalizeFirstLetter(replace));
+					//}
 
 					//If the replacement is a known FSN, we'll skip
 					if (currentFSNs.containsKey(replacement)) {
 						report(t, c, Severity.MEDIUM, ReportActionType.VALIDATION_CHECK, "Replacement is a known FSN: " + replacement);
 						continue;
 					}
-					replaceDescription(t, c, d, replacement, InactivationIndicator.NONCONFORMANCE_TO_EDITORIAL_POLICY, true, "", null	);
+
+					//And we'll replace the PT while we're about it, but warn if there's GB variance
+					String ptTerm = SnomedUtils.deconstructFSN(replacement)[0];
+					Description usPT = c.getPreferredSynonym(US_ENG_LANG_REFSET);
+					Description gbPT = c.getPreferredSynonym(GB_ENG_LANG_REFSET);
+
+					//If the PT does not start with our identifying text, we'll leave it alone
+					if (!usPT.getTerm().toLowerCase().startsWith(identifyingText)) {
+						normalizePT = false;
+						report(t, c, Severity.MEDIUM, ReportActionType.INFO, "PT not normalized as FSN is not pattern 'Angioplasty of X'", usPT);
+						String ptReplacement = usPT.getTerm().replace(identifyingText, prefix.toLowerCase() + " " + identifyingText);
+						replaceDescription(t, c, usPT, ptReplacement, InactivationIndicator.NONCONFORMANCE_TO_EDITORIAL_POLICY, true, null);
+					}
+
+					replaceDescription(t, c, d, replacement, InactivationIndicator.NONCONFORMANCE_TO_EDITORIAL_POLICY, normalizePT, "", null);
+
+					if (normalizePT) {
+						forcePTAlignment(t, c);
+					}
+
+					if (!usPT.equals(gbPT)) {
+						report(t, c, Severity.HIGH, ReportActionType.VALIDATION_CHECK, "Check US / GB Variance", gbPT);
+					}
 					changesMade++;
+				} else {
+					report(t, c, Severity.MEDIUM, ReportActionType.VALIDATION_CHECK, "FSN did not start with " + identifyingText);
 				}
-			}
+		//	}
 		}
 		return changesMade;
 	}
@@ -150,7 +189,7 @@ public class RetermConcepts extends BatchFix {
 		List<Component> toProcess = new ArrayList<>();
 		nextConcept:
 		for (Concept c : SnomedUtils.sort(findConcepts(subsetECL, true, true))) {
-		//for (Concept c : Collections.singleton(gl.getConcept("210215001"))) {
+		//for (Concept c : Collections.singleton(gl.getConcept("710074006"))) {
 			if (!containsRequiredAttributeValue(c)) {
 				continue;
 			}
@@ -160,7 +199,9 @@ public class RetermConcepts extends BatchFix {
 					continue;
 				}
 				String termLower = d.getTerm().toLowerCase();
-				if (termLower.contains(identifyingText) && !termLower.contains(excludeText)) {
+				if (termLower.contains(identifyingText) 
+						&& (excludeText == null  || !termLower.contains(excludeText))
+						&& !termLower.contains(prefix.toLowerCase())) {
 					toProcess.add(c);
 					break;
 				}
@@ -198,6 +239,9 @@ public class RetermConcepts extends BatchFix {
 	}
 
 	private boolean containsRequiredAttributeValue(Concept c) {
+		if (requiredAttributeValue == null) {
+			return true;
+		}
 		return c.getRelationships().stream()
 				.filter(r -> r.isActive())
 				.filter(r -> !r.getType().equals(IS_A))
