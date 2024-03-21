45f1dc2839bb01a0cb881abb4ad2bbef75a33768
Peter Groves Williams <petergwilliams@hotmail.com>
2024-03-21 10:13:47 +0000

    INFRA-12263 Mop up UK langrefset members transferred in error

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/DeleteComponents.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/DeleteComponents.java
@@ -1,6 +1,5 @@
 package org.ihtsdo.termserver.scripting.fixes;
 
-import org.apache.commons.lang.exception.ExceptionUtils;
 import org.ihtsdo.otf.exception.TermServerScriptException;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Task;
@@ -17,21 +16,8 @@ public class DeleteComponents extends BatchFix implements ScriptConstants {
 	private static final Logger LOGGER = LoggerFactory.getLogger(DeleteComponents.class);
 
 	private String[] componentsToDelete = new String[] {
-			"bd14e5fb-478f-4de8-9977-5d37f5575785","9001d8e1-463a-445e-95cc-2bcaf37db18e","94dc0c97-c45b-42f1-9003-993ff2353904",
-			"992ec59e-ce60-4ac6-ab85-be1cbfafdbc8","ea288158-8198-4097-af02-25f4af5954de","bcb02d40-f9f8-4393-a96d-bced68729095",
-			"59fd684c-544c-4fd6-a960-69b8f878c7d9","85c0787e-7746-4e6a-ad28-cc7d2d8b763f","b02c7dee-1e17-4aed-a226-6efa575d8164",
-			"bd1121bf-1ff2-46ab-ab1e-dd91a51ac987","951a3276-9945-4a89-b578-57de0c0ca534","5926923a-91f7-474b-9e86-0be6b14f65be",
-			"adcf6572-fe11-4049-8b18-5c016918c79e","44dff1ce-431e-4548-8e64-b08968d9e3eb","05f45084-ba3b-405d-83cb-9f7b9f14edd6",
-			"ab26dadd-e426-425e-b8d8-b83fabb3de75","9e4eaa44-0aae-4c3a-8da5-8c4e6b75577e","284e98d2-47fe-473a-bd73-316d92404e78",
-			"3636e8c7-94d4-4c53-8341-757e953ea17d","8c6c1455-473e-42fa-bd36-02a42440f4c4","0c2dac1c-de31-4286-b403-945947355584",
-			"ccd6b6f8-e457-4032-b215-f7fe33ab7c0a","658b5485-2a0a-499c-900e-93984b1197d9","68f44366-50d5-41e5-8aad-3080dfbd610b",
-			"17e23b7c-47cc-4d54-b463-abdd0dc63b5f","e8c5cc16-a923-4a78-ab11-1a681104adc1","fc0f8fa1-bad5-4ffc-b3e1-8d8f4f3a123a",
-			"7a867650-c54e-4bc1-99f4-a905f2129c11","9239a886-13ff-41dd-89af-785bcc3836bf","29f0241f-30dc-4dde-8cc9-883aa0634f1d",
-			"1c118fc8-7e2b-4f28-b7c8-0c48953040dc","20a30ed1-5bc6-4714-a106-937227619491","62a18088-b378-45e9-8296-49d4598b96a8",
-			"1c51b3f8-ff8d-4f01-bc12-8b6c59da1b5f","433d3a25-34e2-45b0-86be-beb08a5a7580","6c95892a-bab9-43a4-9dd1-88639a1d4995",
-			"328a9b96-721e-462b-8eeb-d220e7de40e8","f8f5e465-c7de-4972-a56a-7e6d10d16b7e","34b08a77-3580-4206-a5a8-2b3359d2ef1a",
-			"daa29599-7910-4f15-85c2-9138580b3679","1449f983-4bb7-448b-a2d1-60988313b6b6","783977f8-2562-4602-95f7-b7f326ae109b",
-			"9c0a668b-dee8-46f4-b60c-5d18e428b72b","157e61d4-7dc7-40ea-96c4-4ec5bb08a03b","1a9c15ed-20a8-4b80-9e7c-a09685a878ed"
+		"4523b4f4-edc8-4123-8d97-f65e658bdea1","7487abdf-d923-471d-897d-e7db5f518118",
+		"d967c399-049a-47be-8044-09c6210d4efd","db7b351e-e1d6-4300-ae73-594388b4e912"
 	};
 
 	protected DeleteComponents(BatchFix clone) {
