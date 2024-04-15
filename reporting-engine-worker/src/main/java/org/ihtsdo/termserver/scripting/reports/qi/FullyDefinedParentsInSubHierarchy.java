f1d9a535e7b27dd57140f746b7209d8afab4c13e
Peter Groves Williams <petergwilliams@hotmail.com>
2024-04-15 23:00:10 +0100

    RP-887 Make 'Check Concepts with FD Parents' report available to MS Customers and enhance to work with ECL

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/qi/FullyDefinedParentsInSubHierarchy.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/qi/FullyDefinedParentsInSubHierarchy.java
@@ -8,29 +8,25 @@ import org.ihtsdo.otf.exception.TermServerScriptException;
 import org.ihtsdo.termserver.scripting.ReportClass;
 import org.ihtsdo.termserver.scripting.domain.Concept;
 import org.ihtsdo.termserver.scripting.reports.TermServerReport;
+import org.ihtsdo.termserver.scripting.util.SnomedUtils;
 import org.snomed.otf.scheduler.domain.*;
 import org.snomed.otf.scheduler.domain.Job.ProductionStatus;
 import org.snomed.otf.script.dao.ReportSheetManager;
 
 /**
  * QI-31
- * Once remodelling is complete, a "Post Changes" check that can be run is to 
+ * Once remodelling is complete, a "Post Changes" check that can be run is to
  * check for any concepts that still have immediate fully defined parents.
- * 
+ *
  * SUBST-153 Also uses this report.
  * RP-240 Make available in Reporting Platform
+ * RP-887 Make available to MS Customers and upgrade to work with ECL
  */
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class FullyDefinedParentsInSubHierarchy extends TermServerReport implements ReportClass {
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(FullyDefinedParentsInSubHierarchy.class);
 
 	public static void main(String[] args) throws TermServerScriptException, IOException {
 		Map<String, String> params = new HashMap<>();
-		params.put(SUB_HIERARCHY, "105590001"); // Substance
+		params.put(ECL, "<< 105590001"); // Substance
 		TermServerReport.run(FullyDefinedParentsInSubHierarchy.class, args, params);
 	}
 	
@@ -43,7 +39,7 @@ public class FullyDefinedParentsInSubHierarchy extends TermServerReport implemen
 	@Override
 	public Job getJob() {
 		JobParameters params = new JobParameters()
-				.add(SUB_HIERARCHY).withType(JobParameter.Type.CONCEPT).withDefaultValue(ROOT_CONCEPT)
+				.add(ECL).withType(JobParameter.Type.ECL).withMandatory()
 				.build();
 		return new Job()
 				.withCategory(new JobCategory(JobType.REPORT, JobCategory.GENERAL_QA))
@@ -51,13 +47,19 @@ public class FullyDefinedParentsInSubHierarchy extends TermServerReport implemen
 				.withDescription("This report lists all concepts in the specified subhierarchy which have one or more fully defined stated parents.  The issues count displays the number of concepts found.")
 				.withProductionStatus(ProductionStatus.PROD_READY)
 				.withParameters(params)
-				.withTag(INT)
+				.withTag(INT).withTag(MS)
 				.build();
 	}
 	
 	public void runJob() throws TermServerScriptException {
+		Set<Concept> conceptsOfInterest = findConcepts(subsetECL)
+				.stream()
+				.filter(c -> inScope(c))
+				.sorted((c1, c2) -> SnomedUtils.compareSemTagFSN(c1,c2))
+				.collect(Collectors.toSet());
+		
 		nextConcept:
-		for (Concept c : gl.getDescendantsCache().getDescendants(subHierarchy)) {
+		for (Concept c : conceptsOfInterest) {
 			for (Concept parent : c.getParents(CharacteristicType.STATED_RELATIONSHIP)) {
 				if (parent.getDefinitionStatus().equals(DefinitionStatus.FULLY_DEFINED)) {
 					String parentStr = c.getParents(CharacteristicType.STATED_RELATIONSHIP)
