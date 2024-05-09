28f9b62719dfd001698b564459ba2824d45062b1
Elena Ilyukhina <eil@ihtsdo.org>
2024-05-09 19:14:21 +0100

    RAP-26 Add header diff count

--- reporting-engine-worker/scripts/compare-packages-parallel.sh
+++ reporting-engine-worker/scripts/compare-packages-parallel.sh
@@ -1,43 +1,65 @@
 #!/bin/bash
 set -e;
 
-function extractZip {
-	zipName=$1
-	dirName=$2
-	echo "Extracting $zipName to $dirName"
-	mkdir -p $dirName
-	if [ "$enhancedZip" = true ]; then
-		7z e -bd -o$dirName "$zipName" "*.*" -r
+extractZip () {
+	archive=$1
+	targetDir=$2
+
+	mkdir -p "${targetDir}"
+	if [ "${enhancedZip}" ]; then
+		7z e -bd -o"${targetDir}" "${archive}" "*.*" -r
 	else
-		unzip "$zipName" -d $dirName
+		unzip "${archive}" -d "${targetDir}"
 	fi
 }
 
-function createLists {
-	listName=$1
-	dir=$2
-	home=`pwd`
-	cd $dir
-	find . -type f > ${home}/target/"${listName}"_file_list.txt
-	cd - > /dev/null
+createList () {
+	sourceDir=$1
+	targetFile=$2	
+
+	find "${sourceDir}" -type f -execdir basename {} ';' | sort > "${targetFile}"
 }
 
-function stripBetaPrefix() {
+countLines () {
+	sourceDir=$1
+	targetFile=$2
+
+	for thisFile in ${sourceDir}/* ; do
+		fileName=`basename "${thisFile}"`
+		fileCount=$((`wc -l "${thisFile}" | awk '{print $1}'` - 1)) # exclude header line
+		echo -e "${fileName}\t${fileCount}" >> "${targetFile}"
+	done
+}
+
+stripBetaPrefix () {
 	targetDir=$1
-	tempDir=`pwd`
+
+	currentDir=`pwd`
+
 	cd ${targetDir}
 	for thisFile in * ; do
-		if [[ ${thisFile} == x* ]]; then
+		if [[ "${thisFile}" == x* ]]; then
 			nonBetaName=`echo ${thisFile} | cut -c2-`
-			echo "Stripping beta prefix from ${targetDir}/${thisFile}"
-			mv ${thisFile} ${nonBetaName}
+			mv -v "${thisFile}" "${nonBetaName}"
 		fi
 	done
-	cd ${tempDir}
+
+	cd ${currentDir}
 }
 
-rm -rf target || true
-mkdir -p target
+normaliseDates () {
+	targetDir=$1
+
+	currentDir=`pwd`
+
+	cd "${targetDir}"
+	for thisFile in * ; do
+		noDateName=`echo ${thisFile} | sed "s/_\([0-9]\{8\}\)//"`
+		mv -v "${thisFile}" "${noDateName}"
+	done
+
+	cd "${currentDir}"
+}
 
 if [ $# -lt 4 ]; then
 	echo "Usage: compare-packages-parallel.sh <Left Archive Name> <Left Archive Location> <Right Archive Name> <Right Archive Location> [-normaliseDates]" >&2
@@ -53,13 +75,16 @@ then
 fi
 
 echo "Checking if if 7-zip is installed"
-SevenZipInstalled=`command -v 7z || true `
-if [ -z "${SevenZipInstalled}" ]
+sevenZipInstalled=`command -v 7z || true `
+if [ -z "${sevenZipInstalled}" ]
 then
 	echo "Could not detect 7-Zip, falling back to use 'unzip'. This may cause problems for filenames with non-ASCII characters."
 else
 	enhancedZip=true
 fi
+
+rm -rf target || true
+mkdir -p target
 
 leftName=$1
 leftArchive=$2
@@ -73,57 +98,60 @@ if [ "${flags}" == "-normaliseDates" ]; then
 fi
 
 leftLocation="target/left_archive"
-echo "Left archive in ${leftLocation}"
+echo "Extracting left archive into ${leftLocation}"
 extractZip "${leftArchive}" "${leftLocation}"
-echo "Extracted left archive"
 
 rightLocation="target/right_archive"
-echo "Right archive in ${rightLocation}"
+echo "Extracting right archive into ${rightLocation}"
 extractZip "${rightArchive}" "${rightLocation}"
-echo "Extracted right archive"
 
-#Move the left structure into directory "a" excluding json files
+# Move the left structure into directory "a" excluding json files
 mkdir -p target/a
 echo "Moving ${leftName} from ${leftLocation} into flat structure 'a'"
-find ${leftLocation} -type f ! -name "*.json" | xargs -I {} mv {} target/a
+find "${leftLocation}" -type f ! -name "*.json" | xargs -I {} mv {} target/a
 
-#Move the right structure into directory "b" excluding json files
+# Move the right structure into directory "b" excluding json files
 mkdir -p target/b
 echo "Moving ${rightName} from ${rightLocation} into flat structure 'b'"
-find ${rightLocation} -type f ! -name "*.json" | xargs -I {} mv {} target/b
+find "${rightLocation}" -type f ! -name "*.json" | xargs -I {} mv {} target/b
 
-#If we're normalising the dates, remove dates from a and b
-if [ "${normaliseDates}" == true ]; then
+# Remove dates from a and b
+if [ "${normaliseDates}" ]; then
 	echo
 	echo "Normalising dates"
-	for thisFile in target/a/* ; do
-		newFileName=`echo ${thisFile} | sed "s/_\([0-9]\{8\}\)//"`
-		mv -v ${thisFile} ${newFileName}
-	done
-	for thisFile in target/b/* ; do
-		newFileName=`echo ${thisFile} | sed "s/_\([0-9]\{8\}\)//"`
-		mv -v ${thisFile} ${newFileName}
-	done
+	normaliseDates target/a
+	normaliseDates target/b
 fi
 
 echo
-echo "Strip any Beta archive prefix"
+echo "Stripping any beta archive prefix"
 stripBetaPrefix target/a
 stripBetaPrefix target/b
 
 echo
-echo "Create lists"
-createLists "${leftName}" "target/a"
-createLists "${rightName}" "target/b"
+echo "Creating lists"
+
+leftFilesList="_${leftName}"_files_list.txt
+rightFilesList="_${rightName}"_files_list.txt
+commonFilesList="_common_files_list.txt"
+
+createList "target/a" "${leftFilesList}"
+createList "target/b" "${rightFilesList}"
+comm -1 -2 "${leftFilesList}" "${rightFilesList}" > "${commonFilesList}"
 
 mkdir -p target/c
 
-echo "_File list differences ${leftName} vs ${rightName}"
-diff target/"${leftName}"_file_list.txt target/"${rightName}"_file_list.txt | tee target/c/diff_file_list.txt #&& echo "None"
 echo
+echo "Calculating line count without header line"
+countLines "target/a" "target/c/left_files_line_counts.txt"
+countLines "target/b" "target/c/right_files_line_counts.txt"
 
-echo "_File content differences_"
-echo "Between $leftName $leftArchive and $rightName $rightArchive"
+echo
+echo "Files differences between ${leftName} and ${rightName}"
+diff "${leftFilesList}" "${rightFilesList}" | tee target/c/diff__files.txt
+
+echo
+echo "File content differences between ${leftName} and ${rightName}"
 if [[ $OSTYPE == 'darwin'* ]]; then
 	# MacOS
 	echo `md5 $leftArchive`
@@ -138,23 +166,28 @@ echo "Line count diff is ${leftName} minus ${rightName}"
 echo "File size diff is ${leftName} minus ${rightName}"
 
 echo
+echo "Creating process order file"
 processOrderFile="_process_order.txt"
 {
-	find target/b -type f | sed "s/target\/b\///" | grep "sct2_" | sort;
-	find target/b -type f | sed "s/target\/b\///" | grep "der2_" | sort;
-	#Now we'll just do a file size comparison of any other file
-	find target/b -type f | sed "s/target\/b\///" | egrep -v "sct2_|der2_" | sort;
-} >> ${processOrderFile}
+	grep "sct2_" "${commonFilesList}" | sort;
+	grep "der2_" "${commonFilesList}" | sort;
+	grep -E -v "sct2_|der2_" "${commonFilesList}" | sort;
+} >> "${processOrderFile}"
 
-parallelFeed="ParallelFeed_${RANDOM}.txt"
+echo
+echo "Creating parallel feed file"
+parallelFeed="_parallel_feed_${RANDOM}.txt"
 while IFS= read -r file
 do
 	leftFile="target/a/${file}"
 	rightFile="target/b/${file}"
-	echo "${leftFile} ${rightFile} ${file} ${leftName} ${rightName}" >> ${parallelFeed}
-done < "$processOrderFile"
+	echo "${leftFile} ${rightFile} ${file}" >> "${parallelFeed}"
+done < "${processOrderFile}"
 
 parallel -j 6 --no-notice --ungroup --colsep ' ' -a ${parallelFeed} ./compare-files.sh
 
 rm ${parallelFeed}
 rm ${processOrderFile}
+rm ${commonFilesList}
+rm ${leftFilesList}
+rm ${rightFilesList}
