e1afdcd823774dfd67c0d1a6c58e8b9e853b5b44
pgwilliams <petergwilliams@hotmail.com>
2024-06-03 17:08:41 +0100

    MSSP-2881 Bring one-off classes into line with recent changes made on develop branch

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/template/NormaliseConceptsDriven.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/template/NormaliseConceptsDriven.java
@@ -62,8 +62,8 @@ public class NormaliseConceptsDriven extends NormaliseConcepts {
 
 		//Have we specified a ppp in the issues field?
 		Concept newPPP = checkConceptForSpecifiedPPP(c);
-		changesMade += checkAndSetProximalPrimitiveParent(t, c, newPPP, false, false);
-		
+		changesMade += checkAndSetProximalPrimitiveParent(t, c, newPPP, false, false, null);
+
 		//Remove any redundant relationships, or they'll be missing from the inferred view
 		changesMade += removeRedundandRelationships(t,c);
 		
