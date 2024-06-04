ee439e48e663ecc3ddeb0b03c9f33c42646516ca
pgwilliams <petergwilliams@hotmail.com>
2024-06-04 17:17:17 +0100

    MSSP-2881 Refactor, use otf ExceptionUtils to ensure full stack trace is logged on exception

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/AssignConceptsToTasks.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/AssignConceptsToTasks.java
@@ -1,6 +1,6 @@
 package org.ihtsdo.termserver.scripting.fixes;
 
-import org.apache.commons.lang.exception.ExceptionUtils;
+import org.ihtsdo.otf.utils.ExceptionUtils;
 import org.ihtsdo.otf.exception.TermServerScriptException;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.*;
 import org.ihtsdo.termserver.scripting.ValidationFailure;
