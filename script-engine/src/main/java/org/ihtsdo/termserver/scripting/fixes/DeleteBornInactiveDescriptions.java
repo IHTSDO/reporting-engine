6add90637a6f1f6a7e63237f2ac7d6798b97f0fb
pgwilliams <petergwilliams@hotmail.com>
2024-06-04 17:17:17 +0100

    MSSP-2881 Refactor, use otf ExceptionUtils to ensure full stack trace is logged on exception

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/DeleteBornInactiveDescriptions.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/DeleteBornInactiveDescriptions.java
@@ -3,7 +3,7 @@ package org.ihtsdo.termserver.scripting.fixes;
 import java.io.IOException;
 import java.util.*;
 
-import org.apache.commons.lang.exception.ExceptionUtils;
+import org.ihtsdo.otf.utils.ExceptionUtils;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.*;
 import org.ihtsdo.otf.exception.TermServerScriptException;
 import org.ihtsdo.termserver.scripting.domain.*;
