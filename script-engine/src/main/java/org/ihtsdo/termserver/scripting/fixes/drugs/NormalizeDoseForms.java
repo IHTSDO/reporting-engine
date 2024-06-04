55c19a68b286cad762f8fe2bfba734f3a216de9a
pgwilliams <petergwilliams@hotmail.com>
2024-06-04 17:17:17 +0100

    MSSP-2881 Refactor, use otf ExceptionUtils to ensure full stack trace is logged on exception

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/drugs/NormalizeDoseForms.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/drugs/NormalizeDoseForms.java
@@ -3,7 +3,7 @@ package org.ihtsdo.termserver.scripting.fixes.drugs;
 import java.io.IOException;
 import java.util.*;
 
-import org.apache.commons.lang.exception.ExceptionUtils;
+import org.ihtsdo.otf.utils.ExceptionUtils;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Task;
 import org.ihtsdo.otf.exception.TermServerScriptException;
--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/loinc/ZoomAndEnhanceLOINC.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/loinc/ZoomAndEnhanceLOINC.java
@@ -5,7 +5,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.exception.ExceptionUtils;
+import org.ihtsdo.otf.utils.ExceptionUtils;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.*;
 import org.ihtsdo.otf.exception.TermServerScriptException;
 import org.ihtsdo.termserver.scripting.ValidationFailure;
