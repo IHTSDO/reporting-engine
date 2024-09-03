6b978c5dea3fdd7c18553603a2720a52091507d1
pgwilliams <petergwilliams@hotmail.com>
2024-09-03 16:59:07 +0100

    LE-29 Add hard coded attribute mapping overrides

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/pipeline/loinc/AttributePartMapManager.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/pipeline/loinc/AttributePartMapManager.java
@@ -56,12 +56,7 @@ public class AttributePartMapManager implements LoincScriptConstants {
 
 		hardCodedTypeReplacementMap.put(gl.getConcept("410670007 |Time|"), gl.getConcept("370134009 |Time aspect|"));
 
-		hardCodedMappings.put("LP182450-9", List.of(
-				gl.getConcept("259337002 |Calcifediol (substance)|"),
-				gl.getConcept("67517005 |25-hydroxyergocalciferol (substance)|")));
-		hardCodedMappings.put("LP36683-8 ", List.of(
-				gl.getConcept("106202009 |Antigen in ABO blood group system (substance)|"),
-				gl.getConcept("16951006 |Antigen in Rh blood group system (substance)|")));
+		populateHardCodedMappings();
 	}
 
 	public List<RelationshipTemplate> getPartMappedAttributeForType(int idxTab, String loincNum, String loincPartNum, Concept attributeType) throws TermServerScriptException {
@@ -206,4 +201,57 @@ public class AttributePartMapManager implements LoincScriptConstants {
 	public Set<String> getAllMappedLoincPartNums() {
 		return loincPartToAttributeMap.keySet();
 	}
+
+	private void populateHardCodedMappings() throws TermServerScriptException {
+		hardCodedMappings.put("LP29646-4", List.of(
+				gl.getConcept("580171010000100 |Estimated average glucose calculation (calculation)|")));
+		hardCodedMappings.put("LP443407-4", List.of(
+				gl.getConcept("580191010000104 |Entitic mean volume (property) (qualifier value)|")));
+		hardCodedMappings.put("LP443403-3", List.of(
+				gl.getConcept("718499004 |Color (property) (qualifier value)|")));
+		hardCodedMappings.put("LP443402-5", List.of(
+				gl.getConcept("726564007 |Specific gravity (property) (qualifier value)|")));
+		hardCodedMappings.put("LP443404-1", List.of(
+				gl.getConcept("580181010000102 |Entitic mass concentration (property) (qualifier value)")));
+		hardCodedMappings.put("LP30809-5", List.of(
+				gl.getConcept("540101010000101 |Anion gap calculation (calculation)|")));
+		hardCodedMappings.put("LP15429-1", List.of(
+				gl.getConcept("570181010000107 |Base excess calculation (calculation)|")));
+		hardCodedMappings.put("LP17102-2", List.of(
+				gl.getConcept("570191010000105 |International normalized ratio calculation (calculation)|")));
+		hardCodedMappings.put("LP32756-6", List.of(
+				gl.getConcept("570161010000104 |Iron saturation calculation (calculation)|")));
+		hardCodedMappings.put("LP21258-6", List.of(
+				gl.getConcept("570171010000109 |Oxygen saturation calculation (calculation)|")));
+		hardCodedMappings.put("LP38056-5", List.of(
+				gl.getConcept("570091010000109 |Antigen of epidermal growth factor receptor (substance)|")));
+		hardCodedMappings.put("LP65528-9", List.of(
+				gl.getConcept("540161010000100 |Automated refractometry technique (qualifier value)|")));
+		hardCodedMappings.put("LP15678-3", List.of(
+				gl.getConcept("570151010000101 |Iron binding capacity calculation (calculation)|")));
+		hardCodedMappings.put("LP443415-7", List.of(
+				gl.getConcept("723206002 |Anion gap based on sodium, chloride and bicarbonate ions calculation technique (qualifier value)|")));
+		hardCodedMappings.put("LP70259-4", List.of(
+				gl.getConcept("570101010000100 |Automated technique (qualifier value)")));
+		hardCodedMappings.put("LP443406-6", List.of(gl.getConcept("580111010000107 |Distribution width (property) (qualifier value)")));
+		hardCodedMappings.put("LP36715-8", List.of(gl.getConcept("702668005 |Modification of diet in renal disease formula (qualifier value)")));
+		hardCodedMappings.put("LP105134-3", List.of(gl.getConcept("703462006 |Cockcroft-Gault formula (qualifier value)|")));
+		hardCodedMappings.put("LP71296-5", List.of(gl.getConcept("570111010000102 |Immature neutrophil (cell)|")));
+		hardCodedMappings.put("LP69203-5", List.of(gl.getConcept("580101010000109 |Glomerular filtration rate (calculation)|")));
+		hardCodedMappings.put("LP6879-3", List.of(gl.getConcept("762636008 |Duration (property) (qualifier value)|")));
+		hardCodedMappings.put("LP21276-8", List.of(gl.getConcept("118576005 |Time ratio (property) (qualifier value)")));
+		hardCodedMappings.put("LP443431-4", List.of(gl.getConcept("580121010000101 |Modification of diet in renal disease creatinine calculation formula relative to 1.73 square meters body surface area and adjusted for non-African race (qualifier value)|")));
+		hardCodedMappings.put("LP443426-4", List.of(gl.getConcept("580131010000103 |Modification of diet in renal disease creatinine calculation formula relative to 1.73 square meters body surface area and adjusted for African race (qualifier value)|")));
+		hardCodedMappings.put("LP443418-1", List.of(gl.getConcept("580141010000106 |Modification of diet in renal disease creatinine calculation formula relative to 1.73 square meters body surface area (qualifier value)|")));
+		hardCodedMappings.put("LP443421-5", List.of(gl.getConcept("580151010000108 |Chronic Kidney Disease Epidemiology Collaboration creatinine calculation formula relative to 1.73 square meters body surface area (qualifier value)|")));
+		hardCodedMappings.put("LP36683-8", List.of(gl.getConcept("106202009 |Antigen in ABO blood group system (substance)|"), gl.getConcept("16951006 |Antigen in Rh blood group system (substance)")));
+		hardCodedMappings.put("LP15445-7", List.of(gl.getConcept("259498006 |Bilirubin glucuronide (substance)|"),
+				gl.getConcept("73828001 |Bilirubin-albumin complex (substance)")));
+		hardCodedMappings.put("LP182450-9", List.of(gl.getConcept("259337002 |Calcifediol (substance"),
+				gl.getConcept("67517005 |25-hydroxyergocalciferol (substance)")));
+		hardCodedMappings.put("LP443466-0", List.of(gl.getConcept("734842000 |Source (property) (qualifier value)|")));
+		hardCodedMappings.put("LP443467-8", List.of(gl.getConcept("123038009 |Specimen (specimen)|")));
+		hardCodedMappings.put("LP281728-8", List.of(gl.getConcept("410652009 |Blood product (product)|")));
+	}
+
 }
