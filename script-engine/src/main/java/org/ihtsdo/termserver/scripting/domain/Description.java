d3e63f0353ed0034bc3f2315cad7d1c11164ada9
pgwilliams <petergwilliams@hotmail.com>
2024-09-09 12:16:48 +0100

    INFRA-13323 Update LangRefset RefCompId when Description id is changed

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/domain/Description.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/domain/Description.java
@@ -180,6 +180,10 @@ public class Description extends Component implements ScriptConstants, Serializa
 	@Override
 	public void setId(String descriptionId) {
 		this.descriptionId = descriptionId;
+
+		for (LangRefsetEntry thisDialect : getLangRefsetEntries()) {
+			thisDialect.setReferencedComponentId(descriptionId);
+		}
 	}
 
 	public void setDescriptionId(String descriptionId) {
