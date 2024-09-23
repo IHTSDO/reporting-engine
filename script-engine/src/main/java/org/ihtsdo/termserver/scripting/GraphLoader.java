c95e173836ef8970ec8846ce2387ba8ee9001e66
pgwilliams <petergwilliams@hotmail.com>
2024-09-23 18:59:58 +0100

    NUVA-3 Transform NUVA vaccines into SNOMED CT concepts

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/GraphLoader.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/GraphLoader.java
@@ -1781,7 +1781,7 @@ public class GraphLoader implements ScriptConstants {
 	}
 
 	public Map<String, String> getSchemaMap(Concept scheme) {
-		return alternateIdentifierMap.get(scheme);
+		return alternateIdentifierMap.computeIfAbsent(scheme, k -> new HashMap<>());
 	}
 	
 	public Map<Concept, Map<String, String>> getAlternateIdentifierMap() {
