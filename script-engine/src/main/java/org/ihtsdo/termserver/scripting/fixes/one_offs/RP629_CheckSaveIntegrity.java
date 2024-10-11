38807511d2a45e740a3a73debb93e666bdeb8779
pgwilliams <petergwilliams@hotmail.com>
2024-10-11 14:18:48 +0100

    INFRA-13754 refactor oneOffs package name to align with standard naming practice

---
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/one_offs/RP629_CheckSaveIntegrity.java
@@ -0,0 +1,71 @@
+package org.ihtsdo.termserver.scripting.fixes.one_offs;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
+import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Task;
+import org.ihtsdo.otf.exception.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.domain.*;
+import org.ihtsdo.termserver.scripting.fixes.BatchFix;
+import org.ihtsdo.termserver.scripting.util.SnomedUtils;
+
+/**
+ * Save a concept that has description inactivation indicator and association
+ */
+public class RP629_CheckSaveIntegrity extends BatchFix {
+	
+	protected RP629_CheckSaveIntegrity(BatchFix clone) {
+		super(clone);
+	}
+
+	public static void main(String[] args) throws TermServerScriptException, IOException, InterruptedException {
+		RP629_CheckSaveIntegrity fix = new RP629_CheckSaveIntegrity(null);
+		try {
+			fix.selfDetermining = true;
+			fix.reportNoChange = true;
+			fix.populateTaskDescription = false;
+			fix.additionalReportColumns = "Action Detail";
+			fix.init(args);
+			fix.loadProjectSnapshot(true);
+			fix.postInit();
+			fix.processFile();
+		} finally {
+			fix.finish();
+		}
+	}
+
+	@Override
+	public int doFix(Task t, Concept c, String info) throws TermServerScriptException {
+		int changesMade = 0;
+		info("Imported View");
+		outputInactiveDescriptions(c);
+		
+		info("Loaded View");
+		Concept loadedConcept = loadConcept(c, t.getBranchPath());
+		outputInactiveDescriptions(loadedConcept);
+		updateConcept(t, loadedConcept, info);
+		if (true);
+		
+		info("Saved View");
+		loadedConcept = loadConcept(c, t.getBranchPath());
+		outputInactiveDescriptions(loadedConcept);
+		return changesMade;
+	}
+
+	private void outputInactiveDescriptions(Concept c) throws TermServerScriptException {
+		for (Description d : c.getDescriptions(ActiveState.INACTIVE)) {
+			debug (d + " : " + d.getInactivationIndicator());
+			debug ("   " + SnomedUtils.prettyPrintHistoricalAssociations(d, gl));
+		}
+	}
+
+	@Override
+	protected List<Component> identifyComponentsToProcess() throws TermServerScriptException {
+		List<Component> components = new ArrayList<>();
+		//components.add(gl.getConcept("90534006"));
+		components.add(gl.getConcept("418249008"));
+		return components;
+	}
+
+}
