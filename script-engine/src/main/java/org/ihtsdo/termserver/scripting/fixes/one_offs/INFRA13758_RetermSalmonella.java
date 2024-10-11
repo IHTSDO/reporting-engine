495edbc6b364812c207952784eca1350b1dbd3c6
pgwilliams <petergwilliams@hotmail.com>
2024-10-11 14:18:48 +0100

    INFRA-13754 refactor oneOffs package name to align with standard naming practice

---
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/one_offs/INFRA13758_RetermSalmonella.java
@@ -0,0 +1,84 @@
+package org.ihtsdo.termserver.scripting.fixes.one_offs;
+
+import org.ihtsdo.otf.exception.TermServerScriptException;
+import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
+import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Task;
+import org.ihtsdo.otf.utils.ExceptionUtils;
+import org.ihtsdo.termserver.scripting.ValidationFailure;
+import org.ihtsdo.termserver.scripting.domain.*;
+import org.ihtsdo.termserver.scripting.fixes.BatchFix;
+import org.snomed.otf.script.dao.ReportSheetManager;
+
+import java.util.*;
+
+public class INFRA13758_RetermSalmonella extends BatchFix {
+
+	String search = "Salmonella III arizonae";
+	String replace = "Salmonella enterica subsp. diarizonae";
+
+	protected INFRA13758_RetermSalmonella(BatchFix clone) {
+		super(clone);
+	}
+
+	public static void main(String[] args) throws TermServerScriptException {
+		INFRA13758_RetermSalmonella fix = new INFRA13758_RetermSalmonella(null);
+		try {
+			ReportSheetManager.setTargetFolderId(GFOLDER_ADHOC_UPDATES);
+			fix.populateEditPanel = true;
+			fix.populateTaskDescription = true;
+			fix.selfDetermining = true;
+			fix.reportNoChange = true;
+			fix.additionalReportColumns = "Action Detail, Additional Detail";
+			fix.init(args);
+			fix.getArchiveManager().setPopulateReleasedFlag(true);
+			fix.loadProjectSnapshot(false);
+			fix.postInit();
+			fix.processFile();
+		} finally {
+			fix.finish();
+		}
+	}
+
+	@Override
+	public int doFix(Task task, Concept concept, String info) throws TermServerScriptException {
+		int changesMade = 0;
+		try {
+			Concept loadedConcept = loadConcept(concept, task.getBranchPath());
+			changesMade = modifyDescriptions(task, loadedConcept);
+			if (changesMade > 0) {
+				updateConcept(task, loadedConcept, info);
+			}
+		} catch (ValidationFailure v) {
+			report(task, concept, v);
+		} catch (Exception e) {
+			report(task, concept, Severity.CRITICAL, ReportActionType.API_ERROR, "Failed to save changed concept to TS: " + ExceptionUtils.getStackTrace(e));
+		}
+		return changesMade;
+	}
+
+	private int modifyDescriptions(Task t, Concept c) throws TermServerScriptException {
+		int changesMade = 0;
+		for (Description d : c.getDescriptions(ActiveState.ACTIVE)) {
+			if (d.getTerm().contains(search)) {
+				String replacement = d.getTerm().replace(search, replace);
+				replaceDescription(t, c, d, replacement, InactivationIndicator.OUTDATED, true);
+				changesMade++;
+			}
+		}
+		return changesMade;
+	}
+
+	@Override
+	protected List<Component> identifyComponentsToProcess() throws TermServerScriptException {
+		List<Component> process = new ArrayList<>();
+		for (Concept c : ORGANISM.getDescendants(NOT_SET)) {
+			for (Description d : c.getDescriptions(ActiveState.ACTIVE)) {
+				if (d.getTerm().contains(search)) {
+					process.add(c);
+					break;
+				}
+			}
+		}
+		return process;
+	}
+}
