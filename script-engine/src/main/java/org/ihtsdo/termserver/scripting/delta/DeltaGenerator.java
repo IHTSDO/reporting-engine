5e3cd047cf8d55fc5de3a7ef508482f59f1b921e
pgwilliams <petergwilliams@hotmail.com>
2024-10-30 14:26:46 +0000

    LE-35 Add support for RF2 Delta output of Simple Refset Members

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/DeltaGenerator.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/DeltaGenerator.java
@@ -44,6 +44,7 @@ public abstract class DeltaGenerator extends TermServerScript {
 	protected String altIdDeltaFilename;
 	protected String compAnnotDeltaFilename;
 	protected String simpleMapDeltaFilename;
+	protected String simpleRefsetDeltaFilename;
 	protected String edition = "INT";
 	
 	protected String eclSubset = null;
@@ -338,6 +339,10 @@ public abstract class DeltaGenerator extends TermServerScript {
 		simpleMapDeltaFilename = refDir + "Content/der2_sRefset_SimpleMapDelta_"+edition+"_" + today + ".txt";
 		fileMap.put(ComponentType.SIMPLE_MAP, simpleMapDeltaFilename);
 		writeToRF2File(simpleMapDeltaFilename, simpleMapHeader);
+
+		simpleRefsetDeltaFilename = refDir + "Content/der2_Refset_SimpleRefsetDelta_"+edition+"_" + today + ".txt";
+		fileMap.put(ComponentType.SIMPLE_REFSET_MEMBER, simpleRefsetDeltaFilename);
+		writeToRF2File(simpleRefsetDeltaFilename, simpleRefsetHeader);
 		
 		attribValDeltaFilename = refDir + "Content/der2_cRefset_AttributeValueDelta_"+edition+"_" + today + ".txt";
 		fileMap.put(ComponentType.ATTRIBUTE_VALUE, attribValDeltaFilename);
@@ -367,7 +372,7 @@ public abstract class DeltaGenerator extends TermServerScript {
 		return conceptsOutput;
 	}
 	
-	protected void outputRF2(ComponentType componentType, String[] columns) throws TermServerScriptException {
+	public void outputRF2(ComponentType componentType, String[] columns) throws TermServerScriptException {
 		String fileName = fileMap.get(componentType);
 		writeToRF2File(fileName, columns);
 	}
