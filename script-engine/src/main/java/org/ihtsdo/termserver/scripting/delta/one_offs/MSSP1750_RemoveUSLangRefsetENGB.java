c34d198ef55dd616a2582d96e13c071caf1d0682
pgwilliams <petergwilliams@hotmail.com>
2024-11-14 16:09:27 +0000

    MSSP-3325 Include DeltaGenerator 'process' refactor in one-off classes

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/one_offs/MSSP1750_RemoveUSLangRefsetENGB.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/delta/one_offs/MSSP1750_RemoveUSLangRefsetENGB.java
@@ -42,7 +42,8 @@ public class MSSP1750_RemoveUSLangRefsetENGB extends DeltaGenerator implements S
 		}
 	}
 
-	private void process() throws ValidationFailure, TermServerScriptException {
+	@Override
+	protected void process() throws TermServerScriptException {
 		for (Concept c : SnomedUtils.sort(gl.getAllConcepts())) {
 			for (Description d : c.getDescriptions(ActiveState.ACTIVE)) {
 				boolean changesMade = false;
