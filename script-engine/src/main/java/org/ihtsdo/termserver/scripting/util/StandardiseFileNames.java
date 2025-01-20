6c51c00aae2ce9538f5dcdf24855e38b5813daa4
pgwilliams <petergwilliams@hotmail.com>
2025-01-20 16:36:42 +0000

    RP-1000 Refactoring to improve Sonar Qube score.  No functional impact

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/util/StandardiseFileNames.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/util/StandardiseFileNames.java
@@ -50,7 +50,7 @@ public class StandardiseFileNames extends TermServerReport {
 	public static void main(String[] args) throws ScriptException, IOException {
 		StandardiseFileNames curator = new StandardiseFileNames();
 		try {
-			ReportSheetManager.targetFolderId = "13XiH3KVll3v0vipVxKwWjjf-wmjzgdDe"; //Technical Specialist
+			ReportSheetManager.setTargetFolderId("13XiH3KVll3v0vipVxKwWjjf-wmjzgdDe"); //Technical Specialist
 			curator.init(args);
 			curator.postInit(
 					new String[]{
