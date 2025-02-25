03a13e500711ef7cfb9ed11e7d841a787ecad7f1
pgwilliams <petergwilliams@hotmail.com>
2025-02-25 09:52:00 +0000

    INFRA-13738 Remodel Surgical Approach to Procedural Approach

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/ReplaceAttributeTypes.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/fixes/ReplaceAttributeTypes.java
@@ -1,7 +1,6 @@
 package org.ihtsdo.termserver.scripting.fixes;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.*;
@@ -11,10 +10,13 @@ import org.ihtsdo.termserver.scripting.domain.*;
 import org.ihtsdo.termserver.scripting.util.SnomedUtils;
 import org.snomed.otf.script.dao.ReportSheetManager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * LOINC-387 A batch update is required for concepts/expressions on this tab as following:
- * - Change property from 118586006 |Ratio (property) (qualifier value)| to 784316008 |Arbitrary fraction (property) (qualifier value)|
- * - Add Attribute value 704325000 |Relative to (attribute)| 48583005 |Immunoglobulin E (substance)|
+ * - Change property from 118586006 |Ratio (property) (qualifier value)| to 784316008 |Arbitrary fraction (property) (qualifier value)|
+ * - Add Attribute value 704325000 |Relative to (attribute)| 48583005 |Immunoglobulin E (substance)|
  *
  * INFRA-8111 for specified ECL selection map  
  *  424361007 |Using substance (attribute)| -> 363701004 |Direct substance (attribute)
@@ -27,16 +29,13 @@ import org.snomed.otf.script.dao.ReportSheetManager;
  *  			424361007 |Using substance| -> 363701004 |Direct substance|
  *  			Only where 260686004 |Method| = 129332006 |Irrigation - action|
  *  QI-1257    424226004 |Using device (attribute)| --> 363699004 |Direct device (attribute)|
+ *  INFRA-13738 24876005 |Surgical approach (attribute)|  --> 116688005 |Procedure approach (attribute)|
  */
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class ReplaceAttributeTypes extends BatchFix {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ReplaceAttributeTypes.class);
 
-	String ecl = "<< 71388002 |Procedure (procedure)| : 425391005 |Using access device (attribute)| = *";
+	String ecl = "< 71388002 |Procedure (procedure)| : 424876005 |Surgical approach (attribute)| = *";
 	Map<Concept, Concept> replaceTypesMap;
 	RelationshipTemplate addAttribute = null;
 	RelationshipTemplate whereAttributePresent = null;
@@ -53,6 +52,7 @@ public class ReplaceAttributeTypes extends BatchFix {
 			fix.populateTaskDescription = false;
 			fix.reportNoChange = true;
 			fix.selfDetermining = true;
+			fix.classifyTasks = true;
 			fix.init(args);
 			fix.loadProjectSnapshot(false);
 			fix.postInit();
@@ -62,10 +62,11 @@ public class ReplaceAttributeTypes extends BatchFix {
 		}
 	}
 
+	@Override
 	public void postInit() throws TermServerScriptException {
 		replaceTypesMap = new HashMap<>();
-		replaceTypesMap.put(gl.getConcept("425391005 |Using access device (attribute)|"),
-				gl.getConcept("424226004 |Using device (attribute)|"));
+		replaceTypesMap.put(gl.getConcept("424876005 |Surgical approach (attribute)|"),
+				gl.getConcept("116688005 |Procedure approach (attribute)|"));
 		super.postInit();
 	}
 
@@ -84,64 +85,55 @@ public class ReplaceAttributeTypes extends BatchFix {
 		return changesMade;
 	}
 	
-	private int replaceAttributeTypes(Task t, Concept c) throws TermServerScriptException, ValidationFailure {
+	private int replaceAttributeTypes(Task t, Concept c) throws TermServerScriptException {
 		int changesMade = 0;
-		boolean includeDuplicationWarning = false;
 		//Work group at a time and ensure the group contains the required attribute - if specified
 		for (RelationshipGroup g : c.getRelationshipGroups(CharacteristicType.STATED_RELATIONSHIP)) {
-			int replacedAttributesEncouteredInGroup = 0; //Either because we did the replacement or because they're already present
-			if (whereAttributePresent == null || g.containsTypeValue(whereAttributePresent)) {
-				for (Concept targetType : replaceTypesMap.keySet()) {
-					for (Relationship r : g.getRelationships(ActiveState.ACTIVE)) {
-						if (r.getType().equals(targetType)) {
-							Relationship replacement = r.clone();
-							replacement.setType(replaceTypesMap.get(targetType));
-							changesMade += replaceRelationship(t, c, r, replacement);
-							replacedAttributesEncouteredInGroup++;
-						} else if (replaceTypesMap.values().contains(r.getType())) {
-							replacedAttributesEncouteredInGroup++;
-						}
-					}
-				}
-			}
-			if (replacedAttributesEncouteredInGroup > 1) {
-				includeDuplicationWarning = true;
-			}
+			changesMade += replaceAttributeInGroup(t, c, g);
 		}
 		
 		if (addAttribute != null) {
 			changesMade += addRelationship(t, c, addAttribute, SnomedUtils.getFirstFreeGroup(c));
 		}
+		return changesMade;
+	}
 
-		if (includeDuplicationWarning) {
+	private int replaceAttributeInGroup(Task t, Concept c, RelationshipGroup g) throws TermServerScriptException {
+		int changesMade = 0;
+		int replacedAttributesEncouteredInGroup = 0; //Either because we did the replacement or because they're already present
+		if (whereAttributePresent == null || g.containsTypeValue(whereAttributePresent)) {
+			for (Concept targetType : replaceTypesMap.keySet()) {
+				for (Relationship r : g.getRelationships(ActiveState.ACTIVE)) {
+					if (r.getType().equals(targetType)) {
+						Relationship replacement = r.clone();
+						replacement.setType(replaceTypesMap.get(targetType));
+						changesMade += replaceRelationship(t, c, r, replacement);
+						replacedAttributesEncouteredInGroup++;
+					} else if (replaceTypesMap.values().contains(r.getType())) {
+						replacedAttributesEncouteredInGroup++;
+
+					}
+				}
+			}
+		}
+		if (replacedAttributesEncouteredInGroup > 1) {
 			report(t, c, Severity.HIGH, ReportActionType.VALIDATION_CHECK, "Duplicate attributes encountered in concept", c.toExpression(CharacteristicType.STATED_RELATIONSHIP));
 		}
 		return changesMade;
 	}
-	
+
+	@Override
 	protected List<Component> identifyComponentsToProcess() throws TermServerScriptException {
 		LOGGER.info("Identifying concepts to process");
-		
-		//First report those which we are NOT going to process
-		/*findConcepts(ecl).stream()
-				.filter(c -> !meetsProcessingCriteria(c))
-				.sorted((c1, c2) -> SnomedUtils.compareSemTagFSN(c1,c2))
-				.forEach(c -> { 
-					try {
-						report((Task)null, c, Severity.LOW, ReportActionType.VALIDATION_CHECK, "Concept did not meeting processing criteria", c.toExpression(CharacteristicType.STATED_RELATIONSHIP));
-					} catch (TermServerScriptException e) {
-						LOGGER.error("Exception encountered",e);
-					}
-				});*/
-		
 		return findConcepts(ecl).stream()
-			.filter(c -> meetsProcessingCriteria(c))
-			.sorted((c1, c2) -> SnomedUtils.compareSemTagFSN(c1,c2))
-			.collect(Collectors.toList());
+			.filter(this::meetsProcessingCriteria)
+			.sorted(SnomedUtils::compareSemTagFSN)
+				.map(c -> (Component)c)
+			.toList();
 	}
 
 	private boolean meetsProcessingCriteria(Concept c) {
-		if (whereAttributePresent == null || c.getRelationships(whereAttributePresent, ActiveState.ACTIVE).size() > 0) {
+		if (whereAttributePresent == null || !c.getRelationships(whereAttributePresent, ActiveState.ACTIVE).isEmpty()) {
 			for (Concept targetType : replaceTypesMap.keySet()) {
 				for (Relationship r : c.getRelationships(CharacteristicType.STATED_RELATIONSHIP, ActiveState.ACTIVE)) {
 					if (r.getType().equals(targetType)) {
