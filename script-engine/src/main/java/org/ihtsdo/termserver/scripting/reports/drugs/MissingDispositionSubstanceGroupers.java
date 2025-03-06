752289d7d0b0d833d16a05c407a3d5395716b5d4
pgwilliams <petergwilliams@hotmail.com>
2025-03-06 11:36:05 +0000

    INFRA-14594 Tidy up 2018 code for a fresh run of identifying missing disposition groupers

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/reports/drugs/MissingDispositionSubstanceGroupers.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/reports/drugs/MissingDispositionSubstanceGroupers.java
@@ -8,15 +8,14 @@ import org.ihtsdo.termserver.scripting.domain.*;
 import org.ihtsdo.termserver.scripting.reports.TermServerReport;
 import org.ihtsdo.termserver.scripting.util.SnomedUtils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * SUBST-267
  * Report to find dispositions that are not used by a substance disposition groupers
  * Identified by having "mechanism of action" in term.
  */
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class MissingDispositionSubstanceGroupers extends TermServerReport {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(MissingDispositionSubstanceGroupers.class);
@@ -45,31 +44,31 @@ public class MissingDispositionSubstanceGroupers extends TermServerReport {
 		dispositionGrouperMap = new HashMap<>();
 		for (Concept c : SUBSTANCE.getDescendants(NOT_SET)) {
 			for (Concept disposition : SnomedUtils.getTargets(c, new Concept[] {HAS_DISPOSITION}, CharacteristicType.STATED_RELATIONSHIP)) {
-				//Have we seen this disposition before?
-				Set<Concept> usedBy = dispositionSubstanceMap.get(disposition);
-				if (usedBy == null) {
-					usedBy = new HashSet<>();
-					dispositionSubstanceMap.put(disposition, usedBy);
+				populateDispositionUsedByConcept(c, disposition);
+			}
+		}
+		super.postInit();
+	}
+
+	private void populateDispositionUsedByConcept(Concept c, Concept disposition) {
+		//Have we seen this disposition before?
+		Set<Concept> usedBy = dispositionSubstanceMap.computeIfAbsent(disposition, k -> new HashSet<>());
+		usedBy.add(c);
+
+		//Is this a grouper concept?
+		if (c.getFsn().contains("mechanism of action")) {
+			if (dispositionGrouperMap.containsKey(disposition)) {
+				//Ignore the one with multiple dispositions
+				if (c.getFsn().contains(" and ")) {
+					LOGGER.warn("Ignoring multi-disposition substance {} in favour of {}", c, dispositionGrouperMap.get(disposition));
+				} else if (dispositionGrouperMap.get(disposition).getFsn().contains(" and ")) {
+					LOGGER.warn("Ignoring multi-disposition substance {} in favour of {}", dispositionGrouperMap.get(disposition), c);
+					dispositionGrouperMap.put(disposition, c); //Replace existing mapping
+				} else {
+					LOGGER.warn("{} has already been given grouper: {} now also {}", disposition,  dispositionGrouperMap.get(disposition), c);
 				}
-				usedBy.add(c);
-				
-				//Is this a grouper concept?
-				if (c.getFsn().contains("mechanism of action")) {
-					//LOGGER.debug("Adding grouper " + c + " to disposition " + disposition);
-					if (dispositionGrouperMap.containsKey(disposition)) {
-						//Ignore the one with multiple dispositions
-						if (c.getFsn().contains(" and ")) {
-							LOGGER.warn ("Ignoring multi-disposition substance " + c + " in favour of " + dispositionGrouperMap.get(disposition));
-						} else if (dispositionGrouperMap.get(disposition).getFsn().contains(" and ")) {
-							LOGGER.warn ("Ignoring multi-disposition substance " + dispositionGrouperMap.get(disposition) + " in favour of " + c);
-							dispositionGrouperMap.put(disposition, c); //Replace existing mapping
-						} else {
-						 LOGGER.warn (disposition + " has already been given grouper: " + dispositionGrouperMap.get(disposition) + " now also " + c);
-						}
-					} else {
-						dispositionGrouperMap.put(disposition, c);
-					}
-				}
+			} else {
+				dispositionGrouperMap.put(disposition, c);
 			}
 		}
 	}
@@ -83,13 +82,13 @@ public class MissingDispositionSubstanceGroupers extends TermServerReport {
 				String usedByStr = "";
 				if (dispositionSubstanceMap.containsKey(disposition)) {
 					Set<Concept> usedBy = dispositionSubstanceMap.get(disposition);
-					usedByStr = usedBy.stream().map(c -> c.toString())
+					usedByStr = usedBy.stream().map(Concept::toString)
 								.collect(Collectors.joining(", \n"));
 				}
 				incrementSummaryInformation("Missing groupers reported");
 				report(disposition, usedByStr);
 			} else {
-				LOGGER.debug (disposition + " -> " + dispositionGrouperMap.get(disposition));
+				LOGGER.debug("{} -> {}", disposition, dispositionGrouperMap.get(disposition));
 			}
 			incrementSummaryInformation("Dispositions checked");
 		}
