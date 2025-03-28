ed4e5a1505ea706ec5102ae632f9acb86c58c89f
pgwilliams <petergwilliams@hotmail.com>
2025-03-28 11:19:02 +0000

    NPU-3 Align packages with CPM standard. Set default unit of measure where not supplied..

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/pipeline/npu/template/NpuTemplatedConcept.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/pipeline/npu/template/NpuTemplatedConcept.java
@@ -1,4 +1,4 @@
-package org.ihtsdo.termserver.scripting.pipeline.npu;
+package org.ihtsdo.termserver.scripting.pipeline.npu.template;
 
 import java.util.*;
 
@@ -6,13 +6,28 @@ import org.ihtsdo.otf.exception.TermServerScriptException;
 import org.ihtsdo.termserver.scripting.domain.*;
 import org.ihtsdo.termserver.scripting.pipeline.*;
 import org.ihtsdo.termserver.scripting.pipeline.domain.ExternalConcept;
+import org.ihtsdo.termserver.scripting.pipeline.loinc.domain.LoincDetail;
+import org.ihtsdo.termserver.scripting.pipeline.npu.ImportNpuConcepts;
+import org.ihtsdo.termserver.scripting.pipeline.npu.NpuScriptConstants;
 import org.ihtsdo.termserver.scripting.pipeline.npu.domain.NpuConcept;
 import org.ihtsdo.termserver.scripting.pipeline.npu.domain.NpuDetail;
 import org.ihtsdo.termserver.scripting.pipeline.template.TemplatedConcept;
 
 public abstract class NpuTemplatedConcept extends TemplatedConcept implements NpuScriptConstants {
+	private static Concept unitsAttribute;
+	private static RelationshipTemplate defaultUnitOfMeasureAttribute;
+	private static Map<String, NpuDetail> npuDetailMap;
 
-	@Override
+	public static void initialise(ContentPipelineManager cpm,
+	                              Map<String, NpuDetail> npuDetailMap) throws TermServerScriptException {
+		TemplatedConcept.cpm = cpm;
+		TemplatedConcept.gl = cpm.getGraphLoader();
+		Concept unitOfMeasure = gl.getConcept("767524001 |Unit of measure| ");
+		unitsAttribute = gl.getConcept("246514001 |Units|");
+		defaultUnitOfMeasureAttribute = new RelationshipTemplate(unitsAttribute, unitOfMeasure);
+	}
+
+		@Override
 	public String getSchemaId() {
 		return SCTID_NPU_SCHEMA;
 	}
@@ -40,14 +55,14 @@ public abstract class NpuTemplatedConcept extends TemplatedConcept implements Np
 		typeMap.put(NPU_PART_PROPERTY, gl.getConcept("370130000 |Property (attribute)|"));
 		typeMap.put(NPU_PART_SCALE, gl.getConcept("370132008 |Scale type (attribute)|"));
 		typeMap.put(NPU_PART_SYSTEM, gl.getConcept("704319004 |Inheres in|"));
-		typeMap.put(NPU_PART_UNIT, gl.getConcept("246514001 |Units|"));
+		typeMap.put(NPU_PART_UNIT, unitsAttribute);
 	}
 
 	@Override
 	protected void populateParts() throws TermServerScriptException {
 		prepareConceptDefaultedForModule(SCTID_NPU_EXTENSION_MODULE);
 		NpuConcept npuConcept = getNpuConcept();
-		NpuDetail npuDetail = ((ImportNpuConcepts)cpm).npuDetailsMap.get(getExternalIdentifier());
+		NpuDetail npuDetail = ((ImportNpuConcepts)cpm).getDetailsMap().get(getExternalIdentifier());
 		for (Part part : npuDetail.getParts(npuConcept)) {
 			populatePart(part);
 		}
@@ -59,6 +74,13 @@ public abstract class NpuTemplatedConcept extends TemplatedConcept implements Np
 	private void populatePart(Part part) throws TermServerScriptException {
 		List<RelationshipTemplate> attributesToAdd = new ArrayList<>();
 		addAttributeFromDetail(attributesToAdd, part);
+
+		if (part.getPartTypeName().equals(NPU_PART_UNIT) && attributesToAdd.isEmpty()) {
+			//NPU Concepts without a unit will be given a default unit of measure
+			attributesToAdd.add(defaultUnitOfMeasureAttribute);
+			slotTermMap.put(NPU_PART_UNIT, "");
+		}
+
 		for (RelationshipTemplate rt : attributesToAdd) {
 			addAttributesToConcept(rt, part, false);
 		}
