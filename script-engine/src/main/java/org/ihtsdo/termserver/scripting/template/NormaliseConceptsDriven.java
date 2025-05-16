f859cddaf17c6472fc94f8b016e24ac36e5f8136
pgwilliams <petergwilliams@hotmail.com>
2025-05-16 09:38:31 +0100

    MSSP-3796 Sonar Qube improvements on class to normalise concepts

--- script-engine/src/main/java/org/ihtsdo/termserver/scripting/template/NormaliseConceptsDriven.java
+++ script-engine/src/main/java/org/ihtsdo/termserver/scripting/template/NormaliseConceptsDriven.java
@@ -1,7 +1,6 @@
 package org.ihtsdo.termserver.scripting.template;
 
 import org.ihtsdo.otf.exception.TermServerScriptException;
-import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Component;
 import org.ihtsdo.otf.rest.client.terminologyserver.pojo.Task;
 import org.ihtsdo.termserver.scripting.ValidationFailure;
 import org.ihtsdo.termserver.scripting.domain.Concept;
@@ -10,18 +9,11 @@ import org.ihtsdo.termserver.scripting.domain.RelationshipGroup;
 import org.ihtsdo.termserver.scripting.domain.RelationshipTemplate;
 import org.ihtsdo.termserver.scripting.fixes.BatchFix;
 import org.ihtsdo.termserver.scripting.util.SnomedUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.snomed.otf.script.dao.ReportSheetManager;
 
-import java.io.PrintStream;
 import java.util.*;
 
 public class NormaliseConceptsDriven extends BatchFix {
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(NormaliseConceptsDriven.class);
-
-	Concept ppp;
 
 	public NormaliseConceptsDriven(BatchFix clone) {
 		super(clone);
@@ -37,19 +29,20 @@ public class NormaliseConceptsDriven extends BatchFix {
 			app.postInit();
 			app.processFile();
 		} catch (Exception e) {
-			LOGGER.info("Failed to NormaliseTemplateCompliantConcepts due to " + e.getMessage());
-			e.printStackTrace(new PrintStream(System.out));
+			throw new TermServerScriptException("Failed to NormaliseTemplateCompliantConcepts", e);
 		} finally {
 			app.finish();
 		}
 	}
-	
+
+	@Override
 	protected void init(String[] args) throws TermServerScriptException {
 		reportNoChange = false;
 		summaryTabIdx = SECONDARY_REPORT;
 		super.init(args);
 	}
-	
+
+	@Override
 	public void postInit() throws TermServerScriptException {
 		String[] columnHeadings = new String[] {"TASK_KEY, TASK_DESC, SCTID, FSN, ConceptType, Severity, ActionType, CharacteristicType, MatchedTemplate, Detail, Detail, Detail",
 				"Report Metadata, Detail, Detail"};
@@ -59,10 +52,10 @@ public class NormaliseConceptsDriven extends BatchFix {
 	}
 
 	@Override
-	protected int doFix(Task task, Concept concept, String info) throws TermServerScriptException, ValidationFailure {
+	protected int doFix(Task task, Concept concept, String info) throws TermServerScriptException {
 		Concept loadedConcept = loadConcept(concept, task.getBranchPath());
-		if ((loadedConcept.getGciAxioms() != null && loadedConcept.getGciAxioms().size() > 0) 
-				|| (loadedConcept.getAdditionalAxioms() != null && loadedConcept.getAdditionalAxioms().size() > 0)) {
+		if ((loadedConcept.getGciAxioms() != null && !loadedConcept.getGciAxioms().isEmpty())
+				|| (loadedConcept.getAdditionalAxioms() != null && !loadedConcept.getAdditionalAxioms().isEmpty())) {
 			throw new ValidationFailure(task, loadedConcept, "Concept uses axioms");
 		}
 		int changesMade = removeRedundandRelationships(task, loadedConcept);
@@ -100,16 +93,12 @@ public class NormaliseConceptsDriven extends BatchFix {
 		
 		nextInferredGroup:
 		for (RelationshipGroup inferredGroup : inferredGroups) {
-			boolean matchFound = false;
 			for (RelationshipGroup statedGroup : statedGroups) {
 				if (inferredGroup.equals(statedGroup)) {
-					matchFound = true;
 					continue nextInferredGroup;
 				}
 			}
-			if (!matchFound) {
-				toBeStated.add(inferredGroup);
-			}
+			toBeStated.add(inferredGroup);
 		}
 		changesMade += stateRelationshipGroups(t, c, toBeStated);
 		if (changesMade == 0) {
@@ -130,7 +119,7 @@ public class NormaliseConceptsDriven extends BatchFix {
 		Set<Relationship> ungrouped = c.getRelationshipGroup(CharacteristicType.STATED_RELATIONSHIP, UNGROUPED).getRelationships();
 		for (Relationship r : ungrouped) {
 			Set<Relationship> inferredMatches = c.getRelationships(CharacteristicType.INFERRED_RELATIONSHIP, r);
-			if (inferredMatches.size() == 0) {
+			if (inferredMatches.isEmpty()) {
 				removeRelationship(t, c, r, "Redundant ungrouped stated: ");
 				changesMade++;
 			}
@@ -141,7 +130,7 @@ public class NormaliseConceptsDriven extends BatchFix {
 	private int stateRelationshipGroups(Task t, Concept c, List<RelationshipGroup> toBeStated) throws TermServerScriptException {
 		int changesMade = 0;
 		for (RelationshipGroup g : toBeStated) {
-			//Group 0 must remain group 0.  Otherwise find an available group number
+			//Group 0 must remain group 0.  Otherwise, find an available group number
 			int freeGroup = g.getGroupId()==0?0:SnomedUtils.getFirstFreeGroup(c);
 			changesMade += stateRelationshipGroup(t, c, g, freeGroup);
 		}
@@ -159,28 +148,4 @@ public class NormaliseConceptsDriven extends BatchFix {
 		return changesMade;
 	}
 
-	/*protected List<Component> identifyComponentsToProcess() throws TermServerScriptException {
->>>>>>> 00c9a7f1b (INFRA-12848 Add class to normalize concepts based on list loaded from a text file)
-		//First pass attempt to remodel because we don't want to batch anything that results 
-		//in no changes being made.
-		Set<Concept> changesRequired = new HashSet<>();
-		Set<Concept> noChangesRequired = new HashSet<>();
-		
-		setQuiet(true);
-		CharacteristicType charType = useStatedECL ? CharacteristicType.STATED_RELATIONSHIP : CharacteristicType.INFERRED_RELATIONSHIP;
-		for (Concept concept : findConcepts(ecl, true, charType)) {
-			//Make changes to a clone of the concept so we don't affect our local copy
-			Concept clone = concept.cloneWithIds();
-			int changesMade = normaliseConcept(null, clone);
-			if (changesMade > 0) {
-				changesRequired.add(concept);
-			} else {
-				noChangesRequired.add(concept);
-			}
-		}
-		setQuiet(false);
-		addSummaryInformation("Concepts no change required", noChangesRequired.size());
-		return asComponents(changesRequired);
-	}*/
-	
 }
