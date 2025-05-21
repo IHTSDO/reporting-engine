0d1565e91667960a51a924991a816734563e148b
pgwilliams <petergwilliams@hotmail.com>
2025-05-21 10:11:28 +0100

    RP-1016 Ensure Inactive Descriptions report available for MS, and remove option to run with a release zip package

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/release/InactivatedDescriptions.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/release/InactivatedDescriptions.java
@@ -10,26 +10,16 @@ import org.ihtsdo.termserver.scripting.domain.Concept;
 import org.ihtsdo.termserver.scripting.domain.Description;
 import org.ihtsdo.termserver.scripting.reports.TermServerReport;
 import org.ihtsdo.termserver.scripting.util.SnomedUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.snomed.otf.scheduler.domain.*;
 import org.snomed.otf.scheduler.domain.Job.ProductionStatus;
 import org.snomed.otf.scheduler.domain.JobParameter.Type;
 import org.snomed.otf.script.dao.ReportSheetManager;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class InactivatedDescriptions extends TermServerReport implements ReportClass {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(InactivatedDescriptions.class);
-
-	static final String RELEASE = "Release Package";
 	static final String SEMTAG_FILTER_PARAM = "Filter for SemTag";
-
-	private String thisEffectiveTime;
 	private String semtagFilter;
 
 	public static void main(String[] args) throws TermServerScriptException {
@@ -43,11 +33,6 @@ public class InactivatedDescriptions extends TermServerReport implements ReportC
 		semtagFilter = run.getParamValue(SEMTAG_FILTER_PARAM);
 		//Unpromoted changes only is picked up by the TermServerReport parent class as part of init
 		super.init(run);
-
-		//Running unpromoted changes only can't work with a release archive
-		if (unpromotedChangesOnly && !StringUtils.isEmpty(run.getParamValue(RELEASE))) {
-			throw new TermServerScriptException("Unpromoted changes only is not supported for use with a release package");
-		}
 	}
 
 	@Override
@@ -66,19 +51,16 @@ public class InactivatedDescriptions extends TermServerReport implements ReportC
 		JobParameters params = new JobParameters()
 				.add(SEMTAG_FILTER_PARAM).withType(Type.STRING)
 				.add(UNPROMOTED_CHANGES_ONLY).withType(Type.BOOLEAN).withMandatory().withDefaultValue("false")
-				.add(RELEASE).withType(Type.RELEASE_ARCHIVE)
 				.build();
 		return new Job()
 				.withCategory(new JobCategory(JobType.REPORT, JobCategory.RELEASE_STATS))
 				.withName("Inactivated Descriptions")
 				.withDescription("This report lists all descriptions inactivated in the current release cycle " +
 								"along with the reason and historical association where applicable, " +
-								"optionally restricted to a particular semantic tag. " +
-								"Also optional is specifying a previously published release package to run against." +
-								"The issue count here is the total number of descriptions inactivated.")
+								"optionally restricted to a particular semantic tag. ")
 				.withProductionStatus(ProductionStatus.PROD_READY)
 				.withParameters(params)
-				.withTag(INT)
+				.withTag(INT).withTag(MS)
 				.build();
 	}
 
@@ -105,11 +87,9 @@ public class InactivatedDescriptions extends TermServerReport implements ReportC
 	}
 	
 	private boolean inScope(Description d) {
-		// We want inactive descriptions modified either in the current release cycle (i.e. effectiveTime is null), or
-		// in the latest release if we're looking at a particular release package (i.e. effectiveTime is not null)
+		// We want inactive descriptions modified  in the current release cycle i.e. effectiveTime is null
 		// Also optionally, unpromoted changes only
-		return !d.isActiveSafely()
-				&& ((thisEffectiveTime == null && StringUtils.isEmpty(d.getEffectiveTime())) || (thisEffectiveTime != null && thisEffectiveTime.equals(d.getEffectiveTime())))
+		return !d.isActiveSafely() && StringUtils.isEmpty(d.getEffectiveTime())
 				&& (!unpromotedChangesOnly || unpromotedChangesHelper.hasUnpromotedChange(d));
 	}
 	
@@ -123,20 +103,6 @@ public class InactivatedDescriptions extends TermServerReport implements ReportC
 		return semTag != null && semTag.contains(semtagFilter);
 	}
 
-	@Override
-	protected void loadProjectSnapshot(boolean fsnOnly) throws TermServerScriptException {
-		String release = getJobRun().getParamValue(RELEASE);
-		if (!StringUtils.isEmpty(release)) {
-			LOGGER.info("Loading previously published package: {}", release);
-			getProject().setKey(release);
-			super.loadProjectSnapshot(fsnOnly);
-			thisEffectiveTime = gl.getCurrentEffectiveTime();
-			LOGGER.info("Detected this effective time as {}", thisEffectiveTime);
-		} else {
-			super.loadProjectSnapshot(fsnOnly);
-		}
-	}
-	
 	private List<Concept> scopeAndSort(Collection<Concept> superSet) {
 		// Filter concepts by a specific semantic tag (if provided) and then sort
 		return superSet.stream()
