2f0302814095c72b91b3b9a4db7b3161863a3193
Elena Ilyukhina <eil@ihtsdo.org>
2026-02-19 17:56:12 +0000

    RP-956 Add main() method

--- reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/release/ReleaseStatsEditions.java
+++ reporting-engine-worker/src/main/java/org/ihtsdo/termserver/scripting/reports/release/ReleaseStatsEditions.java
@@ -1,10 +1,25 @@
 package org.ihtsdo.termserver.scripting.reports.release;
 
+import org.ihtsdo.otf.exception.TermServerScriptException;
+import org.ihtsdo.termserver.scripting.TermServerScript;
 import org.snomed.otf.scheduler.domain.*;
 import org.snomed.otf.script.dao.ReportConfiguration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SuppressWarnings("java:S110")
 public class ReleaseStatsEditions extends SummaryComponentStats {
+
+	public static void main(String[] args) throws TermServerScriptException {
+		Map<String, String> params = new HashMap<>();
+		params.put(THIS_RELEASE, "SnomedCT_InternationalRF2_PRODUCTION_20260201T120000Z.zip");
+		params.put(PREV_RELEASE, "SnomedCT_InternationalRF2_PRODUCTION_20260101T120000Z.zip");
+		params.put(REPORT_OUTPUT_TYPES, "S3");
+		params.put(REPORT_FORMAT_TYPE, "JSON");
+		TermServerScript.run(ReleaseStatsEditions.class, args, params);
+	}
+
 	@Override
 	public Job getJob() {
 		JobParameters params = new JobParameters()
